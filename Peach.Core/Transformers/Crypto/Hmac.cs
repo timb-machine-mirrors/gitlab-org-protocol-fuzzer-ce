955126cda4a8d4fc3a9c48c2629b716e31b0bd93
egriko <egriko@9c48eac0-7620-0410-89eb-927fe2045f54>
2012-01-13 02:01:14 +0000

    Outlined Crypto Transformers, implemented some of the Type Transformers.

---
+++ Peach.Core/Transformers/Crypto/Hmac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+using System.IO;
+using Peach.Core.Dom;
+using Peach.Core.IO;
+
+namespace Peach.Core.Transformers.Crypto
+{
+    [TransformerAttribute("Hmac", "HMAC as described in RFC 2104.")]
+    public class Hmac : Transformer
+    {
+        public Hmac(Dictionary<string, Variant> args) : base(args)
+		{
+		}
+
+		protected override BitStream internalEncode(BitStream data)
+		{
+            throw new NotImplementedException();
+		}
+
+		protected override BitStream internalDecode(BitStream data)
+		{
+            throw new NotImplementedException();
+		}
+    }
+}
