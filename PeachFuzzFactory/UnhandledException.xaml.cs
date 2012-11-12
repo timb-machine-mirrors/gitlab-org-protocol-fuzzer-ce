c879d78aa48ad0aece15a2deb846ec94e5c17982
matt <matt@dejavusecurity.com>
2012-11-12 15:46:14 -0800

    FuzzFactory + PitMaker work

---
+++ PeachFuzzFactory/UnhandledException.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace PeachFuzzFactory
+{
+  /// <summary>
+  /// Interaction logic for UnhandledException.xaml
+  /// </summary>
+  public partial class UnhandledException : Window
+  {
+    public UnhandledException(Exception ex)
+    {
+      InitializeComponent();
+      StringBuilder message = new StringBuilder();
+
+      message.Append(ex.Message);
+      if (ex.InnerException != null)
+      {
+        message.Append(Environment.NewLine);
+        message.Append(Environment.NewLine);
+        message.Append(ex.InnerException.Message);
+      }
+
+      txtMessage.Text = message.ToString();
+    }
+  }
+}
