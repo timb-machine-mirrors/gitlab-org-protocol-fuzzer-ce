41dea771b1fa07fd8721e49e7466d5cf5c630a52
matt <matt@dejavusecurity.com>
2013-03-27 14:51:27 -0700

    Peach Farm Monitor work

--- PeachFarm/PeachFarm.Common/RabbitMqHelper.cs
+++ PeachFarm/PeachFarm.Common/RabbitMqHelper.cs
@@ -243,7 +243,7 @@ namespace PeachFarm.Common
 				declarer.Close();
 
 			if (connection.IsOpen)
-				connection.Close();
+				connection.Close(1000);
 
 		}
 		#endregion
@@ -314,13 +314,14 @@ namespace PeachFarm.Common
 			ticking = false;
 		}
 
-		private void OpenConnection()
+		private void OpenConnection(int timeout = 5000)
 		{
 			ConnectionFactory factory = new ConnectionFactory();
 			factory.HostName = hostName;
 			factory.Port = port;
 			factory.UserName = userName;
 			factory.Password = password;
+			factory.RequestedConnectionTimeout = timeout;
 			factory.Ssl.Enabled = ssl;
 			factory.Ssl.AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNotAvailable | SslPolicyErrors.RemoteCertificateChainErrors | SslPolicyErrors.RemoteCertificateNameMismatch;
 
