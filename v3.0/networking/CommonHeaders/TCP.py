5d9808a837822c8fbe286633d06de39846a887c3
Josh Dukes <josh@dejavusecurity.com>
2013-03-27 18:49:27 -0700

    one full session is working

--- v3.0/networking/CommonHeaders/TCP.py
+++ v3.0/networking/CommonHeaders/TCP.py
@@ -3,15 +3,12 @@
 from zlib import crc32 
 import time
 
+import code
+
 import clr
 clr.AddReferenceByPartialName('Peach.Core')
 
 import Peach.Core
-
-# import code
-# code.InteractiveConsole(locals=globals()).interact()
-
-#add tracking for window
 
 def init_seq(ctx):
     # print "init running"
@@ -27,12 +24,25 @@ def init_seq(ctx):
     except Exception, e:
         print e
         raise e
-        # code.InteractiveConsole(locals=globals()).interact()
-    # print "verifying value is stored: %s" % get_store_val(ctx, 'SequenceNumber')
 
 
-def process_sequence(ctx):
-    return set_to_store(ctx, AcknowledgmentNumber=int(ctx.dataModel.find('SequenceNumber').InternalValue.ToString())+(len(ctx.dataModel.find('TcpPayload').InternalValue.ToString()) or 1))
+def set_next_seq(ctx):
+    ret = set_to_store(ctx, NextSequenceNumber=int(ctx.dataModel.find('SequenceNumber').InternalValue.ToString())+(len(ctx.dataModel.find('TcpPayload').Value.Value) or 1))
+    print "NextSeq == ", get_store_val(ctx, "NextSequenceNumber")
+    return ret 
+
+
+def sync_from_store(ctx):
+    if bool(int(ctx.dataModel.find('ACK').InternalValue)):
+        set_default_from_store(ctx, "AcknowledgmentNumber")
+    ctx.dataModel.find("SequenceNumber").DefaultValue = Peach.Core.Variant(get_store_val(ctx, "NextSequenceNumber"))
+    ctx.dataModel.Value
+
+
+def store_next_acknum(ctx):
+    ret = set_to_store(ctx, AcknowledgmentNumber=int(ctx.dataModel.find('SequenceNumber').InternalValue.ToString())+(len(ctx.dataModel.find('TcpPayload').InternalValue.ToString()) or 1))
+    print "AckNum == ", get_store_val(ctx, "AcknowledgmentNumber")
+    return ret
                         
 
 def set_to_store(ctx, **kwarg):
@@ -40,6 +50,7 @@ def set_to_store(ctx, **kwarg):
     for k,v in kwarg.iteritems():
         print "setting value with key %s to %s" % (k,hex(v))
         ctx.parent.parent.parent.context.iterationStateStore[k] = v
+    return True
 
 
 def get_store_val(ctx, name):
@@ -68,7 +79,10 @@ def inc_stored_vals(ctx, *args):
 
 
 def chk_if_ack_for_me(ctx):
-    return int(ctx.parent.actions[0].dataModel.find('AcknowledgmentNumber').InternalValue.ToString()) == get_store_val(ctx, 'SequenceNumber') and bool(int(ctx.parent.actions[0].dataModel.find('ACK').InternalValue))
+    print "AckNum ", int(ctx.parent.actions[0].dataModel.find('AcknowledgmentNumber').InternalValue.ToString())
+    print "Expected AckNum ", get_store_val(ctx, 'NextSequenceNumber')
+    ret = int(ctx.parent.actions[0].dataModel.find('AcknowledgmentNumber').InternalValue.ToString()) == (get_store_val(ctx, 'NextSequenceNumber')) and bool(int(ctx.parent.actions[0].dataModel.find('ACK').InternalValue))
+    return ret
 
 
 def set_timestamp(ctx):
