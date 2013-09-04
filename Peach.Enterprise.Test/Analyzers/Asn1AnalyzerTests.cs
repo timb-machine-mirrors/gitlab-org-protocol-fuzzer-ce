1bfc38c589bb097fb5d8db78e5031e07bababd6a
Seth Hinze <seth@dejavusecurity.com>
2013-09-03 17:45:39 -0700

    asn1 work.

--- Peach.Enterprise.Test/Analyzers/Asn1AnalyzerTests.cs
+++ Peach.Enterprise.Test/Analyzers/Asn1AnalyzerTests.cs
@@ -4,6 +4,9 @@ using System.Reflection;
 using System.IO;
 using Asn1Lib;
 using System.Text;
+using Peach.Core.Analyzers;
+using Peach.Core.IO;
+using Peach.Core.Cracker;
 
 namespace Peach.Enterprise.Test.Analyzers
 {
@@ -11,6 +14,15 @@ namespace Peach.Enterprise.Test.Analyzers
 	class Asn1AnalyzerTests
 	{
 		static MemoryStream googleDer = LoadResource("google.der");
+
+		static string xml = @"
+<Peach>
+	<DataModel name='DM'>
+		<Blob>
+			<Analyzer class='Asn1'/>
+		</Blob>
+	</DataModel>
+</Peach>";
 
 		[SetUp]
 		public void SetUp()
@@ -46,5 +58,42 @@ namespace Peach.Enterprise.Test.Analyzers
 			}
 		}
 
+		[Test]
+		public void ParserTest()
+		{
+			var data = new MemoryStream(File.ReadAllBytes("C:\\Users\\seth\\Downloads\\TEST_SUITE\\TEST_SUITE\\tc38.ber"));
+
+			var ber = Asn1.Decode(data);
+
+			var ms = new MemoryStream();
+
+			Asn1.ExportText(ms, ber);
+
+			var final = Encoding.ASCII.GetString(ms.ToArray());
+
+			Console.WriteLine(final);
+
+		}
+
+		[Test]
+		public void Crack1()
+		{
+			var data = new MemoryStream(File.ReadAllBytes("C:\\Users\\seth\\Downloads\\TEST_SUITE\\TEST_SUITE\\tc38.ber"));
+			CrackData(data);
+		}
+
+		private static void CrackData(Stream data)
+		{
+
+			var parser = new PitParser();
+			var dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			data.Seek(0, SeekOrigin.Begin);
+			var bs = new BitStream(data);
+
+			var cracker = new DataCracker();
+			cracker.CrackData(dom.dataModels[0], bs);
+
+		}
 	}
 }
--- Peach.Enterprise/Analyzers/Asn1Analyzer.cs
+++ Peach.Enterprise/Analyzers/Asn1Analyzer.cs
@@ -7,13 +7,15 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Reflection;
 
-using Peach.Enterprise.Analyzers.ASN1;
+//using Peach.Enterprise.Analyzers.ASN1;
 using Peach.Enterprise.DataElements;
 
 using Peach.Core;
 using Peach.Core.Dom;
 using Peach.Core.IO;
 using Peach.Core.Cracker;
+
+using Asn1Lib;
 
 namespace Peach.Enterprise.Analyzers
 {
@@ -22,30 +24,34 @@ namespace Peach.Enterprise.Analyzers
 	[Serializable]
 	public class Asn1Analyzer : Analyzer
 	{
-		string CurrentTag = null;
+//		string CurrentTag = null;
 
 		static Asn1Analyzer()
-        {
-            supportParser = false;
-            supportDataElement = true;
-            supportCommandLine = false;
-            supportTopLevel = false;
-        }
+		{
+			supportParser = false;
+			supportDataElement = true;
+			supportCommandLine = false;
+			supportTopLevel = false;
+		}
 
-        public Asn1Analyzer()
-        {
-        }
+		public Asn1Analyzer()
+		{
+		}
 
 		public Asn1Analyzer(Dictionary<string, Variant> args)
-        {
-        }
+		{
+		}
 
 		public override void asDataElement(DataElement parent, Dictionary<DataElement, Position> positions)
 		{
-			if (!(parent is Blob))
-				throw new PeachException("Error, Asn1 analyzer only operates on Blob elements!");
+			if (parent == null)
+				throw new ArgumentNullException("parent");
 
 			var blob = parent as Blob;
+
+			if (blob == null)
+				throw new PeachException("Error, Asn1 analyzer only operates on Blob elements!");
+
 			var data = blob.Value;
 
 			if (data.Length == 0)
@@ -53,177 +59,186 @@ namespace Peach.Enterprise.Analyzers
 
 			data.Seek(0, SeekOrigin.Begin);
 
-			var parser = new ASN1.Asn1Parser();
-			parser.LoadData(data);
-			var rootNode = parser.RootNode;
+			var asn1 = Asn1.Decode(data);
 
 			var block = new Block(blob.name);
-			block.Add(HandleAsn1Node(block, rootNode));
 
+			// Move over relations
+			foreach (var relation in blob.relations)
+				block.relations.Add(relation);
+			blob.relations.Clear();
+
+			WalkAsn1Tree(block, asn1);
+
+			// Replace blob with block
 			block.parent = blob.parent;
 			block.parent[blob.name] = block;
 		}
 
-		static int NameCounter = 0;
-		DataElementContainer MakeAsn1Der(DataElement data, Asn1Node node)
+		void WalkAsn1Tree(DataElementContainer parent, Asn1Tag tag)
 		{
-			NameCounter++;
-
-			var container = new Block(CurrentTag + NameCounter);
-			var type = new Number("Type");
-			var asn1Length = new Asn1DerLength("Asn1DerLength");
-			var length = new Number("Length");
-			var lengthRelation = new SizeRelation();
-
-			type.length = 8;
-			type.Signed = false;
-			type.LittleEndian = false;
-			type.DefaultValue = new Variant(node.Tag);
-
-			length.length = 32;
-			length.Signed = false;
-			length.LittleEndian = false;
-			length.DefaultValue = new Variant(node.DataLength);
-			asn1Length.Add(length);
-
-			container.Add(type);
-			container.Add(asn1Length);
-			container.Add(data);
-
-			length.relations.Add(lengthRelation);
-			data.relations.Add(lengthRelation);
-			lengthRelation.FromName = length.name;
-			lengthRelation.OfName = data.name;
-
-			return container;
 		}
 
-		DataElementContainer HandleSequence(DataElementContainer parent, Asn1Node sequence)
-		{
-			var block = new Block(CurrentTag + NameCounter);
+		//static int NameCounter = 0;
+		//DataElementContainer MakeAsn1Der(DataElement data, Asn1Node node)
+		//{
+		//    NameCounter++;
 
-			for (int count = 0; count < sequence.ChildNodeCount; count++)
-			{
-				var child = sequence.GetChildNode(count);
-				block.Add(HandleAsn1Node(block, child));
-			}
+		//    var container = new Block(CurrentTag + NameCounter);
+		//    var type = new Number("Type");
+		//    var asn1Length = new Asn1DerLength("Asn1DerLength");
+		//    var length = new Number("Length");
+		//    var lengthRelation = new SizeRelation();
 
-			return MakeAsn1Der(block, sequence);
-		}
+		//    type.length = 8;
+		//    type.Signed = false;
+		//    type.LittleEndian = false;
+		//    type.DefaultValue = new Variant(node.Tag);
 
-		DataElementContainer HandleBitString(DataElementContainer parent, Asn1Node sequence)
-		{
-			var block = new Block(CurrentTag + NameCounter);
+		//    length.length = 32;
+		//    length.Signed = false;
+		//    length.LittleEndian = false;
+		//    length.DefaultValue = new Variant(node.DataLength);
+		//    asn1Length.Add(length);
 
-			var num = new Number();
-			num.length = 8;
-			num.Signed = false;
-			num.LittleEndian = false;
-			num.DefaultValue = new Variant(0);
-			block.Add(num);
+		//    container.Add(type);
+		//    container.Add(asn1Length);
+		//    container.Add(data);
 
-			if (sequence.ChildNodeCount == 0)
-			{
-				var data = new Blob();
-				data.DefaultValue = new Variant(sequence.Data);
-				block.Add(data);
-			}
-			else for (int count = 0; count < sequence.ChildNodeCount; count++)
-			{
-				var child = sequence.GetChildNode(count);
-				block.Add(HandleAsn1Node(block, child));
-			}
+		//    length.relations.Add(lengthRelation);
+		//    data.relations.Add(lengthRelation);
+		//    lengthRelation.FromName = length.name;
+		//    lengthRelation.OfName = data.name;
 
-			return MakeAsn1Der(block, sequence);
-		}
+		//    return container;
+		//}
 
-		DataElement HandleAsn1Node(DataElementContainer parent, Asn1Node node)
-		{
-			NameCounter++;
+		//DataElementContainer HandleSequence(DataElementContainer parent, Asn1Node sequence)
+		//{
+		//    var block = new Block(CurrentTag + NameCounter);
 
-			switch ((Asn1TagClasses)(node.Tag & (byte)Asn1TagClasses.CLASS_MASK))
-			{
-				case Asn1TagClasses.CONTEXT_SPECIFIC:
-					CurrentTag = Asn1TagClasses.CONTEXT_SPECIFIC.ToString();
-					return HandleSequence(parent, node);
+		//    for (int count = 0; count < sequence.ChildNodeCount; count++)
+		//    {
+		//        var child = sequence.GetChildNode(count);
+		//        block.Add(HandleAsn1Node(block, child));
+		//    }
 
-			}
+		//    return MakeAsn1Der(block, sequence);
+		//}
 
-			CurrentTag = ((Asn1Tag)(node.Tag & (byte)Asn1Tag.TAG_MASK)).ToString();
+		//DataElementContainer HandleBitString(DataElementContainer parent, Asn1Node sequence)
+		//{
+		//    var block = new Block(CurrentTag + NameCounter);
 
-			switch ((Asn1Tag)(node.Tag & (byte)Asn1Tag.TAG_MASK))
-			{
+		//    var num = new Number();
+		//    num.length = 8;
+		//    num.Signed = false;
+		//    num.LittleEndian = false;
+		//    num.DefaultValue = new Variant(0);
+		//    block.Add(num);
 
-				case Asn1Tag.PRINTABLE_STRING:
-				case Asn1Tag.IA5_STRING:
-					{
-						var str = new Peach.Core.Dom.String("String");
-						str.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
-						return MakeAsn1Der(str, node);
-					}
-				case Asn1Tag.UTC_TIME:
-				case Asn1Tag.NUMERIC_STRING:
-					{
-						var numstr = new Peach.Core.Dom.String("NumericString");
-						numstr.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
-						// TODO add hint for numeric
-						return MakeAsn1Der(numstr, node);
-					}
-				case Asn1Tag.UTF8_STRING:
-					{
-						var ustr = new Peach.Core.Dom.String("Utf8String");
-						ustr.stringType = StringType.utf8;
-						ustr.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
-						return MakeAsn1Der(ustr, node);				
-					}
+		//    if (sequence.ChildNodeCount == 0)
+		//    {
+		//        var data = new Blob();
+		//        data.DefaultValue = new Variant(sequence.Data);
+		//        block.Add(data);
+		//    }
+		//    else for (int count = 0; count < sequence.ChildNodeCount; count++)
+		//        {
+		//            var child = sequence.GetChildNode(count);
+		//            block.Add(HandleAsn1Node(block, child));
+		//        }
 
-				case Asn1Tag.REAL:
-				case Asn1Tag.INTEGER:
-					// Issue cannot have bigger than 8 byte Number lengths... 
-					/*{
-						var num = new Number();
-						num.length = node.Data.Length * 8;
-						num.Signed = false;
-						num.LittleEndian = false;
-						num.DefaultValue = new Variant(node.Data);
-						return MakeAsn1Der(num, node);
-					}*/
-				case Asn1Tag.T61_STRING:
-				case Asn1Tag.VIDEOTEXT_STRING:
-				case Asn1Tag.GENERALIZED_TIME:
-				case Asn1Tag.GRAPHIC_STRING:
-				case Asn1Tag.VISIBLE_STRING:
-				case Asn1Tag.GENERAL_STRING:
-				case Asn1Tag.UNIVERSAL_STRING:
-				case Asn1Tag.BMPSTRING:
-				case Asn1Tag.OCTET_STRING:
-				case Asn1Tag.RELATIVE_OID:
-				case Asn1Tag.BOOLEAN:
-				case Asn1Tag.TAG_NULL:
-				case Asn1Tag.OBJECT_IDENTIFIER:
-				case Asn1Tag.OBJECT_DESCRIPTOR:
-				case Asn1Tag.EXTERNAL:
-				
-				case Asn1Tag.ENUMERATED:
-					{
-						var blob = new Blob("Blob"+NameCounter);
-						blob.DefaultValue = new Variant(node.Data);
+		//    return MakeAsn1Der(block, sequence);
+		//}
 
-						return MakeAsn1Der(blob, node);
-					}
+		//DataElement HandleAsn1Node(DataElementContainer parent, Asn1Node node)
+		//{
+		//    NameCounter++;
 
-				case Asn1Tag.BIT_STRING: // TODO Double check this
-					return HandleBitString(parent, node);
+		//    switch ((Asn1TagClasses)(node.Tag & (byte)Asn1TagClasses.CLASS_MASK))
+		//    {
+		//        case Asn1TagClasses.CONTEXT_SPECIFIC:
+		//            CurrentTag = Asn1TagClasses.CONTEXT_SPECIFIC.ToString();
+		//            return HandleSequence(parent, node);
 
-				case Asn1Tag.SEQUENCE:
-					return HandleSequence(parent, node);
-				
-				case Asn1Tag.SET:
-					return HandleSequence(parent, node);
-				default:
-					throw new PeachException("Error parsing ASN.1: " + node.Tag.ToString());
-			}
+		//    }
 
-		}
+		//    CurrentTag = ((Asn1Tag)(node.Tag & (byte)Asn1Tag.TAG_MASK)).ToString();
+
+		//    switch ((Asn1Tag)(node.Tag & (byte)Asn1Tag.TAG_MASK))
+		//    {
+
+		//        case Asn1Tag.PRINTABLE_STRING:
+		//        case Asn1Tag.IA5_STRING:
+		//            {
+		//                var str = new Peach.Core.Dom.String("String");
+		//                str.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
+		//                return MakeAsn1Der(str, node);
+		//            }
+		//        case Asn1Tag.UTC_TIME:
+		//        case Asn1Tag.NUMERIC_STRING:
+		//            {
+		//                var numstr = new Peach.Core.Dom.String("NumericString");
+		//                numstr.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
+		//                // TODO add hint for numeric
+		//                return MakeAsn1Der(numstr, node);
+		//            }
+		//        case Asn1Tag.UTF8_STRING:
+		//            {
+		//                var ustr = new Peach.Core.Dom.String("Utf8String");
+		//                ustr.stringType = StringType.utf8;
+		//                ustr.DefaultValue = new Variant(System.Text.Encoding.Default.GetString(node.Data));
+		//                return MakeAsn1Der(ustr, node);
+		//            }
+
+		//        case Asn1Tag.REAL:
+		//        case Asn1Tag.INTEGER:
+		//        // Issue cannot have bigger than 8 byte Number lengths... 
+		//        /*{
+		//            var num = new Number();
+		//            num.length = node.Data.Length * 8;
+		//            num.Signed = false;
+		//            num.LittleEndian = false;
+		//            num.DefaultValue = new Variant(node.Data);
+		//            return MakeAsn1Der(num, node);
+		//        }*/
+		//        case Asn1Tag.T61_STRING:
+		//        case Asn1Tag.VIDEOTEXT_STRING:
+		//        case Asn1Tag.GENERALIZED_TIME:
+		//        case Asn1Tag.GRAPHIC_STRING:
+		//        case Asn1Tag.VISIBLE_STRING:
+		//        case Asn1Tag.GENERAL_STRING:
+		//        case Asn1Tag.UNIVERSAL_STRING:
+		//        case Asn1Tag.BMPSTRING:
+		//        case Asn1Tag.OCTET_STRING:
+		//        case Asn1Tag.RELATIVE_OID:
+		//        case Asn1Tag.BOOLEAN:
+		//        case Asn1Tag.TAG_NULL:
+		//        case Asn1Tag.OBJECT_IDENTIFIER:
+		//        case Asn1Tag.OBJECT_DESCRIPTOR:
+		//        case Asn1Tag.EXTERNAL:
+
+		//        case Asn1Tag.ENUMERATED:
+		//            {
+		//                var blob = new Blob("Blob" + NameCounter);
+		//                blob.DefaultValue = new Variant(node.Data);
+
+		//                return MakeAsn1Der(blob, node);
+		//            }
+
+		//        case Asn1Tag.BIT_STRING: // TODO Double check this
+		//            return HandleBitString(parent, node);
+
+		//        case Asn1Tag.SEQUENCE:
+		//            return HandleSequence(parent, node);
+
+		//        case Asn1Tag.SET:
+		//            return HandleSequence(parent, node);
+		//        default:
+		//            throw new PeachException("Error parsing ASN.1: " + node.Tag.ToString());
+		//    }
+
+		//}
 	}
 }
