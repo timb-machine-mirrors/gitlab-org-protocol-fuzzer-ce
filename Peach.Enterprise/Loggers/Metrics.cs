4c67148164c576448bc0d2020dd353bafc56f52a
Seth Hinze <seth@dejavusecurity.com>
2013-10-24 15:31:51 -0700

    Use foreign keys for all columns in metrics talbe.

--- Peach.Enterprise/Loggers/Metrics.cs
+++ Peach.Enterprise/Loggers/Metrics.cs
@@ -22,18 +22,66 @@ namespace Peach.Enterprise.Loggers
 		private static string fileName = "metrics.sqlite";
 
 		static string create_table = @"
+CREATE TABLE states (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE actions (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE parameters (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE elements (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE mutators (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
+CREATE TABLE datasets (
+ id INTEGER PRIMARY KEY,
+ name TEXT NOT NULL
+);
+
 CREATE TABLE metrics (
  id INTEGER PRIMARY KEY,
- state TEXT NOT NULL,
- action TEXT NOT NULL,
- parameter TEXT NOT NULL,
- element TEXT NOT NULL,
- mutator TEXT NOT NULL,
- dataset TEXT NOT NULL,
- count INTEGER
+ state INTEGER,
+ action INTEGER,
+ parameter INTEGER,
+ element INTEGER,
+ mutator INTEGER,
+ dataset INTEGER,
+ count INTEGER,
+ FOREIGN KEY(state) REFERENCES states(id),
+ FOREIGN KEY(action) REFERENCES actions(id),
+ FOREIGN KEY(parameter) REFERENCES parameters(id),
+ FOREIGN KEY(element) REFERENCES elements(id),
+ FOREIGN KEY(mutator) REFERENCES mutators(id),
+ FOREIGN KEY(dataset) REFERENCES datasets(id)
 );";
 
 		static string create_index = @"
+CREATE UNIQUE INDEX states_index on states ( name );
+
+CREATE UNIQUE INDEX actions_index on actions ( name );
+
+CREATE UNIQUE INDEX parameters_index on parameters ( name );
+
+CREATE UNIQUE INDEX elements_index on elements ( name );
+
+CREATE UNIQUE INDEX mutators_index on mutators ( name );
+
+CREATE UNIQUE INDEX datasets_index on datasets ( name );
+
 CREATE UNIQUE INDEX metrics_index ON metrics (
  state,
  action,
@@ -42,39 +90,6 @@ CREATE UNIQUE INDEX metrics_index ON metrics (
  mutator,
  dataset
 );";
-
-/*
-BEGIN;
- INSERT OR IGNORE INTO metrics
-  (state, action, parameter, element, mutator, dataset, count)
-  VALUES
-  ("state1", "action1", "parameter1", "element1", "mutator1", "", 0);
- UPDATE metrics SET count = count + 1 WHERE
-  state = "state1" AND
-  action = "action1" AND
-  parameter = "parameter1" AND
-  element = "element1" AND
-  mutator = "mutator1" AND
-  dataset = "";
-COMMIT;
-SELECT * FROM metrics;
-*/
-
-		static string add_sample = @"
-BEGIN;
- INSERT OR IGNORE INTO metrics
-  (state, action, parameter, element, mutator, dataset, count)
-  VALUES
-  (:state, :action, :parameter, :element, :mutator, :dataset, 0);
- UPDATE metrics SET count = count + 1 WHERE
-  state = :state AND
-  action = :action AND
-  parameter = :parameter AND
-  element = :element AND
-  mutator = :mutator AND
-  dataset = :dataset;
-COMMIT;";
-
 
 		private SQLiteConnection db;
 		private SQLiteCommand add_cmd;
@@ -110,7 +125,7 @@ COMMIT;";
 			System.IO.Directory.CreateDirectory(dir);
 			var path = System.IO.Path.Combine(dir, fileName);
 
-			db = new SQLiteConnection("data source=\"" + path + "\"");
+			db = new SQLiteConnection("Data Source=\"" + path + "\";Foreign Keys=True");
 			db.Open();
 
 			var cmd = new SQLiteCommand(db);
@@ -122,13 +137,13 @@ COMMIT;";
 			cmd.ExecuteNonQuery();
 
 			add_cmd = new SQLiteCommand(db);
-			add_cmd.CommandText = add_sample;
-			add_cmd.Parameters.Add("state", System.Data.DbType.String);
-			add_cmd.Parameters.Add("action", System.Data.DbType.String);
-			add_cmd.Parameters.Add("parameter", System.Data.DbType.String);
-			add_cmd.Parameters.Add("element", System.Data.DbType.String);
-			add_cmd.Parameters.Add("mutator", System.Data.DbType.String);
-			add_cmd.Parameters.Add("dataset", System.Data.DbType.String);
+			add_cmd.Parameters.Add(new SQLiteParameter("state"));
+			add_cmd.Parameters.Add(new SQLiteParameter("action"));
+			add_cmd.Parameters.Add(new SQLiteParameter("parameter"));
+			add_cmd.Parameters.Add(new SQLiteParameter("element"));
+			add_cmd.Parameters.Add(new SQLiteParameter("mutator"));
+			add_cmd.Parameters.Add(new SQLiteParameter("dataset"));
+			add_cmd.Parameters.Add(new SQLiteParameter("id"));
 		}
 
 		protected override void Engine_TestFinished(RunContext context)
@@ -159,6 +174,7 @@ COMMIT;";
 
 		protected override void MutationStrategy_Mutating(string elementName, string mutatorName)
 		{
+			//sample.Parameter = "";
 			sample.Element = elementName;
 			sample.Mutator = mutatorName;
 
@@ -166,16 +182,45 @@ COMMIT;";
 				OnSample(sample);
 		}
 
+		protected object GetForeignKey(string table, string name)
+		{
+			var cmd = new SQLiteCommand(db);
+			cmd.Parameters.AddWithValue("name", name);
+			cmd.CommandText = "select id from {0} where name = :name;".Fmt(table);
+			object id = cmd.ExecuteScalar();
+			if (id == null)
+			{
+				cmd.CommandText = "insert into {0} (name) values (:name); select last_insert_rowid();".Fmt(table);
+				id = cmd.ExecuteScalar();
+			}
+			return id;
+		}
+
 		protected virtual void OnSample(Sample s)
 		{
-			add_cmd.Parameters["state"].Value = s.State;
-			add_cmd.Parameters["action"].Value = s.Action;
-			add_cmd.Parameters["parameter"].Value = s.Parameter ?? "";
-			add_cmd.Parameters["element"].Value = s.Element;
-			add_cmd.Parameters["mutator"].Value = s.Mutator;
-			add_cmd.Parameters["dataset"].Value = s.DataSet ?? "";
-
+			add_cmd.CommandText = "BEGIN;";
 			add_cmd.ExecuteNonQuery();
+
+			add_cmd.Parameters["state"].Value = GetForeignKey("states", s.State);
+			add_cmd.Parameters["action"].Value =  GetForeignKey("actions", s.Action);
+			add_cmd.Parameters["parameter"].Value =  GetForeignKey("parameters", s.Parameter ?? "");
+			add_cmd.Parameters["element"].Value =  GetForeignKey("elements", s.Element);
+			add_cmd.Parameters["mutator"].Value =  GetForeignKey("mutators", s.Mutator);
+			add_cmd.Parameters["dataset"].Value =  GetForeignKey("datasets", s.DataSet ?? "");
+
+			add_cmd.CommandText = "select id from metrics where state = :state and action = :action and parameter = :parameter and element = :element and mutator = :mutator and dataset = :dataset";
+			object sample_id = add_cmd.ExecuteScalar();
+			if (sample_id == null)
+			{
+				add_cmd.CommandText = "insert into metrics (state, action, parameter, element, mutator, dataset, count) values (:state, :action, :parameter, :element, :mutator, :dataset, 1); COMMIT;";
+				add_cmd.ExecuteNonQuery();
+			}
+			else
+			{
+				add_cmd.Parameters["id"].Value = sample_id;
+				add_cmd.CommandText = "update metrics set count = count + 1 where id = :id; COMMIT;";
+				add_cmd.ExecuteNonQuery();
+			}
 		}
 	}
 }
