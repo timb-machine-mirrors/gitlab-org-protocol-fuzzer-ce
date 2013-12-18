995190942e4a83ef4aa042d98364e1061ad546a4
matt <matt@dejavusecurity.com>
2013-12-17 17:55:06 -0800

    Peach Farm: more monitor download fixes

--- PeachFarm/PeachFarmMonitor-4.5/PeachFarmMonitor/GetJobOutput.aspx.cs
+++ PeachFarm/PeachFarmMonitor-4.5/PeachFarmMonitor/GetJobOutput.aspx.cs
@@ -10,6 +10,10 @@ using System.Web.UI.WebControls;
 using PeachFarm.Common.Mongo;
 using PeachFarm.Common;
 using PeachFarmMonitor.Configuration;
+using Ionic.Zip;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+//using System.IO.Compression;
 
 namespace PeachFarmMonitor
 {
@@ -41,29 +45,141 @@ namespace PeachFarmMonitor
             if (String.IsNullOrEmpty(filepath))
             {
               string temppath = Path.GetTempPath();
-              FileWriter.DumpFiles(monitorconfig.MongoDb.ConnectionString, temppath, job, ignoreextensions);
-              string zippath = ZipWriter.GetZip(job, temppath);
-              ((List<string>)Session["tempfiles"]).Add(zippath);
-							((List<string>)Session["tempfiles"]).Add(Path.Combine(temppath, job.JobFolder));
 
-							WriteFileToResponse(zippath, job.JobFolder + ".zip");
-            }
+
+							MongoServer server = new MongoClient(monitorconfig.MongoDb.ConnectionString).GetServer();
+							MongoDatabase db = server.GetDatabase(MongoNames.Database);
+							string jobfolder = job.JobFolder + "*";
+							var query = Query.Matches("filename", new MongoDB.Bson.BsonRegularExpression(jobfolder, "i"));
+							var files = db.GridFS.Find(query);
+
+							Response.Clear();
+
+							Response.BufferOutput = false;
+							Response.ContentType = "application/zip";
+							Response.AddHeader("Content-Disposition", "attachment; filename=" + job.JobFolder + ".zip");
+
+
+							using (var zip = new ZipFile())
+							{
+								zip.CompressionLevel = Ionic.Zlib.CompressionLevel.None;
+								zip.UseZip64WhenSaving = Zip64Option.Always;
+								zip.BufferSize = 4 * 1024 * 1024;
+
+								foreach (var file in files)
+								{
+									bool ignore = ((ignoreextensions != null) && (ignoreextensions.Contains(Path.GetExtension(file.Name))));
+									if (!ignore)
+									{
+										if (zip.ContainsEntry(file.Name))
+											continue;
+										
+										zip.AddEntry(file.Name, db.GridFS.OpenRead(file.Name));
+									}
+								}
+								zip.Save(Response.OutputStream);
+							}
+
+						}
             else
             {
 							if (DatabaseHelper.GridFSFileExists(filepath, monitorconfig.MongoDb.ConnectionString) == false)
 							{
 								filepath = filepath.Replace('\\','/');
 							}
-              var temppath = GetTempFile();
-              DatabaseHelper.DownloadFromGridFS(temppath, filepath, monitorconfig.MongoDb.ConnectionString);
-              string filename = Path.GetFileName(filepath);
 
-							WriteFileToResponse(temppath, filename, "text/plain");
+							string filename = Path.GetFileName(filepath);
+
+							MongoServer server = new MongoClient(monitorconfig.MongoDb.ConnectionString).GetServer();
+							MongoDatabase db = server.GetDatabase(MongoNames.Database);
+							using (var stream = db.GridFS.OpenRead(filepath))
+							{
+								WriteStreamToResponse(stream, filename);
+							}
+							//server.Disconnect();
+							
+							
+							
+							
+
             }
           }
         }
       }
     }
+
+		private void WriteStreamToResponse(Stream stream, string pathtobrowser, string contentType = "application/octet-stream")
+		{
+			//System.IO.Stream iStream = null;
+
+			// Buffer to read 10K bytes in chunk:
+			byte[] buffer = new Byte[10000];
+
+			// Length of the file:
+			int length;
+
+			// Total bytes to read:
+			long dataToRead;
+
+			// Identify the file to download including its path.
+			//string filepath = "DownloadFileName";
+
+			// Identify the file name.
+			//string filename = System.IO.Path.GetFileName(filepath);
+
+			try
+			{
+				// Open the file.
+				//iStream = new System.IO.FileStream(filepath, System.IO.FileMode.Open,
+				//			System.IO.FileAccess.Read, System.IO.FileShare.Read);
+
+
+				// Total bytes to read:
+				dataToRead = stream.Length;
+
+				Response.ContentType = contentType;
+				Response.AddHeader("Content-Disposition", "attachment; filename=" + pathtobrowser);
+
+				// Read the bytes.
+				while (dataToRead > 0)
+				{
+					// Verify that the client is connected.
+					if (Response.IsClientConnected)
+					{
+						// Read the data in buffer.
+						length = stream.Read(buffer, 0, 10000);
+
+						// Write the data to the current output stream.
+						Response.OutputStream.Write(buffer, 0, length);
+
+						// Flush the data to the HTML output.
+						Response.Flush();
+
+						buffer = new Byte[10000];
+						dataToRead = dataToRead - length;
+					}
+					else
+					{
+						//prevent infinite loop if user disconnects
+						dataToRead = -1;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				// Trap the error, if any.
+				Response.Write("Error : " + ex.Message);
+			}
+			finally
+			{
+				//if (stream != null)
+				//{
+				//	//Close the file.
+				//	stream.Close();
+				//}
+				//Response.Close();
+			}
+		}
 
 		private void WriteFileToResponse(string filepath, string pathtobrowser, string contentType = "application/octet-stream")
 		{
@@ -134,7 +250,7 @@ namespace PeachFarmMonitor
 					//Close the file.
 					iStream.Close();
 				}
-				Response.Close();
+				//Response.Close();
 			}
 		}
   }
