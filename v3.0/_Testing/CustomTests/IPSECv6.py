1bcf3a4c8d2921642d56749f059bf993ead71845
jordyn <jordyn@dejavusecurity.com>
2014-01-23 12:14:30 -0800

    Fixed ipsec test to run only on linux

--- v3.0/_Testing/CustomTests/IPSECv6.py
+++ v3.0/_Testing/CustomTests/IPSECv6.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 test(name="IPSECv6",
-		test="Default")
-    
+		test="Default",
+    platform="linux")
+
 test(name="IPSECv6",
-		test="AH")
+		test="AH",
+    platform="linux")
