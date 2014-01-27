c9c98af0f202dac16ae01bb7593d43c232ab3c4e
jordyn <jordyn@dejavusecurity.com>
2014-01-27 13:49:36 -0800

    added test script for snmp linux

--- v3.0/_Testing/CustomTests/SNMP.py
+++ v3.0/_Testing/CustomTests/SNMP.py
@@ -4,4 +4,17 @@ test(name="SNMP",
     test="Default")
 
 test(name="SNMP",
-    test="Server")
+    test="Server",
+    platform="linux",
+    setup=setup)
+
+test(name="SNMP",
+    test="Server",
+    platform="windows")
+
+test(name="SNMP",
+    test="Server",
+    platform="osx")
+
+def setup(ctx):
+    os.system("sudo apt-get install snmp -y")
