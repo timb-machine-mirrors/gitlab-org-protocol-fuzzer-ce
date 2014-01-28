36586c56c1789c1d56c63acc1cf27c95375c19c5
jordyn <jordyn@dejavusecurity.com>
2014-01-28 10:13:29 -0800

    Added PORT support for ftp client

--- v3.0/_Common/Models/Net/ftpFixup.py
+++ v3.0/_Common/Models/Net/ftpFixup.py
@@ -1,19 +1,15 @@
+class FtpPortToHexFixup:
+  """
+      FTP Python Fixup.
+      Converts the port number of an IP/Port combination to hex.
+      Input: 127.0.0.1,31337 Outputs: 127,0,0,1,122,105
+  """
 
-#
-# Example Python fixup for Peach 3
-#
-# Authors:
-# Michael Eddingtion
-#
-
-# -- Example returning a string ---------------
-
-class PortCommandFormatFixup:
   def __init__(self, parent):
     self._parent = parent
 
   def fixup(self, element):
-    splt = element.InternalValue.split(",")
+    splt = str(element.InternalValue).split(",")
     ip = splt[0].replace(".", ",")
     port = hex(int(splt[1])).split("0x")[1]
     p1 = port[:2]
@@ -22,7 +18,30 @@ class PortCommandFormatFixup:
       port = str(int(p1,16))+","+str(int(p2,16))
     else:
       port = str(int(p1,16))
+    return ip + "," + port
 
+# --------------------------------------------
+
+class FtpHexPortToDecimalFixup:
+  """
+      FTP Python Fixup.
+      Parses port for the FTP IP/Hex port combination as a decimal
+      Input: 127,0,0,1,122,105 Outputs: 127.0.0.1,31337
+  """
+
+  def __init__(self, parent):
+    self._parent = parent
+
+  def fixup(self, element):
+    splt = str(element.InternalValue).split(",")
+    ip = splt[0].replace(".", ",")
+    port = hex(int(splt[1])).split("0x")[1]
+    p1 = port[:2]
+    p2 = port[2:]
+    if p1 is not '':
+      port = str(int(p1,16))+","+str(int(p2,16))
+    else:
+      port = str(int(p1,16))
     return ip + "," + port
 
 # --------------------------------------------
