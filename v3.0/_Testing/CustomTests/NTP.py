822dad8bd804f288e1e28a0ca7883fa733119002
Byron Lantzsch <byron@dejavusecurity.com>
2014-01-28 17:40:11 -0800

    disable osx process that interferes with ntp pit

---
+++ v3.0/_Testing/CustomTests/NTP.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import Popen, PIPE
+
+def setup(ctx):
+    if "osx" in get_platform():
+        ctx.null = open('/dev/null', 'r+')
+        ctx.disabler = Popen('systemsetup', '-setusingnetworktime', 'off'],
+                           stdin=ctx.null, stdout=ctx.null, stderr=ctx.null)
+        ctx.disabler.wait()
+        
+
+def teardown(ctx):
+    pass
+
+test(name="NTP",
+     setup=setup,
+     teardown=teardown)
