5fee6cb1a9e5e3e680c7bce7009cf9c2a1834b8e
jordyn <jordyn@dejavusecurity.com>
2014-01-28 17:27:07 -0800

    Added ftp port mode for both client and server

--- v3.0/_Testing/CustomTests/NTP.py
+++ v3.0/_Testing/CustomTests/NTP.py
@@ -6,7 +6,7 @@ from subprocess import Popen, PIPE
 def setup(ctx):
     if "osx" in get_platform():
         ctx.null = open('/dev/null', 'r+')
-        ctx.disabler = Popen('systemsetup', '-setusingnetworktime', 'off'],
+        ctx.disabler = Popen(['systemsetup', '-setusingnetworktime', 'off'],
                            stdin=ctx.null, stdout=ctx.null, stderr=ctx.null)
         ctx.disabler.wait()
         
