433d7d28360eaa0f7df6b10c2eb0b133c07084a0
jordyn <jordyn@dejavusecurity.com>
2014-01-29 12:03:29 -0800

    Added ftp passive server support

--- v3.0/_Common/Models/Net/ftpFixup.py
+++ v3.0/_Common/Models/Net/ftpFixup.py
@@ -10,6 +10,8 @@ class FtpPortToHexFixup:
 
   def fixup(self, element):
     splt = str(element.InternalValue).split(",")
+    if len(splt) > 4:
+      return str(element.InternalValue)
     ip = splt[0].replace(".", ",")
     port = hex(int(splt[1])).split("0x")[1]
     p1 = port[:2]
@@ -18,30 +20,7 @@ class FtpPortToHexFixup:
       port = str(int(p1,16))+","+str(int(p2,16))
     else:
       port = str(int(p1,16))
-    return ip + "," + port
-
-# --------------------------------------------
-
-class FtpHexPortToDecimalFixup:
-  """
-      FTP Python Fixup.
-      Parses port for the FTP IP/Hex port combination as a decimal
-      Input: 127,0,0,1,122,105 Outputs: 127.0.0.1,31337
-  """
-
-  def __init__(self, parent):
-    self._parent = parent
-
-  def fixup(self, element):
-    splt = str(element.InternalValue).split(",")
-    ip = splt[0].replace(".", ",")
-    port = hex(int(splt[1])).split("0x")[1]
-    p1 = port[:2]
-    p2 = port[2:]
-    if p1 is not '':
-      port = str(int(p1,16))+","+str(int(p2,16))
-    else:
-      port = str(int(p1,16))
+    print port
     return ip + "," + port
 
 # --------------------------------------------
