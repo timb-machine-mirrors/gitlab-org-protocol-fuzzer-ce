97a9166c1054d1376929a5f3df05cf0b206ad121
Seth Hinze <seth@dejavusecurity.com>
2014-03-10 17:35:27 -0700

    Getting godel to use python instead of ocl.

--- Godel.Core/ExtendPeach.cs
+++ Godel.Core/ExtendPeach.cs
@@ -4,23 +4,167 @@ using System.Linq;
 using System.Text;
 using Peach.Core;
 using Peach.Core.Dom;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using IronPython;
+using IronPython.Hosting;
+using System.IO;
 
 namespace Godel.Core
 {
 	public class ExtendPeach
 	{
-		public ExtendAction ExtendAction { get; set; }
-		public ExtendState ExtendState { get; set; }
-		public ExtendStateModel ExtendStateModel { get; set; }
-		public RunContext Context { get; set; }
+		private RunContext Context { get; set; }
+		private NamedCollection<GodelContext> Expressions { get; set; }
+		private ScriptEngine Engine { get; set; }
+		private StateModel OriginalStateModel { get; set; }
 
 		public ExtendPeach(RunContext context)
 		{
-			context.stateStore["OclContexts"] = new Dictionary<string, OCL.OclContext>();
 			Context = context;
-			ExtendAction = new ExtendAction(context);
-			ExtendState = new ExtendState(context);
-			ExtendStateModel = new ExtendStateModel(context);
+
+			Context.engine.TestStarting += engine_TestStarting;
+		}
+
+		GodelContext GetExpr(params string[] names)
+		{
+			var name = string.Join(".", names);
+			GodelContext ret;
+			if (Expressions.TryGetValue(name, out ret))
+				return ret;
+			return null;
+		}
+
+		void engine_TestStarting(RunContext context)
+		{
+			var dom = context.dom as Godel.Core.Dom;
+			if (dom == null || dom.godel.Count == 0)
+			{
+				Context.engine.TestFinished -= engine_TestFinished;
+				return;
+			}
+
+			// Create the engine
+			Engine = IronPython.Hosting.Python.CreateEngine();
+
+			// Add any specified paths to our engine.
+			ICollection<string> enginePaths = Engine.GetSearchPaths();
+			foreach (string path in Peach.Core.Scripting.Paths)
+				enginePaths.Add(path);
+			foreach (string path in ClassLoader.SearchPaths)
+				enginePaths.Add(Path.Combine(path, "Lib"));
+			Engine.SetSearchPaths(enginePaths);
+
+			// Import any modules
+			var modules = new Dictionary<string, object>();
+			foreach (string import in Peach.Core.Scripting.Imports)
+				if (!modules.ContainsKey(import))
+					modules.Add(import, Engine.ImportModule(import));
+
+			// Create the global scope
+			var scope = Engine.CreateScope();
+
+			// Add the imports to the global scope
+			foreach (var kv in modules)
+				scope.SetVariable(kv.Key, kv.Value);
+
+
+			foreach (var item in dom.godel)
+			{
+				// Pre-compile all the expressions
+				item.OnTestStarting(Context, scope);
+			}
+
+			Expressions = dom.godel;
+
+			Peach.Core.Dom.StateModel.Starting += StateModel_Starting;
+			Peach.Core.Dom.StateModel.Finished += StateModel_Finished;
+			Peach.Core.Dom.State.Starting += State_Starting;
+			Peach.Core.Dom.State.Finished += State_Finished;
+			Peach.Core.Dom.Action.Starting += Action_Starting;
+			Peach.Core.Dom.Action.Finished += Action_Finished;
+		}
+
+
+		void engine_TestFinished(RunContext context)
+		{
+			foreach (var item in Expressions)
+			{
+				item.OnTestFinished();
+			}
+
+			OriginalStateModel = null;
+			Expressions = null;
+			Engine = null;
+
+			Peach.Core.Dom.StateModel.Starting -= StateModel_Starting;
+			Peach.Core.Dom.StateModel.Finished -= StateModel_Finished;
+			Peach.Core.Dom.State.Starting -= State_Starting;
+			Peach.Core.Dom.State.Finished -= State_Finished;
+			Peach.Core.Dom.Action.Starting -= Action_Starting;
+			Peach.Core.Dom.Action.Finished -= Action_Finished;
+		}
+
+		void StateModel_Starting(Peach.Core.Dom.StateModel model)
+		{
+			// Keep a copy of the original for 'pre' variable in the post 
+			OriginalStateModel = ObjectCopier.Clone(model);
+
+			// StateModel.parent and State.parent are not copied, so fix them up
+			foreach (var state in OriginalStateModel.states)
+			{
+				state.parent = OriginalStateModel;
+
+				foreach (var action in state.actions)
+				{
+					action.parent = state;
+				}
+			}
+
+			var expr = GetExpr(model.name);
+			if (expr != null)
+				expr.Pre(model);
+		}
+
+		void StateModel_Finished(Peach.Core.Dom.StateModel model)
+		{
+			var expr = GetExpr(model.name);
+			if (expr != null)
+				expr.Post(model, OriginalStateModel);
+		}
+
+		void State_Starting(Peach.Core.Dom.State state)
+		{
+			var expr = GetExpr(state.parent.name, state.name);
+			if (expr != null)
+				expr.Pre(state);
+		}
+
+		void State_Finished(Peach.Core.Dom.State state)
+		{
+			var expr = GetExpr(state.parent.name, state.name);
+			if (expr != null)
+			{
+				var pre = OriginalStateModel.states[state.name];
+				expr.Post(state, pre);
+			}
+		}
+
+		void Action_Starting(Peach.Core.Dom.Action action)
+		{
+			var expr = GetExpr(action.parent.parent.name, action.parent.name, action.name);
+			if (expr != null)
+				expr.Pre(action);
+		}
+
+		void Action_Finished(Peach.Core.Dom.Action action)
+		{
+			var expr = GetExpr(action.parent.parent.name, action.parent.name, action.name);
+			if (expr != null)
+			{
+				var pre = OriginalStateModel.states[action.parent.name].actions[action.name];
+				expr.Post(action, pre);
+			}
 		}
 	}
 }
--- Godel.Tests/BasicTests.cs
+++ Godel.Tests/BasicTests.cs
@@ -1,16 +1,157 @@
-﻿//using System;
+﻿using System;
+using System.IO;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Text;
-//using NUnit.Framework;
+
+using NUnit.Framework;
 //using NUnit.Framework.Constraints;
-//using Peach.Core;
+
+using Peach.Core;
 //using Peach.Core.Dom;
 //using Peach.Core.Analyzers;
 //using Peach.Core.IO;
 
-//namespace Godel.Tests
-//{
+namespace Godel.Tests
+{
+	[TestFixture]
+	class BasicTests
+	{
+		[Test]
+		public void ParserTest()
+		{
+			string xml = @"
+<Peach>
+
+	<DataModel name='DM'>
+		<String value='Hello World'/>
+	</DataModel>
+
+	<Godel name='BasicContext' inv='1 == 1' pre='2 == 2' post='3 == 3'/>
+
+	<Godel name='DerivedContext' ref='BasicContext' post='4 == 4'/>
+
+	<Godel name='ControlOnly' ref='DerivedContext' controlOnly='true'/>
+
+	<StateModel name='SM' initialState='Initial'>
+		<Godel ref='DerivedContext' pre='5 == 5'/>
+
+		<State name='Initial'>
+			<Godel ref='BasicContext'/>
+
+			<Action type='output'>
+				<Godel ref='ControlOnly'/>
+				<DataModel ref='DM'/>
+			</Action>
+
+			<Action type='output'>
+				<Godel inv='True == True'/>
+				<DataModel ref='DM'/>
+			</Action>
+
+			<Action type='output'>
+				<Godel ref='DerivedContext' post='6 == 6'/>
+				<DataModel ref='DM'/>
+			</Action>
+
+			<Action type='output'>
+				<Godel ref='ControlOnly' controlOnly='false'/>
+				<DataModel ref='DM'/>
+			</Action>
+		</State>
+
+	</StateModel>
+
+	<Test name='Default'>
+		<StateModel ref='SM'/>
+		<Publisher class='Null'/>
+	</Test>
+
+</Peach>
+";
+			var e = new Engine(null);
+
+			var parser = new Godel.Core.GodelPitParser();
+
+			var dom = parser.asParser(null, new MemoryStream(Encoding.ASCII.GetBytes(xml))) as Godel.Core.Dom;
+
+			Assert.NotNull(dom);
+
+			Assert.AreEqual(6, dom.godel.Count);
+
+			Assert.AreEqual(dom.godel[0].name, "SM.Initial.Action");
+			Assert.AreEqual(dom.godel[0].controlOnly, true);
+			Assert.AreEqual(dom.godel[0].inv, "1 == 1");
+			Assert.AreEqual(dom.godel[0].pre, "2 == 2");
+			Assert.AreEqual(dom.godel[0].post, "4 == 4");
+
+			Assert.AreEqual(dom.godel[1].name, "SM.Initial.Action_1");
+			Assert.AreEqual(dom.godel[1].controlOnly, false);
+			Assert.AreEqual(dom.godel[1].inv, "True == True");
+			Assert.AreEqual(dom.godel[1].pre, null);
+			Assert.AreEqual(dom.godel[1].post, null);
+
+			Assert.AreEqual(dom.godel[2].name, "SM.Initial.Action_2");
+			Assert.AreEqual(dom.godel[2].controlOnly, false);
+			Assert.AreEqual(dom.godel[2].inv, "1 == 1");
+			Assert.AreEqual(dom.godel[2].pre, "2 == 2");
+			Assert.AreEqual(dom.godel[2].post, "6 == 6");
+
+			Assert.AreEqual(dom.godel[3].name, "SM.Initial.Action_3");
+			Assert.AreEqual(dom.godel[3].controlOnly, false);
+			Assert.AreEqual(dom.godel[3].inv, "1 == 1");
+			Assert.AreEqual(dom.godel[3].pre, "2 == 2");
+			Assert.AreEqual(dom.godel[3].post, "4 == 4");
+
+			Assert.AreEqual(dom.godel[4].name, "SM.Initial");
+			Assert.AreEqual(dom.godel[4].controlOnly, false);
+			Assert.AreEqual(dom.godel[4].inv, "1 == 1");
+			Assert.AreEqual(dom.godel[4].pre, "2 == 2");
+			Assert.AreEqual(dom.godel[4].post, "3 == 3");
+
+			Assert.AreEqual(dom.godel[5].name, "SM");
+			Assert.AreEqual(dom.godel[5].controlOnly, false);
+			Assert.AreEqual(dom.godel[5].inv, "1 == 1");
+			Assert.AreEqual(dom.godel[5].pre, "5 == 5");
+			Assert.AreEqual(dom.godel[5].post, "4 == 4");
+		}
+
+		[Test]
+		public void SimpleInvariant()
+		{
+			string xml = @"
+<Peach>
+
+	<DataModel name='DM'>
+		<String value='Hello World'/>
+	</DataModel>
+
+	<StateModel name='SM' initialState='Initial'>
+		<State name='Initial'>
+			<Action type='output'>
+				<Godel inv='self != None'/>
+				<DataModel ref='DM'/>
+			</Action>
+		</State>
+	</StateModel>
+
+	<Test name='Default'>
+		<StateModel ref='SM'/>
+		<Publisher class='Null'/>
+	</Test>
+</Peach>
+";
+			var e = new Engine(null);
+			var epeach = new Godel.Core.ExtendPeach(e.context);
+			var parser = new Godel.Core.GodelPitParser() { ExtendPeach = epeach };
+			var dom = parser.asParser(null, new MemoryStream(Encoding.ASCII.GetBytes(xml))) as Godel.Core.Dom;
+			var config = new RunConfiguration();
+			config.singleIteration = true;
+			e.startFuzzing(dom, config);
+		}
+	}
+}
+
 //    [TestFixture]
 //    class BasicTests : DataModelCollector
 //    {
