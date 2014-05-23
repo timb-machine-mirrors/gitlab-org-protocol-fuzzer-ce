ca90df63742f621d69b14bd447610d21054dbdfa
Byron Lantzsch <byron@dejavusecurity.com>
2014-05-23 11:42:07 -0700

    add testing config and some test code

--- Peach.Enterprise.Web.TS/test/karma.conf.js
+++ Peach.Enterprise.Web.TS/test/karma.conf.js
@@ -4,11 +4,21 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
+      // libs, includes, etc
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/jQuery/dist/jquery.js',
+      'bower_components/ng-grid/build/ng-grid.js',
+      'Content/js/line-chart.min.js',
+      'Scripts/angular-poller.js',
+      'Scripts/angular-local-storage.js',
+
+      // our code, application and test
       'app/js/**/*.js',
+      'app/ts/**/*.js',
       'test/unit/**/*.js'
     ],
 
