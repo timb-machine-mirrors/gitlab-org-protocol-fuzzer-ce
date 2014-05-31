2012a63b751847d3d51a63540362263b7dfcd312
matt <matt@dejavusecurity.com>
2014-05-30 17:13:03 -0700

    peach web work, moving stuff around, fixing tsc-args

---
+++ Peach.Enterprise.Web.TS/app/ts/Controllers/main.ts
@@ -0,0 +1,86 @@
+﻿/// <reference path="../Models/wizard.ts" />
+/// <reference path="../Models/peach.ts" />
+/// <reference path="../../../Scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../../scripts/typings/angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
+
+module DashApp {
+
+	import P = Models.Peach;
+
+	export class MainController {
+		private storage: ng.ILocalStorageService;
+		private peachService: Services.IPeachService;
+		private modal: ng.ui.bootstrap.IModalService;
+
+		//#region Public Properties
+		private _pit: P.Pit;
+		public get pit(): P.Pit {
+			if (this._pit != undefined)
+				return this._pit;
+			else
+				return this.storage.get(StorageStrings.Pit);
+		}
+
+		public set pit(pit: P.Pit) {
+			if (this._pit != pit) {
+				this._pit = pit;
+				this.storage.set(StorageStrings.Pit, pit);
+			}
+		}
+
+		public location: ng.ILocationService;
+		public job: P.Job;
+		//#endregion
+
+
+		static $inject = ["$scope", "$resource", "$location", "$modal", "poller", "peachService", "localStorageService"];
+
+		constructor($scope: ViewModelScope, $resource, $location: ng.ILocationService, $modal: ng.ui.bootstrap.IModalService, poller, peachService: Services.IPeachService, localStorageService: ng.ILocalStorageService) {
+			$scope.vm = this;
+
+			this.modal = $modal;
+			this.peachService = peachService;
+			this.location = $location;
+			this.storage = localStorageService;
+			this.storage.remove("pit");
+
+			var jobResource = peachService.GetJob();
+
+			var jobPoller = poller.get(jobResource);
+
+			jobPoller.promise.then(null, null, function (data) {
+				this.job = data;
+			});
+
+			this.showPitSelector();
+		}
+
+		public showPitSelector() {
+			this.modal.open({
+				templateUrl: "/partials/pitlibrary.html",
+				keyboard: false,
+				backdrop: 'static',
+				controller: PitLibraryController,
+				resolve: {
+					peachsvc: () => {
+						return this.peachService;
+					}
+				}
+			}).result.then((pitUrl: string) => {
+				this.peachService.GetPit(pitUrl, (data: P.Pit) => { this.pit = data });
+				});
+		}
+	}
+
+	export interface ViewModelScope extends ng.IScope {
+		vm: any;
+	}
+
+	export class StorageStrings {
+		static Pit = "pit";
+		static FaultMonitors = "faultMonitors";
+		static DataMonitors = "dataMonitors";
+		static AutoMonitors = "autoMonitors";
+		static PitDefines = "pitDefines";
+	}
+}
---
+++ Peach.Enterprise.Web.TS/app/ts/Services/peach.ts
@@ -0,0 +1,92 @@
+﻿/// <reference path="..\..\..\Scripts\typings\angularjs\angular.d.ts" />
+/// <reference path="..\..\..\Scripts\typings\angularjs\angular-resource.d.ts" />
+
+module DashApp.Services {
+	import P = DashApp.Models.Peach;
+
+	export interface IPeachService {
+		GetDefines(pitUrl: string): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetFaultQA(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetDataQA(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetAutoQA(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+
+		GetJob(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetJobFaults(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetPit(id: number, success: (data: P.Pit) => void): void;
+		GetPit(url: string, success: (data: P.Pit) => void): void;
+
+		TestConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		GetLibraries(success: (data: P.PitLibrary[]) => void): void;
+
+		//PostPitConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+		//PostMonitorConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>>;
+
+	}
+
+
+	export class PeachService implements IPeachService {
+		private resource: ng.resource.IResourceService;
+		private http: ng.IHttpService;
+
+		constructor($resource: ng.resource.IResourceService, $http: ng.IHttpService) {
+			this.resource = $resource;
+			this.http = $http;
+		}
+
+		public GetFaultQA(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("../testdata/wizard_qa_fault.json");
+		}
+
+		public GetDefines(pitUrl: string): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource(pitUrl + "/config");
+		}
+
+		public GetDataQA(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("../testdata/wizard_qa_data.json");
+		}
+
+		public GetAutoQA(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("../testdata/wizard_qa_auto.json");
+		}
+
+		public GetJob(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource('/p/jobs/1');
+		}
+
+		public GetJobFaults(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("/p/faults");
+		}
+		 
+		public GetPit(IdOrUrl: any, success: (data: P.Pit) => void): void { 
+			if (typeof IdOrUrl == "number") {
+				this.http.get("/p/pits/" + parseInt(IdOrUrl)).success((data) => success(<P.Pit>data));
+			}
+			else if (typeof IdOrUrl == "string") {
+				this.http.get(IdOrUrl).success((data) => success(<P.Pit>data));
+			}
+			else {
+				throw new Error("GetPit: Argument 0 is of an incompatible type.");
+			}
+		}
+
+
+		public PostPitConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("/p/conf/wizard/config");
+		}
+
+		public PostMonitorConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("/p/conf/wizard/monitors");
+		}
+
+		public TestConfiguration(): ng.resource.IResourceClass<ng.resource.IResource<any>> {
+			return this.resource("/testdata/test_results.json");
+		}
+
+		public GetLibraries(success: (data: P.PitLibrary[]) => void): void {
+			this.http.get("/p/libraries").success((data) => {
+				var libs: P.PitLibrary[] = <P.PitLibrary[]>data;
+				success(libs);
+			});
+		}
+	}
+} 
