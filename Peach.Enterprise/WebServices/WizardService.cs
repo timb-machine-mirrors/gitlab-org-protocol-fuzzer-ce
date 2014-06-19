846f70291e197a8b607f6d797e477ce118fbf424
Seth Hinze <seth@dejavusecurity.com>
2014-06-18 18:58:12 -0700

    Implementing save monitors.

--- Peach.Enterprise/WebServices/WizardService.cs
+++ Peach.Enterprise/WebServices/WizardService.cs
@@ -51,37 +51,27 @@ namespace Peach.Enterprise.WebServices
 			if (pit.Locked)
 				return HttpStatusCode.Forbidden;
 
-			var fileName = pit.Versions[0].Files[0].Name + ".config";
-			var defines = PitDefines.Parse(fileName);
-
-			// For now, read in the current defines off disk and
-			// apply any applicable changes. We don't currently expect
-			// this api to add new defines or delete old defines.
-			foreach (var item in cfg.Config)
-			{
-				foreach (var def in defines.Where(d => d.Key == item.Key))
-					def.Value = item.Value;
-			}
-
-			var final = new PitDefines()
-			{
-				Platforms = new List<PitDefines.Collection>(new[] {
-					new PitDefines.All()
-					{
-						Defines = defines,
-					}
-				}),
-			};
-
-			XmlTools.Serialize(fileName, final);
+			PitDatabase.SaveConfig(pit, cfg.Config);
 
 			return HttpStatusCode.OK;
 		}
 
 		object PostMonitors()
 		{
-			/*var monitors = */this.Bind<PitMonitors>();
-			return HttpStatusCode.EnhanceYourCalm;
+			var monitors = this.Bind<PitMonitors>();
+			var db = new PitDatabase(".");
+			var pit = db.GetPitByUrl(monitors.PitUrl);
+			if (pit == null)
+				return HttpStatusCode.NotFound;
+
+			// Don't allow changing configuration values
+			// of locked pits
+			if (pit.Locked)
+				return HttpStatusCode.Forbidden;
+
+			PitDatabase.SaveMonitors(pit, monitors.Monitors);
+
+			return HttpStatusCode.OK;
 		}
 	}
 }
