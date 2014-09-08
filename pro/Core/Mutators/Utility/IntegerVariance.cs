72cfc2fb0bd515b9374a3716739177f7c9ee977c
Seth Hinze <seth@dejavusecurity.com>
2014-09-08 16:50:02 -0700

    Implementing integer variance base mutator.

--- pro/Core/Mutators/Utility/IntegerVariance.cs
+++ pro/Core/Mutators/Utility/IntegerVariance.cs
@@ -1,48 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-using Peach.Core;
 using Peach.Core.Dom;
 
 namespace Peach.Core.Mutators.Utility
 {
-	public class IntegerVariance
+	public abstract class IntegerVariance : Mutator
 	{
-		long minValue;
-		ulong maxValue;
-		long lValue;
-		ulong ulValue;
+		VarianceGenerator gen;
+
 		bool signed;
-		bool isUlong = false;
+		long value;
+		long min;
+		long max;
 
-		public IntegerVariance(object value, long minValue, ulong maxValue, bool signed)
+		public IntegerVariance(DataElement obj)
 		{
-			this.minValue = minValue;
-			this.maxValue = maxValue;
-			this.signed = signed;
+			GetLimits(obj, out signed, out value, out min, out max);
 
-			if (value is long)
-				lValue = (long)value;
+			if (!signed)
+				gen = new VarianceGenerator((ulong)value, (ulong)min, (ulong)max);
 			else
+				gen = new VarianceGenerator(value, min, max);
+		}
+
+		/// <summary>
+		/// The logger to use.
+		/// </summary>
+		protected abstract NLog.Logger Logger
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Get the minimum and maximum values to generate edge cases for.
+		/// </summary>
+		/// <remarks>
+		/// If value is unsigned, just cast it to a long.
+		/// </remarks>
+		/// <param name="obj">The element this mutator is bound to.</param>
+		/// <param name="signed">Is the number space signed.</param>
+		/// <param name="value">The value to center the variance distribution around.</param>
+		/// <param name="min">The minimum value of the number space.</param>
+		/// <param name="max">The maximum value of the number space.</param>
+		protected abstract void GetLimits(DataElement obj, out bool signed, out long value, out long min, out long max);
+
+		/// <summary>
+		/// Mutate the data element.
+		/// </summary>
+		/// <param name="obj">The element to mutate.</param>
+		/// <param name="value">The value to use when mutating.</param>
+		protected abstract void performMutation(DataElement obj, long value);
+
+		/// <summary>
+		/// Mutate the data element.  This is called when the value to be used
+		/// for mutation is larger than long.MaxValue.
+		/// </summary>
+		/// <param name="obj">The element to mutate.</param>
+		/// <param name="value">The value to use when mutating.</param>
+		protected abstract void performMutation(DataElement obj, ulong value);
+
+		public override uint mutation
+		{
+			get;
+			set;
+		}
+
+		public override int count
+		{
+			get
 			{
-				ulValue = (ulong)value;
-				isUlong = true;
+				return gen.Count;
 			}
 		}
 
-		public object Next(Peach.Core.Random random)
+		public override void sequentialMutation(DataElement obj)
 		{
-			if(isUlong)
+			// sequential is the same as random
+			randomMutation(obj);
+		}
+
+		public override void randomMutation(DataElement obj)
+		{
+			while (true)
 			{
-				ulong range = maxValue - (ulong)minValue;
-				return (ulong) (random.NextGaussian() * (double)range);
-			}
-			else
-			{
-				long range = (long)maxValue - minValue;
-				return (long) (random.NextGaussian() * (double)range);
+				var value = gen.Next(context.Random);
+
+				// If we get our default value, pick again as that
+				// is not really a mutation
+				if (value == this.value)
+					continue;
+
+				// VarianceGenerator gurantees value is between min/max
+				// Promote to ulong if appropriate
+				if (value < 0 && !signed)
+				{
+					var asUlong = unchecked((ulong)value);
+					Logger.Trace("performMutation(value={0}", asUlong);
+					performMutation(obj, asUlong);
+				}
+				else
+				{
+					Logger.Trace("performMutation(value={0}", value);
+					performMutation(obj, value);
+				}
+
+				return;
 			}
 		}
 	}
---
+++ pro/Test/Core/Mutators/Utility/IntegerVarianceTests.cs
@@ -0,0 +1,137 @@
+using NUnit.Framework;
+using Peach.Core.Dom;
+using Peach.Core.Mutators.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peach.Core.Test.Mutators.Utility
+{
+	[TestFixture]
+	class IntegerVarianceTests
+	{
+		class Tester : IntegerVariance
+		{
+			class Strategy : MutationStrategy
+			{
+				uint iteration;
+
+				public Strategy()
+					: base(null)
+				{
+					_context = new RunContext() { config = new RunConfiguration() };
+
+				}
+				public override bool UsesRandomSeed
+				{
+					get { return false; }
+				}
+
+				public override bool IsDeterministic
+				{
+					get { return false; }
+				}
+
+				public override uint Count
+				{
+					get { return 0; }
+				}
+
+				public override uint Iteration
+				{
+					get
+					{
+						return iteration;
+					}
+					set
+					{
+						iteration = value;
+						SeedRandom();
+					}
+				}
+			}
+
+			public Action<long> LongMutation;
+			public Action<ulong> ULongMutation;
+
+			public Tester(Number obj)
+				: base(obj)
+			{
+				this.context = new Strategy();
+			}
+
+			static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+			protected override NLog.Logger Logger
+			{
+				get { return logger; }
+			}
+
+			protected override void GetLimits(DataElement obj, out bool signed, out long value, out long min, out long max)
+			{
+				var num = (Number)obj;
+
+				signed = num.Signed;
+				value = (long)num.DefaultValue;
+				min = (long)num.MinValue;
+				max = (long)num.MaxValue;
+			}
+
+			protected override void performMutation(Dom.DataElement obj, long value)
+			{
+				LongMutation(value);
+			}
+
+			protected override void performMutation(Dom.DataElement obj, ulong value)
+			{
+				ULongMutation(value);
+			}
+		}
+
+		void TestVariance(int size, bool signed, long value, int count)
+		{
+			var num = new Number("num") { length = size, Signed = signed, DefaultValue = new Variant(value) };
+
+			var tester = new Tester(num);
+
+			var totals = new Dictionary<long, int>();
+
+			for (long x = num.MinValue; x <= (long)num.MaxValue; ++x)
+				totals[x] = 0;
+
+			tester.LongMutation = v =>
+			{
+				totals[v] = totals[v] + 1;
+			};
+
+			tester.ULongMutation = v => Assert.Fail();
+
+			// Run 10 times the count
+			for (uint i = 1; i < count; ++i)
+			{
+				tester.context.Iteration = i;
+				tester.randomMutation(null);
+			}
+
+			var sb = new StringBuilder();
+
+			foreach (var kv in totals)
+				if (kv.Value == 0 && kv.Key != value)
+					sb.AppendFormat("{0} ", kv.Key);
+
+			// Make sure after 10x the number space, we hit all the
+			// possible number values.
+			var str = sb.ToString();
+			if (!string.IsNullOrEmpty(str))
+				Assert.Fail("Missed numbers: {0}", str);
+
+			Assert.AreEqual(0, totals[value]);
+		}
+
+		[Test]
+		public void Test()
+		{
+			TestVariance(8, true, 0, 100000);
+		}
+	}
+}
