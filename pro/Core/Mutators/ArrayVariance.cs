c20851f493f05b9e1107ef9fe43ecd42ddc8b2b0
Seth Hinze <seth@dejavusecurity.com>
2014-09-09 13:07:09 -0700

    Implement new array variance mutator.

--- pro/Core/Mutators/ArrayVariance.cs
+++ pro/Core/Mutators/ArrayVariance.cs
@@ -4,112 +4,51 @@
 //
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-using Peach.Core.Mutators.Utility;
-using Peach.Core;
 using Peach.Core.Dom;
-using Peach.Core.IO;
 
-namespace Peach.Enterprise.Mutators
+using NLog;
+
+namespace Peach.Core.Mutators
 {
-	[Mutator("ArrayVarianceMutator")]
+	[Mutator("ArrayVariance")]
 	[Description("Change the length of arrays to count - N to count + N")]
-	//[Hint("ArrayVarianceMutator-N", "Gets N by checking node for hint, or returns default (50).")]
-	public class ArrayVariance : Mutator
+	public class ArrayVariance : Utility.IntegerVariance
 	{
-		int n;
-		int minCount;
-		int maxCount;
-		int currentCount;
-		int arrayCount;
-
-		IntegerVariance variance;
+		static NLog.Logger logger = LogManager.GetCurrentClassLogger();
 
 		public ArrayVariance(DataElement obj)
+			: base(obj)
 		{
-			name = "ArrayVariance";
-			n = getN(obj, 50);
-			arrayCount = ((Peach.Core.Dom.Array)obj).Count;
-			minCount = arrayCount - n;
-			maxCount = arrayCount + n;
-
-			if (minCount < 0)
-				minCount = 0;
-
-			currentCount = minCount;
-
-			variance = new IntegerVariance(arrayCount, ushort.MinValue, ushort.MaxValue, false);
 		}
 
-		protected int MinCount
+		protected override NLog.Logger Logger
 		{
-			get { return minCount; }
-			set { minCount = value; }
-		}
-
-		protected int CurrentCount
-		{
-			get { return currentCount; }
-			set { currentCount = value; }
-		}
-
-		public int getN(DataElement obj, int n)
-		{
-			// check for hint
-			if (obj.Hints.ContainsKey(name + "-N"))
+			get
 			{
-				Hint h = null;
-				if (obj.Hints.TryGetValue(name + "-N", out h))
-				{
-					try
-					{
-						n = Int32.Parse(h.Value);
-					}
-					catch (Exception ex)
-					{
-						throw new PeachException("Expected numerical value for Hint named " + h.Name, ex);
-					}
-				}
+				return logger;
 			}
-
-			return n;
 		}
 
-		public override uint mutation
+		protected override void GetLimits(DataElement obj, out bool signed, out long value, out long min, out long max)
 		{
-			get { return (uint)(currentCount - minCount); }
-			set { currentCount = (int)value + minCount; }
-		}
-
-		public override int count
-		{
-			get { return maxCount - minCount + 1; }
+			signed = false;
+			value = ((Core.Dom.Array)obj).Count;
+			min = uint.MinValue;
+			max = uint.MaxValue;
 		}
 
 		public new static bool supportedDataElement(DataElement obj)
 		{
-			if (obj is Peach.Core.Dom.Array && obj.isMutable)
+			if (obj is Dom.Array && obj.isMutable)
 				return true;
 
 			return false;
 		}
 
-		public override void sequentialMutation(DataElement obj)
+		protected override void performMutation(DataElement obj, long num)
 		{
-			throw new NotSupportedException("TODO - Make this work");
-		}
-
-		public override void randomMutation(DataElement obj)
-		{
-			performMutation(obj, (int)variance.Next(context.Random));
-			obj.mutationFlags = MutateOverride.Default;
-		}
-
-		public void performMutation(DataElement obj, int num)
-		{
-			Core.Dom.Array objAsArray = (Core.Dom.Array)obj;
+			var objAsArray = (Core.Dom.Array)obj;
 
 			if (num < objAsArray.Count)
 			{
@@ -126,10 +65,15 @@ namespace Peach.Enterprise.Mutators
 			{
 				// add some items, but do it by replicating
 				// the last item over and over to save memory
-				objAsArray.ExpandTo(num);
+				objAsArray.ExpandTo((int)num);
 			}
 		}
 
+		protected override void performMutation(DataElement obj, ulong value)
+		{
+			// Should never get a ulong
+			throw new NotImplementedException();
+		}
 	}
 }
 
