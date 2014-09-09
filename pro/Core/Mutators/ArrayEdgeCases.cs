802d176b28387cc66c9fac9a6e57cf322d62c5e5
Seth Hinze <seth@dejavusecurity.com>
2014-09-09 13:04:50 -0700

    Implement new array edge case mutator.

--- pro/Core/Mutators/ArrayEdgeCases.cs
+++ pro/Core/Mutators/ArrayEdgeCases.cs
@@ -1,15 +1,10 @@
-﻿
-//
+﻿//
 // Copyright (c) Deja vu Security
 //
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-using Peach.Enterprise.Mutators.Utility;
 using Peach.Core.Dom;
-using Peach.Core.IO;
 
 using NLog;
 
@@ -17,29 +12,27 @@ namespace Peach.Core.Mutators
 {
 	[Mutator("ArrayEdgeCases")]
 	[Description("Change the length of arrays to integer edge cases")]
-	//[Hint("ArrayNumericalEdgeCases-N", "Gets N by checking node for hint, or returns default (50).")]
-	public class ArrayEdgeCases : Mutator
+	public class ArrayEdgeCases : Utility.IntegerEdgeCases
 	{
 		static NLog.Logger logger = LogManager.GetCurrentClassLogger();
-		IntegerEdgeCases edgeCases;
 
 		public ArrayEdgeCases(DataElement obj)
+			: base(obj)
 		{
-			name = "ArrayEdgeCases";
-			edgeCases = new IntegerEdgeCases(UInt16.MinValue, UInt16.MaxValue);
 		}
 
-		public override uint mutation
+		protected override NLog.Logger Logger
 		{
-			/* TODO - Make this work :) */
-			get { return 0; }
-			set {  }
+			get
+			{
+				return logger;
+			}
 		}
 
-		public override int count
+		protected override void GetLimits(DataElement obj, out long min, out ulong max)
 		{
-			// TODO- Make this real :)
-			get { return 1000; }
+			min = uint.MinValue;
+			max = uint.MaxValue;
 		}
 
 		public new static bool supportedDataElement(DataElement obj)
@@ -50,22 +43,9 @@ namespace Peach.Core.Mutators
 			return false;
 		}
 
-		public override void sequentialMutation(DataElement obj)
+		protected override void performMutation(DataElement obj, long num)
 		{
-			throw new NotSupportedException("TODO - Make this work!");
-		}
-
-		public override void randomMutation(DataElement obj)
-		{
-			performMutation(obj, (int) edgeCases.Next(context.Random));
-			obj.mutationFlags = MutateOverride.Default;
-		}
-
-		public void performMutation(DataElement obj, int num)
-		{
-			logger.Trace("performMutation(num=" + num + ")");
-
-			Dom.Array objAsArray = (Dom.Array)obj;
+			var objAsArray = (Dom.Array)obj;
 
 			if (num < objAsArray.Count)
 			{
@@ -82,10 +62,15 @@ namespace Peach.Core.Mutators
 			{
 				// add some items, but do it by replicating
 				// the last item over and over to save memory
-				objAsArray.ExpandTo(num);
+				objAsArray.ExpandTo((int)num);
 			}
+		}
+
+		protected override void performMutation(DataElement obj, ulong value)
+		{
+			// Should never get a ulong
+			throw new NotImplementedException();
 		}
 	}
 }
 
-// end
