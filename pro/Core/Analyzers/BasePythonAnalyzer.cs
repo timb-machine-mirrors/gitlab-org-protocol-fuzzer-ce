5b82e27029b0b89440678397d0d2f0e189850f5e
Michael Eddington <mike@dejavusecurity.com>
2014-09-11 12:58:55 -0700

    added helper base classes for pure native python extension

---
+++ pro/Core/Analyzers/BasePythonAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Peach.Core;
+
+namespace Peach.Enterprise.Analyzers
+{
+	[Serializable]
+	public abstract class BasePythonAnalyzer : Analyzer
+	{
+		public BasePythonAnalyzer()
+		{
+		}
+
+		public BasePythonAnalyzer(Dictionary<string, Variant> args)
+		{
+		}
+
+		[OnCloning]
+		private bool OnCloning(object context)
+		{
+			return false;
+		}
+	}
+}
