176ba717752b0f0b54e78be1cc5f3d1c83f08b18
Michael Eddington <mike@dejavusecurity.com>
2014-09-11 18:42:35 -0700

    interface for pit serialization

---
+++ core/Core/IPitSerializable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Xml;
+
+namespace Peach.Core
+{
+	/// <summary>
+	/// Implemented by items that can be serialized to a Peach Pit format.
+	/// </summary>
+	public interface IPitSerializable
+	{
+		/// <summary>
+		/// Converts object into Pit XML
+		/// </summary>
+		/// <remarks>
+		/// Objects that implement this interface must create their full
+		/// xml representation. This includes calls to WriteElementString.
+		/// </remarks>
+		/// <param name="writer"></param>
+		void WritePit(XmlWriter writer);
+	}
+}
