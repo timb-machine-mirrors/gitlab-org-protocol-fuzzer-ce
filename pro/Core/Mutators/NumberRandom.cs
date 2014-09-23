2585af7b976ef1d456f971ebb622a96e7c17df14
Seth Hinze <seth@dejavusecurity.com>
2014-09-22 17:09:17 -0700

    Adding number random mutator.

---
+++ pro/Core/Mutators/NumberRandom.cs
@@ -0,0 +1,100 @@
+//
+// Copyright (c) Deja vu Security
+//
+
+using System;
+
+using Peach.Core.Dom;
+
+using NLog;
+
+namespace Peach.Core.Mutators
+{
+	[Mutator("NumberRandom")]
+	[Description("Produce random number in range of underlying element.")]
+	public class NumberRandom : Mutator
+	{
+		int total;
+		Func<Variant> gen;
+
+		public NumberRandom(DataElement obj)
+			: base(obj)
+		{
+			var asNum = obj as Dom.Number;
+
+			if (asNum == null)
+			{
+				System.Diagnostics.Debug.Assert(obj is Dom.String);
+
+				total = 64; // Log(ulong.MaxValue, 2)
+				gen = () => new Variant(context.Random.Next(long.MinValue, long.MaxValue).ToString());
+			}
+			else if (asNum.Signed)
+			{
+				total = (int)Math.Round(Math.Log((ulong)((long)asNum.MaxValue - asNum.MinValue), 2));
+ 
+				if (asNum.lengthAsBits < 32)
+					gen = () => new Variant(context.Random.Next((int)asNum.MinValue, (int)asNum.MaxValue));
+				else if (asNum.lengthAsBits == 32)
+					gen = () => new Variant(context.Random.NextInt32());
+				else if (asNum.lengthAsBits < 64)
+					gen = () => new Variant(context.Random.Next((long)asNum.MinValue, (long)asNum.MaxValue));
+				else if (asNum.lengthAsBits == 64)
+					gen = () => new Variant(context.Random.NextInt64());
+				else
+					throw new NotSupportedException();
+			}
+			else
+			{
+				total = (int)Math.Round(Math.Log(asNum.MaxValue, 2));
+
+				if (asNum.lengthAsBits < 32)
+					gen = () => new Variant(context.Random.Next((uint)asNum.MinValue, (uint)asNum.MaxValue));
+				else if (asNum.lengthAsBits == 32)
+					gen = () => new Variant(context.Random.NextUInt32());
+				else if (asNum.lengthAsBits < 64)
+					gen = () => new Variant(context.Random.Next((ulong)asNum.MinValue, (ulong)asNum.MaxValue));
+				else if (asNum.lengthAsBits == 64)
+					gen = () => new Variant(context.Random.NextUInt64());
+				else
+					throw new NotSupportedException();
+			}
+		}
+
+		public new static bool supportedDataElement(DataElement obj)
+		{
+			if (obj is Dom.String && obj.isMutable)
+				return obj.Hints.ContainsKey("NumericalString");
+
+			// Ignore numbers <= 8 bits, they will be mutated
+			// with the NumericalVariance mutator
+			return obj is Dom.Number && obj.isMutable && obj.lengthAsBits > 8;
+		}
+
+		public override int count
+		{
+			get
+			{
+				return total;
+			}
+		}
+
+		public override uint mutation
+		{
+			get;
+			set;
+		}
+
+		public override void sequentialMutation(DataElement obj)
+		{
+			// Same as random, but seed is fixed
+			randomMutation(obj);
+		}
+
+		public override void randomMutation(DataElement obj)
+		{
+			obj.MutatedValue = gen();
+			obj.mutationFlags = MutateOverride.Default;
+		}
+	}
+}
