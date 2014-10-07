dab7d773a2fac483b1220ea04b40632398fd5d4d
Seth Hinze <seth@dejavusecurity.com>
2014-10-07 16:56:09 -0700

    Commenting configuration.

--- build/config/doc.py
+++ build/config/doc.py
@@ -2,6 +2,16 @@ from waflib import Utils, Errors
 from waflib.TaskGen import feature
 import os.path
 
+'''
+For Windows 7 x64:
+ * Strawberry Perl 5.18.2.2 64bit (strawberry-perl-5.18.2.2-64bit.msi)
+ * Ruby 1.9.3-p545 (rubyinstaller-1.9.3-p545.exe)
+ * Java SE Development Kit 8u11 x64 (jdk-8u11-windows-x64.exe)
+
+For Ubuntu 14.04 x64:
+ * default-jdk ruby-1.9.1 perl xsltproc xmllint
+'''
+
 host_plat = [ 'win32', 'linux', 'darwin' ]
 
 archs = [ ]
