6910332967676fb41d1cdd3de9e1a2d57769ea40
Seth Hinze <seth@dejavusecurity.com>
2014-10-14 11:35:26 -0700

    Rename global output zip to archive.zip and include installers.

--- build/tools/pkg.py
+++ build/tools/pkg.py
@@ -2,34 +2,39 @@ from waflib.Build import InstallContext
 from waflib import Task, Utils, Logs, Configure, Context, Options, Errors
 import os, zipfile, sys
 
-class ZipContext(InstallContext):
+class PkgContext(InstallContext):
+	'''create product installers'''
+
+	cmd = 'pkg'
+
+	def __init__(self, **kw):
+		super(PkgContext, self).__init__(**kw)
+		self.is_pkg = True
+
+class ZipContext(PkgContext):
 	'''zip contents of output directory'''
 
 	cmd = 'zip'
 
 	def __init__(self, **kw):
-		super(self.__class__, self).__init__(**kw)
+		super(ZipContext, self).__init__(**kw)
 		self.installed_files = []
 
 	def do_install(self, src, tgt, chmod=Utils.O644):
 		self.installed_files.append(tgt)
-		super(self.__class__, self).do_install(src, tgt, chmod)
+		super(ZipContext, self).do_install(src, tgt, chmod)
 
 	def execute(self):
-		super(self.__class__, self).execute()
+		super(ZipContext, self).execute()
 		if self.installed_files:
 			self.archive()
 
 	def archive(self):
 		env = self.env
-		version = '%s' % (env.BUILDTAG)
-		args = [ env.APPNAME, version, env.TARGET ]
-		if env.SUBARCH: args.append(env.SUBARCH)
-		if env.VARIANT: args.append(env.VARIANT)
 
-		base_path = self.path.make_node(env.BINDIR)
-		base_name = '-'.join(args).lower()
-		arch_name = '%s.zip' % (os.path.join(env.OUTPUT, base_name))
+		base_path = self.path.make_node(env.PREFIX)
+		base_name = 'archive.zip'
+		arch_name = os.path.join(env.PREFIX, base_name)
 
 		Logs.warn('Creating archive: %s' % arch_name)
 
@@ -73,18 +78,9 @@ class ZipContext(InstallContext):
 			dgst.delete()
 		except Exception:
 			pass
-		dgst.write('SHA1(%s.zip)= %s\n' % (base_name, digest))
+		dgst.write('SHA1(%s)= %s\n' % (base_name, digest))
 
 		Logs.warn('New archive created: %s (sha1=%s)' % (arch_name, digest))
-
-class PkgContext(InstallContext):
-	'''create product installers'''
-
-	cmd = 'pkg'
-
-	def __init__(self, **kw):
-		super(self.__class__, self).__init__(**kw)
-		self.is_pkg = True
 
 class PkgTask(Task.Task):
 	def runnable_status(self):
