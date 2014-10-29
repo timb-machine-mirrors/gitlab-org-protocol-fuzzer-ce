bc0913048035b479ea7f3056ec9df566c4a71fd4
Frank Laub <f.laub@dejavusecurity.com>
2014-10-29 13:19:52 -0700

    Squashed 'pits/' changes from 914062d..a194fc8
    a194fc8 adding randofaulter pit
    60ca2ea Removing errant text that makes xml non-parsable.
    b0c094d Moving http sapmles into pro.
    dadd153 Update Process.adoc
    a7d403e Update Process.adoc
    e4ab6b6 Update Process.adoc
    5d97e48 Update Process.adoc
    8ae5cf9 Merge branch 'master' of github:dejavu/pits
    46bb4b0 Merge branch 'master' of github:dejavu/pits
    92d03cc merging fixes
    a1566d2 Merge branch 'feature/bgp_fuzzing' of github:dejavu/pits
    0dbe163 Last minute fixups to BGPIdentifier and others.
    817f8f8 Accidentally recovered from strange HTML file
    a726e93 Deleting old MP-EXT
    6efb94f Adding back the RouteRefresh module
    764346c changes
    416614a rename capture file to indicate it was created by Deja
    2ef1df4 capture: 2-way RTP session created using baresip
    b3885ea Merge branch 'master' of github:dejavu/pits
    ecffd73 sftp base code
    dd8a29e add details to pit development process for Huawei
    0bd88e2 promoting HTTP pit to pro
    138708f Removed constraints, and replaced with tokens.
    8334cdb Moving samples
    47a4d97 Partial State Model.
    5c5e233 Completed the DataModel for Quagga Support.
    1d327f6 Refactored BGP_Data, breaking out RFCs in to separate files for sanity.
    ace9ef3 added filter
    9dd5d31 generated new docs
    0d92a08 updated schedule
    c238b9c updated docs for icmp
    32fdf5f tested pits
    1318bc4 updating config
    c3cf7a5 Got basic datamodel up.
    2028669 updated huawei pit stuffs
    54e70ab Changing pit to use RawEther publisher with IPv6 header.
    f96534d started documenting pittester
    c862060 updates to huawei scops, new captures, updated schedule
    b07d0ad started work on process document
    bbb203c updated library docs
    90a3379 fixing pittester errors
    d2213bc Merge branch 'master' of github:dejavu/pits
    be7085c removing input action to fix bug
    5aefb76 formatting
    4ba8843 minor updates to usage and main server xml
    a43758c Merge branch 'feature-14Q2-hotfix-tcp'
    e40543e updating to work like v4 changes
    28b7f8e disabled parsing of tcp options for inbound packets
    c598013 allowing it to work with noisy services
    dcc7bf7 added comments and some new code to fix some issues
    ba6ff69 huawei pits
    b9b157b Merge branch 'master' of github:dejavu/pits
    ff89888 renamed blob to correct tlv
    e594f96 Removing bad sample.
    e5dbda3 fixing unittests and model
    4c0e07c Created Q2 pdf
    6d02c82 More fixes for SNMP v2c
    3196c6e Moved HTTP pit to deja from pro
    5554842 updating snmp pit
    
    git-subtree-dir: pits
    git-subtree-split: a194fc8f5121979db4f6ce286970fcba4e0d6364

---
+++ client/huawei/SSH_FTP/SftpExtensions/SshChannelPublisher.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+using System.IO;
+using System.Diagnostics;
+using NLog;
+
+using Peach.Core;
+using Peach.Core.IO;
+using Peach.Core.Publishers;
+
+namespace SftpExtensions
+{
+    public class OtherSftpClient : BaseClient
+    {
+        private SshSubsystemSession sftpSession;
+        private TimeSpan _operationTimeout;
+
+        public OtherSftpClient(ConnectionInfo connectionInfo)
+            : base(connectionInfo)
+        {
+        }
+
+        // default value is -1 ms, for infinite time-out period
+        public TimeSpan OperationTimeout
+        {
+            get
+            {
+                //CheckDisposed();
+                return _operationTimeout;
+            }
+            set
+            {
+                //CheckDisposed();
+                _operationTimeout = value;
+            }
+        }
+
+        protected override void OnConnected()
+        {
+            base.OnConnected();
+
+            this.sftpSession = new SshSubsystemSession(this.Session, this.OperationTimeout, this.ConnectionInfo.Encoding);
+            this.sftpSession.Connect();
+        }
+
+        protected override void OnDisconnecting()
+        {
+            base.OnDisconnecting();
+
+            // disconnects and disposes the SFTP session
+            // disposal is necessary since we create a new SFTP session on each connect
+            if (this.sftpSession != null)
+            {
+                this.sftpSession.Disconnect();
+                this.sftpSession.Dispose();
+                this.sftpSession = null;
+            }
+        }
+
+        // true to release both managed and unmanaged resources
+        // false to release only unmanaged resources
+        protected override void Dispose(bool disposing)
+        {
+            if (this.sftpSession != null)
+            {
+                this.sftpSession.Dispose();
+                this.sftpSession = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        public void Send(byte[] buffer)
+        {
+            if (this.sftpSession != null)
+            {
+                this.sftpSession.SendData(buffer);
+            }
+        }
+
+        public byte[] NextBuffer()
+        {
+            byte[] buffer = null;
+            if (this.sftpSession != null)
+            {
+                buffer = this.sftpSession.GetNextBuffer();
+            }
+            if (buffer != null)
+            {
+                return buffer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    class SshSubsystemSession : SubsystemSession
+    {
+
+        private ConcurrentQueue<byte[]> queue;
+
+        public SshSubsystemSession(Session session, TimeSpan operationTimeout, System.Text.Encoding encoding)
+            : base(session, "sftp", operationTimeout, encoding)
+        {
+            this.queue = new ConcurrentQueue<byte[]>();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.queue = null;
+            base.Dispose(disposing);
+        }
+
+        protected override void OnChannelOpen()
+        {
+            Console.WriteLine("Opened a channel.");
+        }
+
+        protected override void OnDataReceived(uint dataTypeCode, byte[] data)
+        {
+            this.queue.Enqueue(data);
+        }
+
+        public byte[] GetNextBuffer()
+        {
+            byte[] buffer;
+            bool success = this.queue.TryDequeue(out buffer);
+            if (success)
+            {
+                return buffer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    [Publisher("SshChannelPublisher", true)]
+    [Parameter("Host", typeof(string), "Hostname")]
+    [Parameter("Username", typeof(string), "Username")]
+    [Parameter("Password", typeof(string), "Password", "")]
+    [Parameter("KeyPath", typeof(string), "Path to ssh key", "")]
+    public class SshChannelPublisher : StreamPublisher
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string KeyPath { get; set; }
+
+        private OtherSftpClient otherSftpClient;
+        private ConnectionInfo connInfo;
+        private AuthenticationMethod authMethod;
+
+        public SshChannelPublisher(Dictionary<string, Variant> args)
+            : base(args)
+        {
+            ParameterParser.Parse(this, args);
+
+            // both user/pass and key-based SSH auth
+            if (Password == null && KeyPath == null && KeyPath != "")
+                throw new PeachException("Either Password or KeyPath is required.");
+
+			if (!string.IsNullOrEmpty(Password))
+            {
+                this.authMethod = new PasswordAuthenticationMethod(Username, Password);
+            }
+			else if (!string.IsNullOrEmpty(KeyPath))
+			{
+				var key = new PrivateKeyFile(KeyPath);
+				this.authMethod = new PrivateKeyAuthenticationMethod(Username, key);
+			}
+			else
+			{
+				throw new PeachException("Error: Must supply one of 'Password' or 'KeyPath' to SshChannelPublisher.");
+			}
+
+            if (this.authMethod != null)
+            {
+                this.connInfo = new ConnectionInfo(Host, Username, this.authMethod);
+                this.otherSftpClient = new OtherSftpClient(this.connInfo);
+            }
+
+            stream = new MemoryStream();
+        }
+
+        private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        protected override NLog.Logger Logger
+        {
+            get { return logger; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return stream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            stream.SetLength(value);
+        }
+
+        protected override void OnOpen()
+        {
+            this.otherSftpClient.Connect();
+        }
+
+        protected override void OnClose()
+        {
+            this.otherSftpClient.Disconnect();
+        }
+
+        protected override void OnOutput(BitwiseStream data)
+        {
+            var bitReader = new BitReader(data);
+            var buffer = new byte[data.Length];
+            bitReader.ReadBytes(buffer.Length);
+            this.otherSftpClient.Send(buffer);
+        }
+
+        protected override void OnInput()
+        {
+            byte[] buffer = this.otherSftpClient.NextBuffer();
+            if (buffer != null)
+            {
+                var position = stream.Position;
+                stream.Seek(0, SeekOrigin.End);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Position = position;
+            }
+        }
+    }
+}
