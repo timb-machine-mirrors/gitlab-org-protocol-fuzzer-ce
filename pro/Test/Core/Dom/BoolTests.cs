783825f7082b554b6cb4128e6b8e2b62c51b4f88
jordyn <jordyn@dejavusecurity.com>
2014-11-19 18:43:14 -0800

    Added support for Bool, Null and Sequences. Added more json element tests. Added more JsonAnalayzer tests. Updated xsd

---
+++ pro/Test/Core/Dom/BoolTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+using Peach.Core.Analyzers;
+using Peach.Core.Cracker;
+using Peach.Core.Dom;
+using Peach.Core.Test;
+using Peach.Enterprise.Dom;
+
+namespace Peach.Enterprise.Test.Dom
+{
+    class BoolTests : DataModelCollector
+    {
+        [Test]
+        [Category("Peach")]
+        public void SimpleTest()
+        {
+            var b = new Bool();
+
+            Assert.AreEqual(1, b.lengthAsBits);
+            Assert.AreEqual(LengthType.Bits, b.lengthType);
+        }
+    }
+}
