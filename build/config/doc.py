2a2902e7f7713d4235db38f4bb0f24b9bf916d4c
Seth Hinze <seth@dejavusecurity.com>
2014-11-19 17:11:55 -0800

    Starting doxygen build support.

--- build/config/doc.py
+++ build/config/doc.py
@@ -7,6 +7,7 @@ For Windows 7 x64:
  * Strawberry Perl 5.18.2.2 64bit (strawberry-perl-5.18.2.2-64bit.msi)
  * Ruby 1.9.3-p545 (rubyinstaller-1.9.3-p545.exe)
  * Java SE Development Kit 8u11 x64 (jdk-8u11-windows-x64.exe)
+ * Doxygen (doxygen-1.8.8-setup.exe)
 
 For Ubuntu 14.04 x64:
  * ruby1.9.1 perl xsltproc libxml2-utils ghostscript
@@ -22,6 +23,7 @@ tools = [
 	'misc',
 	'tools.asciidoc',
 	'tools.utils',
+	'tools.doxygen',
 ]
 
 def prepare(conf):
