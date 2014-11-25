caae38bbc965ef13724afb9e2faf45a89e0e9bcd
Frank Laub <f.laub@dejavusecurity.com>
2014-11-24 17:51:50 -0800

    Update frontend so it can be unit tested

---
+++ pro/Web/public/ts/app/reference.ts
@@ -0,0 +1,32 @@
+/** Library references (unordered) */
+/// <reference path="../def/reference.ts" />
+
+/** The models package (unordered) */
+/// <reference path="models/models.ts" />
+
+/** Managed references (unordered) */
+//grunt-start
+/// <reference path="controllers/config-monitors.ts" />
+/// <reference path="controllers/config-test.ts" />
+/// <reference path="controllers/config-variables.ts" />
+/// <reference path="controllers/copypit.ts" />
+/// <reference path="controllers/dash.ts" />
+/// <reference path="controllers/faults.ts" />
+/// <reference path="controllers/main.ts" />
+/// <reference path="controllers/metrics.ts" />
+/// <reference path="controllers/pitlibrary.ts" />
+/// <reference path="controllers/pittest.ts" />
+/// <reference path="controllers/startjob.ts" />
+/// <reference path="controllers/wizard.ts" />
+/// <reference path="services/peach.ts" />
+/// <reference path="services/pitconfigurator.ts" />
+//grunt-end
+
+/** Angular module dependencies
+*
+* Note: These references must be
+*/
+/* (ordered) */
+
+/** Entry-point */
+/// <reference path="app.ts" />
---
+++ pro/Web/public/ts/test/reference.ts
@@ -0,0 +1,7 @@
+/** Load app references */
+/// <reference path="../app/reference.ts" />
+
+/** Unordered deps */
+//grunt-start
+/// <reference path="test.ts" />
+//grunt-end
