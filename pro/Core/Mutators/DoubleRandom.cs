7b48057489499f5befa3156d1f96eac5b3422286
jordyn <jordyn@dejavusecurity.com>
2014-11-25 12:12:50 -0800

    Fixed double random mutator

--- pro/Core/Mutators/DoubleRandom.cs
+++ pro/Core/Mutators/DoubleRandom.cs
@@ -6,36 +6,32 @@ using NLog;
 
 namespace Peach.Core.Mutators
 {
-    //Hide this mutator as its not fully tested
-    [Mutator("DoubleRandom")]
+	//Hide this mutator as its not fully tested
+	[Mutator("DoubleRandom")]
 	[Description("Produce random number in range of underlying element.")]
 	public class DoubleRandom : Mutator
-    {
-        const int maxCount = 5000; // Maximum count is 5000
+	{
+		const int maxCount = 5000; // Maximum count is 5000
 
 		Func<Variant> gen;
 
-        public DoubleRandom(DataElement obj)
+		public DoubleRandom(DataElement obj)
 			: base(obj)
 		{
-            var asNum = obj as Peach.Core.Dom.Double;
+			var asNum = obj as Peach.Core.Dom.Double;
 
-            if (asNum.lengthAsBits == 32)
-                gen = () => new Variant(BitConverter.ToDouble(BitConverter.GetBytes(context.Random.NextUInt32()), 0));
-            else if (asNum.lengthAsBits == 64)
-                gen = () => new Variant(BitConverter.ToDouble(BitConverter.GetBytes(context.Random.NextUInt64()), 0));
-            else
-                throw new NotSupportedException();
+			if (asNum == null)
+			{
+				System.Diagnostics.Debug.Assert(obj is Dom.String);
 
-            if (asNum == null)
-            {
-                System.Diagnostics.Debug.Assert(obj is Dom.String);
-
-                if (asNum.lengthAsBits == 32)
-                    gen = () => new Variant(BitConverter.ToDouble(BitConverter.GetBytes(context.Random.NextUInt32()), 0).ToString());
-                else if (asNum.lengthAsBits == 64)
-                    gen = () => new Variant(BitConverter.ToDouble(BitConverter.GetBytes(context.Random.NextUInt64()), 0).ToString());
-            }
+				gen = () => new Variant(BitConverter.Int64BitsToDouble(context.Random.NextInt64()).ToString());
+			}
+			else if (asNum.lengthAsBits == 32)
+				gen = () => new Variant(BitConverter.ToSingle(BitConverter.GetBytes(context.Random.NextInt32()), 0));
+			else if (asNum.lengthAsBits == 64)
+				gen = () => new Variant(BitConverter.Int64BitsToDouble(context.Random.NextInt64()));
+			else
+				throw new NotSupportedException();
 		}
 
 		public new static bool supportedDataElement(DataElement obj)
@@ -43,14 +39,14 @@ namespace Peach.Core.Mutators
 			if (obj is Dom.String && obj.isMutable)
 				return obj.Hints.ContainsKey("NumericalString");
 
-            return obj is Peach.Core.Dom.Double && obj.isMutable && (obj.lengthAsBits == 64 || obj.lengthAsBits == 32);
+			return obj is Peach.Core.Dom.Double && obj.isMutable && (obj.lengthAsBits == 64 || obj.lengthAsBits == 32);
 		}
 
 		public override int count
 		{
 			get
 			{
-                return maxCount;
+				return maxCount;
 			}
 		}
 
@@ -62,14 +58,14 @@ namespace Peach.Core.Mutators
 
 		public override void sequentialMutation(DataElement obj)
 		{
-            // Same as random, but seed is fixed
-            randomMutation(obj);
+			// Same as random, but seed is fixed
+			randomMutation(obj);
 		}
 
 		public override void randomMutation(DataElement obj)
 		{
-            obj.MutatedValue = gen();
-            obj.mutationFlags = MutateOverride.Default;
+			obj.MutatedValue = gen();
+			obj.mutationFlags = MutateOverride.Default;
 		}
-    }
+	}
 }
--- pro/Test/Core/Mutators/DoubleRandomTests.cs
+++ pro/Test/Core/Mutators/DoubleRandomTests.cs
@@ -5,28 +5,53 @@ using Peach.Core.Dom;
 
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Peach.Core.Test.Mutators
 {
-    [TestFixture]
-    class DoubleRandomTests
-    {
-        [Test]
-        public void TestSupported()
-        {
-            var runner = new MutatorRunner("DoubleRandom");
+	[TestFixture]
+	class DoubleRandomTests
+	{
+		[Test]
+		public void TestSupported()
+		{
+			var runner = new MutatorRunner("DoubleRandom");
 
-            Assert.False(runner.IsSupported(new Blob()));
+			Assert.False(runner.IsSupported(new Blob()));
 
-            Assert.True(runner.IsSupported(new Peach.Core.Dom.Double() { length = 64 }));
-            Assert.True(runner.IsSupported(new Peach.Core.Dom.Double() { length = 32 }));
+			Assert.True(runner.IsSupported(new Peach.Core.Dom.Double() { length = 64 }));
+			Assert.True(runner.IsSupported(new Peach.Core.Dom.Double() { length = 32 }));
 
-            Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("0") }));
-            Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("100") }));
-            Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("-100") }));
-            Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("NaN") }));
-            Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("Infinity") }));
-            Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("-Infinity") }));
-        }
-    }
+			Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("0") }));
+			Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("100") }));
+			Assert.True(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("-100") }));
+			Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("NaN") }));
+			Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("Infinity") }));
+			Assert.False(runner.IsSupported(new Dom.String() { DefaultValue = new Variant("-Infinity") }));
+		}
+
+		[Test]
+		public void Double64BitTest()
+		{
+			var runner = new MutatorRunner("DoubleRandom");
+
+			var dble = new Dom.Double("Double") { DefaultValue = new Variant(1.0) };
+
+			var m = runner.Random(500, dble);
+
+			Assert.AreEqual(500, m.Count());
+		}
+
+		[Test]
+		public void Double32BitTest()
+		{
+			var runner = new MutatorRunner("DoubleRandom");
+
+			var dble = new Dom.Double("Double") { DefaultValue = new Variant(1.0), length = 32 };
+
+			var m = runner.Random(500, dble);
+
+			Assert.AreEqual(500, m.Count());
+		}
+	}
 }
