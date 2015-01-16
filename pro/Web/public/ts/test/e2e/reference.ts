78bb7ccbeb745367a1cf7a2633a43c7142b8d6a1
Frank Laub <f.laub@dejavusecurity.com>
2015-01-16 01:29:59 -0800

    1st cut at using protractor for end-to-end testing

---
+++ pro/Web/public/ts/test/e2e/reference.ts
@@ -0,0 +1,8 @@
+/** Library references */
+/// <reference path="../def/jasmine/jasmine.d.ts" />
+/// <reference path="../def/angular-protractor/angular-protractor.d.ts" />
+
+/** Unordered deps */
+//grunt-start
+/// <reference path="test.ts" />
+//grunt-end
