85d2ce380116430912aed6d89f3f5ed840ab5f19
Frank Laub <f.laub@dejavusecurity.com>
2015-02-05 23:36:29 -0800

    Adding more serial port tests, fix bugs along the way

--- core/Core/Utilities.cs
+++ core/Core/Utilities.cs
@@ -37,6 +37,7 @@ using System.Text;
 using NLog;
 using NLog.Targets;
 using NLog.Config;
+using System.Threading;
 
 namespace Peach.Core
 {
@@ -639,6 +640,34 @@ namespace Peach.Core
 			return bytes + " Bytes";
 		}
 	}
-}
 
-// end
+	public class Retry
+	{
+		public static readonly TimeSpan DefaultRetryDelay = TimeSpan.FromSeconds(0.5);
+		public const int DefaultRetryCount = 3;
+
+		public static void Execute(Action fn, int retryCount = DefaultRetryCount)
+		{
+			Execute(fn, DefaultRetryDelay, retryCount);
+		}
+
+		public static void Execute(Action fn, TimeSpan retryDelay, int retryCount = DefaultRetryCount)
+		{
+			int count = 0;
+			while (true)
+			{
+				try
+				{
+					fn();
+					break;
+				}
+				catch (Exception ex)
+				{
+					if (count++ == retryCount)
+						throw ex;
+					Thread.Sleep(retryDelay);
+				}
+			}
+		}
+	}
+}
