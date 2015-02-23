83607ac0bdfddbebe8d02576565cc05ef1b7f3ed
Seth Hinze <seth@dejavusecurity.com>
2015-02-23 12:14:07 -0800

    Fix failing test

--- pro/Test/Core/Agent/LegacyMonitorTests.cs
+++ pro/Test/Core/Agent/LegacyMonitorTests.cs
@@ -130,7 +130,7 @@ namespace Peach.Pro.Test.Core.Agent
 
 			var pe = Assert.Throws<PeachException>(() =>  e.startFuzzing(dom, cfg));
 
-			Assert.AreEqual("Fault detected on control iteration.", pe.Message);
+			Assert.AreEqual("Fault detected on control record iteration.", pe.Message);
 			Assert.AreEqual(1, faults.Count);
 			Assert.AreEqual(FaultType.Fault, faults[0].type);
 			Assert.That(faults[0].title, Is.StringStarting("Process output matched FaulOnRegex"));
