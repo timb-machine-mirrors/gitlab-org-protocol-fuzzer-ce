c95308710da36a6cd6afad3fdfcd421a3c90e785
Seth Hinze <seth@dejavusecurity.com>
2015-02-24 14:41:01 -0800

    Fix integer overflow with maxOutputSize. fixes: #253

--- pro/Test/Core/Mutators/DataElementDuplicateTests.cs
+++ pro/Test/Core/Mutators/DataElementDuplicateTests.cs
@@ -208,5 +208,41 @@ namespace Peach.Pro.Test.Core.Mutators
 			// (100 - 11)/11 = 8
 			Assert.AreEqual(8, mutatedDataModels.Count);
 		}
+
+		[Test]
+		public void TestMaxOutputSizeOverflow()
+		{
+			string xml = @"
+<Peach>
+	<DataModel name='DM'>
+		<String name='str1' value='Hello!' />
+		<String name='str2' value='World!' />
+	</DataModel>
+
+	<StateModel name='StateModel' initialState='initial'>
+		<State name='initial'>
+			<Action type='output'>
+				<DataModel ref='DM'/>
+			</Action> 
+		</State>
+	</StateModel>
+
+	<Test name='Default' maxOutputSize='5'>
+		<StateModel ref='StateModel'/>
+		<Publisher class='Null'/>
+		<Strategy class='Sequential'/>
+		<Mutators mode='include'>
+			<Mutator class='DataElementDuplicate' />
+		</Mutators>
+	</Test>
+</Peach>
+";
+
+			RunEngine(xml);
+
+			// Should be no mutations since duplication
+			// would surpass maxOutputSize
+			Assert.AreEqual(0, mutatedDataModels.Count);
+		}
 	}
 }
