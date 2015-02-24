bbfb24803a0d86789c9b66cee9e021b0a63505f8
Seth Hinze <seth@dejavusecurity.com>
2015-02-24 14:41:01 -0800

    Fix integer overflow with maxOutputSize. fixes: #253

--- pro/Test/Core/Mutators/StringAsciiRandomTests.cs
+++ pro/Test/Core/Mutators/StringAsciiRandomTests.cs
@@ -66,5 +66,42 @@ namespace Peach.Pro.Test.Core.Mutators
 			foreach (var m in mutatedDataModels)
 				Assert.LessOrEqual(m.Value.Length, 50);
 		}
+
+		[Test]
+		public void TestMaxOutputSizeOverflow()
+		{
+			string xml = @"
+<Peach>
+	<DataModel name='DM'>
+		<String name='str1' value='Hello!' />
+		<String name='str2' value='' />
+	</DataModel>
+
+	<StateModel name='StateModel' initialState='initial'>
+		<State name='initial'>
+			<Action type='output'>
+				<DataModel ref='DM'/>
+			</Action> 
+		</State>
+	</StateModel>
+
+	<Test name='Default' maxOutputSize='5'>
+		<StateModel ref='StateModel'/>
+		<Publisher class='Null'/>
+		<Strategy class='Sequential'/>
+		<Mutators mode='include'>
+			<Mutator class='StringAsciiRandom' />
+		</Mutators>
+	</Test>
+</Peach>
+";
+
+			RunEngine(xml);
+
+			Assert.AreEqual(7, mutatedDataModels.Count);
+
+			foreach (var item in mutatedDataModels)
+				Assert.LessOrEqual(item.Value.Length, 6);
+		}
 	}
 }
