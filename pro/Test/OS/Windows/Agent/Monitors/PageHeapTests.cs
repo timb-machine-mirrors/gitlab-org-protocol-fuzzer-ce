512c17bdbb918bc44aa3c59b798872ea67f1a657
Frank Laub <f.laub@dejavusecurity.com>
2015-03-05 19:06:38 -0800

    Add PageHeap tests

--- pro/Test/OS/Windows/Agent/Monitors/PageHeapTests.cs
+++ pro/Test/OS/Windows/Agent/Monitors/PageHeapTests.cs
@@ -1,8 +1,12 @@
 ﻿using NUnit.Framework;
+using Peach.Core;
+using Peach.Core.Test;
+using Peach.Pro.OS.Windows.Agent.Monitors;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace Peach.Pro.Test.OS.Windows.Agent.Monitors
 {
@@ -10,10 +14,62 @@ namespace Peach.Pro.Test.OS.Windows.Agent.Monitors
 	[Category("Peach")]
 	class PageHeapTests
 	{
-		[Test]
-		public void TestTODO()
+		const string Monitor = "PageHeap";
+
+		private bool Check(string exe)
 		{
-			Assert.Fail();
+			var dbgPath = WindowsDebuggerHybrid.FindWinDbg(); ;
+			var p = SubProcess.Run(Path.Combine(dbgPath, "gflags.exe"), "/p");
+			var stdout = p.StdOut.ToString();
+			var lines = stdout.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+			bool isFirst = true;
+			foreach (var line in lines)
+			{
+				if (isFirst)
+				{
+					isFirst = false;
+					continue;
+				}
+
+				if (line.Contains("{0}: page heap enabled".Fmt(exe)))
+					return true;
+			}
+
+			return false;
+		}
+
+		[Test]
+		public void TestNoParams()
+		{
+			var runner = new MonitorRunner(Monitor, new Dictionary<string, string>());
+			var ex = Assert.Catch(() => runner.Run());
+			Assert.That(ex, Is.InstanceOf<PeachException>());
+			var msg = "Could not start monitor \"PageHeap\".  Monitor 'PageHeap' is missing required parameter 'Executable'.";
+			StringAssert.StartsWith(msg, ex.Message);
+		}
+
+		[Test]
+		public void TestBasic()
+		{
+			var exe = "foobar";
+			var runner = new MonitorRunner(Monitor, new Dictionary<string, string>
+			{
+				{"Executable", exe},
+			})
+			{
+				SessionStarting = m =>
+				{
+					m.SessionStarting();
+					Assert.IsTrue(Check(exe), "PageHeap should be enabled");
+				},
+				SessionFinished = m =>
+				{
+					m.SessionFinished();
+					Assert.IsFalse(Check(exe), "PageHeap should be disabled");
+				},
+			};
+			runner.Run();
 		}
 	}
 }
