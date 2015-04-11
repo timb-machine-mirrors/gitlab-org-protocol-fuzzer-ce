297989aae27e20d2d7b4eaee1c941b07643b4f1d
Frank Laub <f.laub@dejavusecurity.com>
2015-04-11 14:27:20 -0700

    Try to interact with teamcity

---
+++ build/teamcity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+if __name__ == "__main__":
+	for key in os.environ.keys():
+		print('%s: %s' % (key, os.environ[key]))
+
+	print("##teamcity[buildNumber'0.0.0']")
