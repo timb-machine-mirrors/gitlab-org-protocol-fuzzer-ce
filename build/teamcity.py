54dee9ebba20f8935debb9c295ea604d62331168
Frank Laub <f.laub@dejavusecurity.com>
2015-04-11 14:54:53 -0700

    Missing space

--- build/teamcity.py
+++ build/teamcity.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
 	for key in os.environ.keys():
 		print('%s: %s' % (key, os.environ[key]))
 
-	print("##teamcity[buildNumber'0.0.0']")
+	print("##teamcity[buildNumber '0.0.0']")
