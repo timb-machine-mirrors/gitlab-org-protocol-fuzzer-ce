2eab64fee31ed6205b04cca58d97649fbf305a9f
Seth Hinze <seth@peachfuzzer.com>
2015-06-11 15:15:22 -0700

    Moving copyright to peach fuzzer. refs: #309

--- core/Test/Properties/AssemblyInfo.cs
+++ core/Test/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Peach.Core.Test")]
 [assembly: AssemblyDescription("Peach Fuzzing Platform")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Deja vu Security")]
+[assembly: AssemblyCompany("Peach Fuzzer, LLC")]
 [assembly: AssemblyProduct("Peach.Core.Test")]
-[assembly: AssemblyCopyright("Copyright © Deja vu Security")]
+[assembly: AssemblyCopyright("Copyright (c) Peach Fuzzer, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
