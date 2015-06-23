e76568504b68fe7797e03126d459c8481a492993
Seth Hinze <seth@peachfuzzer.com>
2015-06-23 16:48:27 -0700

    Adding weight tuning to the random strategy. fixes: #345

--- pro/Core/MutationStrategies/RandomStrategy.cs
+++ pro/Core/MutationStrategies/RandomStrategy.cs
@@ -49,6 +49,7 @@ namespace Peach.Pro.Core.MutationStrategies
 	[Parameter("SwitchCount", typeof(int), "Number of iterations to perform per-mutator befor switching.", "200")]
 	[Parameter("MaxFieldsToMutate", typeof(int), "Maximum fields to mutate at once.", "6")]
 	[Parameter("StateMutation", typeof(bool), "Enable state mutations.", "false")]
+	[Parameter("Weighting", typeof(int), "Controls mutation weight evaulation.", "10")]
 	public class RandomStrategy : MutationStrategy
 	{
 		[DebuggerDisplay("{InstanceName} {ElementName} Mutators = {Mutators.Count}")]
@@ -212,6 +213,11 @@ namespace Peach.Pro.Core.MutationStrategies
 		WeightedList<MutationScope> mutableItems = new WeightedList<MutationScope>();
 
 		/// <summary>
+		/// Controls how weights are applied to the weighted list.
+		/// </summary>
+		readonly Func<int, int> _tuneWeights;
+
+		/// <summary>
 		/// The selected mutations for a given fuzzing iteration
 		/// </summary>
 		MutableItem[] mutations;
@@ -253,6 +259,17 @@ namespace Peach.Pro.Core.MutationStrategies
 				maxFieldsToMutate = int.Parse((string)args["MaxFieldsToMutate"]);
 			if (args.ContainsKey("StateMutation"))
 				stateMutations = bool.Parse((string)args["StateMutation"]);
+
+			var weighting = 10;
+			if (args.ContainsKey("Weighting"))
+				weighting = int.Parse((string)args["Weighting"]);
+
+			if (weighting < 1)        // If param<1, weight=1
+				_tuneWeights = x => 1;
+			else if (weighting == 1)  // If param=1, weight=SelectionWeight
+				_tuneWeights = x => x;
+			else                      // If param>1, weight=LOG(SelectionWeight,param)
+				_tuneWeights = x => (int)Math.Round(Math.Log(x, weighting)) + 1;
 
 			mutationScopeGlobal = new MutationScope("All");
 			mutationScopeState = new List<MutationScope>();
@@ -437,6 +454,8 @@ namespace Peach.Pro.Core.MutationStrategies
 				// Add global scope 1st
 				if (mutationScopeGlobal.Count > 0)
 					mutableItems.Add(mutationScopeGlobal);
+
+				mutableItems.TransformWeight(_tuneWeights);
 
 				// Cleanup containers used to collect the different scopes
 				mutationScopeGlobal = null;
