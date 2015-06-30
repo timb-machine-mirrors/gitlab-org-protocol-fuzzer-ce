26229ed7815d5c882adaa0b5553d512cea24d712
Frank Laub <frank@peachfuzzer.com>
2015-06-30 13:47:42 -0700

    Replace --promote with --advance, add --root

--- build/teamcity.py
+++ build/teamcity.py
@@ -4,17 +4,16 @@ import re
 import subprocess
 import argparse
 
-def init(match):
-
-	return buildtag
-
-def promote(buildtag):
-	pass
+def tag(roots):
+	for root in roots:
+		subprocess.check_call([ 'git', 'tag', '-a', '-m', 'Tagging build', 'v%s' % buildtag ], cwd=root)
+		subprocess.check_call([ 'git', 'push', '--tags' ], cwd=root)
 
 if __name__ == "__main__":
 	p = argparse.ArgumentParser(description='teamcity init')
-	p.add_argument('--promote', action='store_true')
+	p.add_argument('--advance', action='count')
 	p.add_argument('--match', default="v*")
+	p.add_argument('--root', action='append')
 
 	args = p.parse_args()
 
@@ -25,11 +24,9 @@ if __name__ == "__main__":
 	if branch == 'master' or branch.startswith('prod-'):
 		match = re.match(r'v(\d+)\.(\d+)\.(\d+).*', desc)
 		if match:
-			buildtag = '%s.%s.%d' % (match.group(1), match.group(2), int(match.group(3)) + 1)
+			buildtag = '%s.%s.%d' % (match.group(1), match.group(2), int(match.group(3)) + args.advance)
+			if args.advance:
+				tag(arg.root)
 
 	print("##teamcity[setParameter name='BuildTag' value='%s']" % buildtag)
 	print("##teamcity[buildNumber '%s']" % desc)
-
-	if args.promote and buildtag != '0.0.0':
-		subprocess.check_call([ 'git', 'tag', '-a', '-m', 'Tagging build', 'v%s' % buildtag ])
-		subprocess.check_call([ 'git', 'push', '--tags' ])
