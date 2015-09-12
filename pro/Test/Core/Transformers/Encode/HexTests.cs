e3a5162404eca725186ffee5c47c93cf6f154284
Frank Laub <frank@peachfuzzer.com>
2015-09-11 19:14:18 -0700

    Add lowercase param to Hex transformer, modbus requires uppercase

--- pro/Test/Core/Transformers/Encode/HexTests.cs
+++ pro/Test/Core/Transformers/Encode/HexTests.cs
@@ -59,6 +59,53 @@ namespace Peach.Pro.Test.Core.Transformers.Encode
 		}
 
 		[Test]
+		public void TestUppercase()
+		{
+			// standard test
+
+			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+				"<Peach>" +
+				"   <DataModel name=\"TheDataModel\">" +
+				"       <Block name=\"TheBlock\">" +
+				"           <Transformer class=\"Hex\">" +
+				"               <Param name=\"lowercase\" value=\"false\" />" +
+				"           </Transformer>" +
+				"           <Blob name=\"Data\" value=\"Hello\"/>" +
+				"       </Block>" +
+				"   </DataModel>" +
+
+				"   <StateModel name=\"TheState\" initialState=\"Initial\">" +
+				"       <State name=\"Initial\">" +
+				"           <Action type=\"output\">" +
+				"               <DataModel ref=\"TheDataModel\"/>" +
+				"           </Action>" +
+				"       </State>" +
+				"   </StateModel>" +
+
+				"   <Test name=\"Default\">" +
+				"       <StateModel ref=\"TheState\"/>" +
+				"       <Publisher class=\"Null\"/>" +
+				"   </Test>" +
+				"</Peach>";
+
+			PitParser parser = new PitParser();
+
+			Peach.Core.Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			RunConfiguration config = new RunConfiguration();
+			config.singleIteration = true;
+
+			Engine e = new Engine(this);
+			e.startFuzzing(dom, config);
+
+			// verify values
+			// -- this is the pre-calculated result from Peach2.3 on the blob: "Hello"
+			byte[] precalcResult = new byte[] { 0x34, 0x38, 0x36, 0x35, 0x36, 0x43, 0x36, 0x43, 0x36, 0x46 };
+			Assert.AreEqual(1, values.Count);
+			Assert.AreEqual(precalcResult, values[0].ToArray());
+		}
+
+		[Test]
 		public void CrackTest()
 		{
 			string xml = @"
