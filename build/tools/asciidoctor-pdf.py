e8c36e81e2b37e9e411d6d1e20439431c0fad85e
Frank Laub <frank@peachfuzzer.com>
2015-11-02 13:39:04 -0800

    Allow asciidoctor-pdf 1.5.x

--- build/tools/asciidoctor-pdf.py
+++ build/tools/asciidoctor-pdf.py
@@ -65,7 +65,7 @@ gemspec :path => "%s"
 	adr = conf.find_program('asciidoctor')
 	(out,err) = conf.cmd_and_log(adr + ['--version'], output=Context.BOTH)
 
-	if 'Asciidoctor 1.5. ' not in out:
+	if 'Asciidoctor 1.5.' not in out:
 		raise Errors.WafError("Expected Asciidoctor 1.5.x but found:\n%s" % out)
 
 	v.append_value('supported_features', 'asciidoctor-pdf')
