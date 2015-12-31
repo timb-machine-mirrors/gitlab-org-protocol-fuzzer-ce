04b66f98e052c81f4b1c8b9774901fffa3df2ed0
Seth Hinze <seth@peachfuzzer.com>
2015-12-31 10:44:40 -0800

    Fix another deadlock in ssl publisher when talking to openssl s_server.

--- pro/Test/Core/Publishers/SslPublisherTests.cs
+++ pro/Test/Core/Publishers/SslPublisherTests.cs
@@ -398,5 +398,47 @@ qLk0TB3QXaoHknsz7EhRnw==
 				e.startFuzzing(dom, cfg);
 			}
 		}
+
+		[Test]
+		[Ignore]
+		public void TestOpenssl()
+		{
+			const string xml = @"
+<Peach>
+	<StateModel name='SM' initialState='Initial'>
+		<State name='Initial'>
+			<Action type='output'>
+				<DataModel name='DM'>
+					<String value='GET / HTTP/1.0\r\n\r\n' />
+				</DataModel>
+			</Action>
+			<Action type='input'>
+				<DataModel name='DM'>
+					<String />
+				</DataModel>
+			</Action>
+		</State>
+	</StateModel>
+
+	<Test name='Default'>
+		<StateModel ref='SM' />
+		<Publisher class='Ssl'>
+			<Param name='Host' value='127.0.0.1' />
+			<Param name='Port' value='44330' />
+		</Publisher>
+	</Test>
+</Peach>
+";
+
+			var dom = DataModelCollector.ParsePit(xml);
+			var cfg = new RunConfiguration { singleIteration = true };
+			var e = new Engine(null);
+
+			e.startFuzzing(dom, cfg);
+
+			var dm = dom.tests[0].stateModel.states[0].actions[1].dataModel;
+
+			Console.WriteLine(dm.InternalValue.BitsToString());
+		}
 	}
 }
