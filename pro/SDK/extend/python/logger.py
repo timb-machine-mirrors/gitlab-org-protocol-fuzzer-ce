5c0398cb21c20828a16cd57559618241c6e0665d
Seth Hinze <seth@peachfuzzer.com>
2016-01-26 13:19:06 -0800

    Fixing up python sdk examples.

--- pro/SDK/extend/python/logger.py
+++ pro/SDK/extend/python/logger.py
@@ -10,7 +10,7 @@ clr.AddReference("Peach.Pro")
 import Peach.Core
 from Peach.Core import Variant, Logger, RunContext
 from Peach.Core.Dom import Block, String, DataElement
-from Peach.Pro.Loggers import BasePythonLogger
+from Peach.Pro.Core.Loggers import BasePythonLogger
 
 # Add the special assembly that our Python extensions will 
 # appear in. This is the list of assemblies that Peach checks
@@ -27,28 +27,28 @@ ParameterAttr = clrtype.attribute(Peach.Core.ParameterAttribute)
 
 class PythonLogger(BasePythonLogger):
 	'''Example of adding a custom Logger to Peach using only Python'''
-	
+
 	__metaclass__ = clrtype.ClrClass
 	_clrnamespace = "PythonExamples"   
-	
+
 	_clrclassattribs = [
 		SerializableAttr(),
 		LoggerAttr("PythonLogger", True),
 		DescriptionAttr("Example Logger in Python"),
 	]
-	
+
 	@clrtype.accepts(System.Collections.Generic.Dictionary[clr.GetClrType(str), Variant])
 	def __init__(self, parent, args):
 		pass
-	
+
 	@clrtype.accepts(RunContext)
 	def Engine_TestStarting(self, context):
 		print ">> TEST STARTING"
-	
+
 	@clrtype.accepts(RunContext)
 	def Engine_TestFinished(self, context):
 		print ">> TEST FINISHED"
-	
+
 	@clrtype.accepts(RunContext, System.UInt32, System.UInt32)
 	def Engine_IterationStarting(self, context, currentIteration, totalIterations):
 		print ">> ITERATION STARTING: " + str(currentIteration)
