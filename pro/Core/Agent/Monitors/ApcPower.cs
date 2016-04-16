8f99251b39dd110b5e34736efec53bd24672f91d
Jake Mitchell <jake@peachfuzzer.com>
2016-04-15 20:05:40 -0700

    PF-10: utility classes for controlling power outlets via SNMP
    Utility classes are marked `internal` because they aren't meant to be
    used directly by the customer. Collectively these classes encapsulate
    the side-effects. This should simplify the `Monitor` implementation.
    
    Tests include mock SNMP agents and an actual SNMP agent in the form of
    an APC Power Distribution Unit. The APC PDU test is ignored until a
    robust long-term testing strategy is implemented. For the sake of
    testing, internals in `Pro.Peach` assembly are now visible to
    `Peach.Pro.Test`.
    
    The new classes introduce a new dependency called #SNMP
    (https://sharpsnmplib.codeplex.com/). By design, none of its API leaks
    into the interfaces for these internal classes, so #SNMP could someday
    be replaced without affecting much else. If we stick with #SNMP, I'll
    need to add it to the `3rdParty` directory and waf toolchain.

---
+++ pro/Core/Agent/Monitors/ApcPower.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Linq;
+using Peach.Core;
+using Peach.Core.Agent;
+using NLog;
+using System.Collections.Generic;
+using Monitor = Peach.Core.Agent.Monitor2;
+using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
+using System.Threading;
+using System.Diagnostics;
+using Lextm.SharpSnmpLib.Messaging;
+using Lextm.SharpSnmpLib;
+using System.Net;
+
+namespace Peach.Pro.Core.Agent.Monitors
+{
+	// TODO: implement Monitor using SnmpPowerDistributionUnit
+	// TODO: test more devices
+	// TODO: integrate #SNMP lib into 3rdParty
+
+	[Monitor("ApcPower")]
+	[Description("Controls an APC Switched Power Distribution Unit")]
+	public class ApcPower : Monitor
+	{
+		static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+		public ApcPower(string name)
+			: base(name)
+		{
+			Logger.Debug("Constructed!");
+		}
+	}
+
+
+	internal enum SwitchState { On, Off, Unknown };
+
+	internal class FailedStateChangeArgs : EventArgs
+	{
+		public SwitchState TargetState { get; }
+		public SwitchState CurrentState { get; }
+
+		public FailedStateChangeArgs(SwitchState target, SwitchState current)
+		{
+			TargetState = target;
+			CurrentState = current;
+		}
+	}
+
+	internal delegate void FailedStateChangeHandler(object sender, FailedStateChangeArgs e);
+
+	internal abstract class PowerSwitch
+	{
+		public abstract SwitchState CurrentState();
+
+		public void Switch(SwitchState targetState)
+		{
+			if (targetState == SwitchState.Unknown)
+			{
+				throw new ArgumentException("Switching to SwitchState.Unknown is not permitted!");
+			}
+
+			try
+			{
+				DoSwitch(targetState);
+			}
+			catch
+			{
+			}
+
+			var postState = CurrentState();
+			if (postState != targetState)
+			{
+				OnFailedStateChange(new FailedStateChangeArgs(targetState, postState));
+			}
+		}
+
+		protected abstract void DoSwitch(SwitchState toState);
+
+		public event FailedStateChangeHandler FailedStateChange;
+		protected virtual void OnFailedStateChange(FailedStateChangeArgs e)
+		{
+			if (FailedStateChange != null)
+				FailedStateChange(this, e);
+		}
+
+		public void Reset()
+		{
+			if (CurrentState() != SwitchState.Off)
+			{
+				Switch(SwitchState.Off);
+			}
+			Thread.Sleep(ResetPauseDuration());
+			Switch(SwitchState.On);
+		}
+
+		protected virtual int ResetPauseDuration()
+		{
+			return 100;
+		}
+	}
+
+	internal class SnmpOid
+	{
+		public string Value { get; }
+
+		public SnmpOid(string oid)
+		{
+			Value = oid;
+		}
+	}
+
+	internal interface ISnmpAgent
+	{
+		int Get(SnmpOid oid);
+
+		int Set(SnmpOid oid, int code);
+	}
+
+	internal class SnmpAgent : ISnmpAgent
+	{
+		string Host { get; }
+		int Port { get; }
+		string ReadCommunity { get; }
+		string WriteCommunity { get; }
+		int RequestTimeout { get; set; }
+
+		public SnmpAgent(string host, int port, string readCommunity, string writeCommunity)
+		{
+			Host = host;
+			Port = port;
+			ReadCommunity = readCommunity;
+			WriteCommunity = writeCommunity;
+		}
+
+		public int Get(SnmpOid oid)
+		{
+			var response = Messenger.Get(
+				VersionCode.V1,
+				new IPEndPoint(IPAddress.Parse(Host), Port),
+				new OctetString(ReadCommunity),
+				new List<Variable> {
+					new Variable(new ObjectIdentifier(oid.Value))
+				},
+				RequestTimeout);
+			// TODO: handle unexpected response
+			return ((Integer32)response[0].Data).ToInt32();
+		}
+
+		public int Set(SnmpOid oid, int code)
+		{
+			var response = Messenger.Set(
+				VersionCode.V1,
+				new IPEndPoint(IPAddress.Parse(Host), Port),
+				new OctetString(WriteCommunity),
+				new List<Variable> {
+					new Variable(new ObjectIdentifier(oid.Value), new Integer32(code))
+				},
+				RequestTimeout);
+			// TODO: handle unexpected response
+			return ((Integer32)response[0].Data).ToInt32();
+		}
+	}
+
+	internal class SnmpPowerSwitch : PowerSwitch
+	{
+		const int OnCode = 1;
+		const int OffCode = 2;
+
+		ISnmpAgent Agent { get; }
+		SnmpOid Oid { get; }
+
+		public SnmpPowerSwitch(ISnmpAgent agent, SnmpOid oid)
+		{
+			Agent = agent;
+			Oid = oid;
+		}
+
+		private SwitchState DecodeState(int stateCode)
+		{
+			switch (stateCode) {
+				case OnCode:
+					return SwitchState.On;
+				case OffCode:
+					return SwitchState.Off;
+				default:
+					return SwitchState.Unknown;
+			}
+		}
+
+		public override SwitchState CurrentState()
+		{
+			try
+			{
+				return DecodeState(Agent.Get(Oid));
+			}
+			catch
+			{
+				return SwitchState.Unknown;
+			}
+		}
+
+		protected override void DoSwitch(SwitchState toState)
+		{
+			if (toState == SwitchState.Unknown)
+				return;
+			Agent.Set(Oid, toState == SwitchState.On ? OnCode : OffCode);
+		}
+	}
+
+	internal class SnmpPowerDistributionUnit : PowerSwitch
+	{
+		IEnumerable<SnmpPowerSwitch> PowerSwitches { get; }
+
+		public SnmpPowerDistributionUnit(ISnmpAgent agent, IEnumerable<SnmpOid> oids)
+		{
+			PowerSwitches = from oid in oids
+			                select new SnmpPowerSwitch(agent, oid);
+			AttachEventHandlers();
+		}
+
+		private void AttachEventHandlers()
+		{
+			foreach (var outlet in PowerSwitches)
+			{
+				outlet.FailedStateChange += new FailedStateChangeHandler(FailedOutletSwitchAttempt);
+			}
+		}
+
+		private void FailedOutletSwitchAttempt(object sender, FailedStateChangeArgs e)
+		{
+			OnFailedStateChange(e);
+		}
+
+		public override SwitchState CurrentState()
+		{
+			if (PowerSwitches.Count() == 0)
+			{
+				return SwitchState.Off;
+			}
+
+			var states = from sw in PowerSwitches
+			             select sw.CurrentState();
+			var allSame = states.All(s => s == states.First());
+			return allSame ? states.First() : SwitchState.Unknown;
+		}
+
+		protected override void DoSwitch(SwitchState toState)
+		{
+			foreach (var outlet in PowerSwitches)
+			{
+				outlet.Switch(toState);
+			}
+		}
+	}
+}
+
---
+++ pro/Test/Core/Monitors/SnmpPowerDistributionUnitTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using NUnit.Framework;
+using Lextm.SharpSnmpLib.Messaging;
+using Lextm.SharpSnmpLib;
+using System.Collections.Generic;
+using System.Net;
+using Peach.Pro.Core.Agent.Monitors;
+
+namespace Peach.Pro.Test.Core.Monitors
+{
+	class MockSnmpAgent : ISnmpAgent
+	{
+		Dictionary<SnmpOid, int> _state = new Dictionary<SnmpOid, int>();
+
+		public int Get(SnmpOid oid)
+		{
+			return _state[oid];
+		}
+
+		public int Set(SnmpOid oid, int code)
+		{
+			_state[oid] = code;
+			return _state[oid];
+		}
+	}
+
+	class MockSnmpAgentReadOnly : ISnmpAgent
+	{
+		public int Get(SnmpOid oid)
+		{
+			return -1;  // TODO: change API to ensure this decodes to PowerState.Unknown
+		}
+
+		public int Set(SnmpOid oid, int code)
+		{
+			throw new Exception("Expected failure; agent is read-only for testing");
+		}
+	}
+
+	[TestFixture]
+	class SnmpPowerDistributionUnitTests
+	{
+		List<SnmpOid> oids = new List<SnmpOid>() {
+			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.6"),
+			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.7"),
+			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.8"),
+		};
+
+		#region MockedSnmpAgent
+		[Test]
+		public void TurnOffSnmpPdu()
+		{
+			var spdu = new SnmpPowerDistributionUnit(new MockSnmpAgent(), oids);
+			spdu.Switch(SwitchState.Off);
+			Assert.AreEqual(SwitchState.Off, spdu.CurrentState());
+		}
+
+		[Test]
+		public void TurnOnSnmpPdu()
+		{
+			var spdu = new SnmpPowerDistributionUnit(new MockSnmpAgent(), oids);
+			spdu.Switch(SwitchState.On);
+			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+		}
+
+		[Test]
+		public void ResetSnmpPdu()
+		{
+			var spdu = new SnmpPowerDistributionUnit(new MockSnmpAgent(), oids);
+			spdu.Reset();
+			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+		}
+
+		[Test]
+		public void TestFailedStateChangeEvent()
+		{
+			var readOnlySpdu = new SnmpPowerDistributionUnit(new MockSnmpAgentReadOnly(), oids);
+
+			var enteredHandler = false;
+			readOnlySpdu.FailedStateChange += (object sender, FailedStateChangeArgs e) => {
+				enteredHandler = true;
+
+				Assert.AreEqual(SwitchState.Off, e.TargetState);
+				Assert.AreEqual(SwitchState.Unknown, e.CurrentState);
+			};
+
+			readOnlySpdu.Switch(SwitchState.Off);
+
+			Assert.True(enteredHandler);
+		}
+		#endregion
+
+
+		#region RealSnmpAgent
+		[Test]
+		[Ignore("Assumes APC Power Distribution Unit is ready and waiting")]
+		// TODO: dynamically start a real test SnmpAgent
+		public void TestRealSnmpAgent()
+		{
+			var host = "10.0.1.101";
+			var port = 161;
+			var spdu = new SnmpPowerDistributionUnit(
+				new SnmpAgent(
+					host,
+					port,
+					"public",
+					"private"),
+				oids);
+
+			spdu.FailedStateChange += (object sender, FailedStateChangeArgs e) => {
+				Assert.Fail("Failed to change to state {0}. Currently in state {1}.",
+					e.TargetState,
+					e.CurrentState);
+			};
+
+			spdu.Switch(SwitchState.On);
+			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+
+			spdu.Switch(SwitchState.Off);
+			Assert.AreEqual(SwitchState.Off, spdu.CurrentState());
+
+			spdu.Reset();
+			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+		}
+		#endregion
+	}
+}
+
