489bc0dba66b8557183935ec02a2434191b680b5
Seth Hinze <seth@peachfuzzer.com>
2016-04-18 15:47:21 -0700

    Fixing more unit test failures.

--- core/Test/ObjectCopierTests.cs
+++ core/Test/ObjectCopierTests.cs
@@ -399,7 +399,8 @@ namespace Peach.Core.Test
 			sw.Stop();
 			Assert.NotNull(copy);
 			Assert.AreEqual(copy.Count, list2.Count);
-			Assert.LessOrEqual(sw.ElapsedMilliseconds, list1Elaps*list1Elaps);
+			var min = Math.Max(4000, list1Elaps * list1Elaps);
+			Assert.LessOrEqual(sw.ElapsedMilliseconds, min);
 		}
 
 		bool VerifyField(FieldInfo field, Type type)
