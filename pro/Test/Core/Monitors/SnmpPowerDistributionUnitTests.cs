f6951dea91237e25e6e204c03be78d798d712554
Jake Mitchell <jake@peachfuzzer.com>
2016-04-19 14:34:26 -0700

    PF-10: additional tests

--- pro/Test/Core/Monitors/SnmpPowerDistributionUnitTests.cs
+++ pro/Test/Core/Monitors/SnmpPowerDistributionUnitTests.cs
@@ -5,6 +5,10 @@ using Lextm.SharpSnmpLib;
 using System.Collections.Generic;
 using System.Net;
 using Peach.Pro.Core.Agent.Monitors;
+using System.Diagnostics;
+using Peach.Core.Test;
+using System.Collections.Concurrent;
+using Peach.Core;
 
 namespace Peach.Pro.Test.Core.Monitors
 {
@@ -42,17 +46,17 @@ namespace Peach.Pro.Test.Core.Monitors
 	[TestFixture]
 	class SnmpPowerDistributionUnitTests
 	{
-		List<SnmpOid> oids = new List<SnmpOid>() {
+		static IReadOnlyList<SnmpOid> OIDs = new List<SnmpOid>() {
 			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.6"),
 			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.7"),
 			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.8"),
-		};
+		}.AsReadOnly();
 
 		#region FakedSnmpAgent
 		[Test]
 		public void TurnOffSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), oids);
+			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
 			spdu.Switch(SwitchState.Off);
 			Assert.AreEqual(SwitchState.Off, spdu.CurrentState());
 		}
@@ -60,7 +64,7 @@ namespace Peach.Pro.Test.Core.Monitors
 		[Test]
 		public void TurnOnSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), oids);
+			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
 			spdu.Switch(SwitchState.On);
 			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
 		}
@@ -68,15 +72,39 @@ namespace Peach.Pro.Test.Core.Monitors
 		[Test]
 		public void ResetSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), oids);
+			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
 			spdu.Reset();
 			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
 		}
 
 		[Test]
+		public void ResetSnmpPduPauseDuration()
+		{
+			const int percentMarginOfError = 5;
+
+			var shortDuration = 1;
+			var longDuration = 500;
+
+			var fastResetSpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs, shortDuration);
+			var slowResetSpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs, longDuration);
+
+			var timer = new Stopwatch();
+
+			timer.Start(); fastResetSpdu.Reset(); timer.Stop();
+			var fastTime = timer.ElapsedMilliseconds;
+
+			timer.Restart(); slowResetSpdu.Reset();	timer.Stop();
+			var slowTime = timer.ElapsedMilliseconds;
+
+			Assert.That(
+				fastTime / slowTime,
+				Is.EqualTo(shortDuration / longDuration).Within(percentMarginOfError).Percent);
+		}
+
+		[Test]
 		public void TestFailedStateChangeEvent()
 		{
-			var readOnlySpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgentReadOnly(), oids);
+			var readOnlySpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgentReadOnly(), OIDs);
 
 			var enteredHandler = false;
 			readOnlySpdu.StateChangeAttempt += (object sender, StateChangeAttemptArgs e) => {
@@ -95,21 +123,17 @@ namespace Peach.Pro.Test.Core.Monitors
 
 
 		#region RealSnmpAgent
-		[Test]
-		[Ignore("Assumes APC Power Distribution Unit is ready and waiting")]
-		// TODO: dynamically start a real test SnmpAgent
-		public void TestRealSnmpAgent()
-		{
-			var host = "10.0.1.101";
-			var port = 161;
-			var spdu = new SnmpPowerDistributionUnit(
+		SnmpPowerDistributionUnit APC_SWITCHED_RACK_POWER_DISTRIBUTION_UNIT_AP7900 =
+			new SnmpPowerDistributionUnit(
 				new SnmpAgent(
-					host,
-					port,
+					"10.0.1.101",
+					161,
 					"public",
 					"private"),
-				oids);
+				OIDs);
 
+		private void RunTestAgainstActualDevice(SnmpPowerDistributionUnit spdu)
+		{
 			spdu.StateChangeAttempt += (object sender, StateChangeAttemptArgs e) => {
 				if (e.Successful)
 				{
@@ -132,7 +156,69 @@ namespace Peach.Pro.Test.Core.Monitors
 			spdu.Reset();
 			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
 		}
+
+		[Test]
+		[Ignore("Assumes APC Power Distribution Unit is ready and waiting")]
+		// TODO: dynamically start a real test SnmpAgent
+		public void TestApcSwitchedRackPowerDistributionUnit()
+		{
+			RunTestAgainstActualDevice(APC_SWITCHED_RACK_POWER_DISTRIBUTION_UNIT_AP7900);
+		}
 		#endregion
+	}
+
+	[TestFixture]
+	class ApcPowerMonitorTests
+	{
+		private ConcurrentQueue<string> _history;
+
+		private MonitorRunner SnmpPowerRunner(Dictionary<string, string> args)
+		{
+			args.Add("Host", "10.0.1.101");
+			args.Add("Port", "161");
+			args.Add("OIDs", ".1.3.6.1.4.1.318.1.1.4.4.2.1.3.6,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.7,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.8");
+			// TODO: check for other important args
+
+			var runner = new MonitorRunner("ApcPower", args)
+				{
+					SessionStarting = m =>
+						{
+							m.InternalEvent += (s, e) =>
+								{
+									var sce = (StateChangeAttemptArgs)e;
+									var monitor = (ApcPowerMonitor)s;
+
+									_history.Enqueue("Port {0}: {1}".Fmt(monitor.Port, sce.CurrentState));
+								};
+
+							m.SessionStarting();
+						},
+					DetectedFault = m =>
+						{
+							Assert.False(m.DetectedFault(), "Monitor should not have detected fault");
+							// Force data collection
+							return true;
+						}
+				};
+			return runner;
+		}
+
+		[Test]
+		public void TestSnmpPowerBasic()
+		{
+			var runner = SnmpPowerRunner(new Dictionary<string, string>());
+			var faults = runner.Run();
+			Assert.AreEqual(0, faults.Length, "Faults length mismatch");
+
+			var expected = new[]
+				{
+					"Port 161: On",
+					"Port 161: Off",
+					"Port 161: On",
+				};
+
+			Assert.That(_history.ToArray(), Is.EqualTo(expected), "History mismatch");
+		}
 	}
 }
 
