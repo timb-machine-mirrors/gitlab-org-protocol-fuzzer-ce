9370b35c5345e2cbdb462553d99e021c951bec9f
Jake Mitchell <jake@peachfuzzer.com>
2016-04-22 12:30:13 -0700

    PF-10: SnmpPowerMonitor code/test improvements

--- pro/Test/Core/Monitors/SnmpPowerDistributionUnitTests.cs
+++ pro/Test/Core/Monitors/SnmpPowerDistributionUnitTests.cs
@@ -14,14 +14,14 @@ namespace Peach.Pro.Test.Core.Monitors
 {
 	class FakeSnmpAgent : ISnmpAgent
 	{
-		Dictionary<SnmpOid, int> _state = new Dictionary<SnmpOid, int>();
+		Dictionary<string, int> _state = new Dictionary<string, int>();
 
-		public int Get(SnmpOid oid)
+		public int Get(string oid)
 		{
 			return _state[oid];
 		}
 
-		public int Set(SnmpOid oid, int code)
+		public int Set(string oid, int code)
 		{
 			_state[oid] = code;
 			return _state[oid];
@@ -32,12 +32,12 @@ namespace Peach.Pro.Test.Core.Monitors
 	{
 		int _unrecognizedSwitchState = -1;
 
-		public int Get(SnmpOid oid)
+		public int Get(string oid)
 		{
 			return _unrecognizedSwitchState;
 		}
 
-		public int Set(SnmpOid oid, int code)
+		public int Set(string oid, int code)
 		{
 			throw new Exception("Expected failure; agent is read-only for testing");
 		}
@@ -46,49 +46,66 @@ namespace Peach.Pro.Test.Core.Monitors
 	[TestFixture]
 	class SnmpPowerDistributionUnitTests
 	{
-		static IReadOnlyList<SnmpOid> OIDs = new List<SnmpOid>() {
-			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.6"),
-			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.7"),
-			new SnmpOid(".1.3.6.1.4.1.318.1.1.4.4.2.1.3.8"),
+		static IReadOnlyList<string> OIDs = new List<string>() {
+			".1.3.6.1.4.1.318.1.1.4.4.2.1.3.1",
+			".1.3.6.1.4.1.318.1.1.4.4.2.1.3.2",
+			".1.3.6.1.4.1.318.1.1.4.4.2.1.3.3",
 		}.AsReadOnly();
+		const int OnCode = 1;
+		const int OffCode = 2;
+
+		void AssertAgentState(SnmpPowerDistributionUnit spdu, OutletState targetState)
+		{
+			foreach (var oid in OIDs)
+			{
+				Assert.AreEqual(spdu.EncodeState(targetState), spdu.Agent.Get(oid));
+			}
+		}
 
 		#region FakedSnmpAgent
 		[Test]
 		public void TurnOffSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
-			spdu.Switch(SwitchState.Off);
-			Assert.AreEqual(SwitchState.Off, spdu.CurrentState());
+			var agent = new FakeSnmpAgent();
+			var spdu = new SnmpPowerDistributionUnit(agent, OIDs, OnCode, OffCode);
+
+			spdu.Switch(OutletState.Off);
+			AssertAgentState(spdu, OutletState.Off);
 		}
 
 		[Test]
 		public void TurnOnSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
-			spdu.Switch(SwitchState.On);
-			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+			var agent = new FakeSnmpAgent();
+			var spdu = new SnmpPowerDistributionUnit(agent, OIDs, OnCode, OffCode);
+
+			spdu.Switch(OutletState.On);
+			AssertAgentState(spdu, OutletState.On);
 		}
 
 		[Test]
 		public void ResetSnmpPdu()
 		{
-			var spdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs);
+			var agent = new FakeSnmpAgent();
+			var spdu = new SnmpPowerDistributionUnit(agent, OIDs, OnCode, OffCode);
+
 			spdu.Reset();
-			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
+			AssertAgentState(spdu, OutletState.On);
 		}
 
 		[Test]
 		public void ResetSnmpPduPauseDuration()
 		{
 			const int percentMarginOfError = 5;
+			var timer = new Stopwatch();
 
 			var shortDuration = 1;
 			var longDuration = 500;
 
-			var fastResetSpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs, shortDuration);
-			var slowResetSpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs, longDuration);
-
-			var timer = new Stopwatch();
+			Func<int, SnmpPowerDistributionUnit> newSpdu = (int duration) =>
+				new SnmpPowerDistributionUnit(new FakeSnmpAgent(), OIDs, OnCode, OffCode, duration);
+			var fastResetSpdu = newSpdu(shortDuration);
+			var slowResetSpdu = newSpdu(longDuration);
 
 			timer.Start(); fastResetSpdu.Reset(); timer.Stop();
 			var fastTime = timer.ElapsedMilliseconds;
@@ -104,91 +121,67 @@ namespace Peach.Pro.Test.Core.Monitors
 		[Test]
 		public void TestFailedStateChangeEvent()
 		{
-			var readOnlySpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgentReadOnly(), OIDs);
+			var readOnlySpdu = new SnmpPowerDistributionUnit(new FakeSnmpAgentReadOnly(), OIDs, OnCode, OffCode);
 
-			var enteredHandler = false;
-			readOnlySpdu.StateChangeAttempt += (object sender, StateChangeAttemptArgs e) => {
-				enteredHandler = true;
-
-				Assert.AreEqual(SwitchState.Off, e.TargetState);
-				Assert.AreEqual(SwitchState.Unknown, e.CurrentState);
-				Assert.IsFalse(e.Successful);
-			};
-
-			readOnlySpdu.Switch(SwitchState.Off);
-
-			Assert.True(enteredHandler);
+			Assert.Throws<Exception>(() => readOnlySpdu.Switch(OutletState.Off));
 		}
 		#endregion
 
 
 		#region RealSnmpAgent
-		SnmpPowerDistributionUnit APC_SWITCHED_RACK_POWER_DISTRIBUTION_UNIT_AP7900 =
-			new SnmpPowerDistributionUnit(
-				new SnmpAgent(
-					"10.0.1.101",
-					161,
-					"public",
-					"private"),
-				OIDs);
-
-		private void RunTestAgainstActualDevice(SnmpPowerDistributionUnit spdu)
-		{
-			spdu.StateChangeAttempt += (object sender, StateChangeAttemptArgs e) => {
-				if (e.Successful)
-				{
-					Assert.AreEqual(e.TargetState, e.CurrentState);
-				}
-				else
-				{
-					Assert.Fail("Failed to change to state {0}. Currently in state {1}.",
-						e.TargetState,
-						e.CurrentState);
-				}
-			};
-
-			spdu.Switch(SwitchState.On);
-			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
-
-			spdu.Switch(SwitchState.Off);
-			Assert.AreEqual(SwitchState.Off, spdu.CurrentState());
-
-			spdu.Reset();
-			Assert.AreEqual(SwitchState.On, spdu.CurrentState());
-		}
-
 		[Test]
-		[Ignore("Assumes APC Power Distribution Unit is ready and waiting")]
-		// TODO: dynamically start a real test SnmpAgent
+		[Ignore("APC Power Distribution Unit must be set up for test")]
+		/*
+		 * Directions for setting up test device:
+		 *
+		 *   1. Power on APC Switched Racke Power Distribution Unit (AP7900).
+		 *   2. Connect it to the LAN via ethernet cable.
+		 *   3. Using its MAC address, assign it an unused IP address in the local ARP table:
+		 *        $ sudo arp -s 10.0.1.101 00:c0:b7:8a:13:3b
+		 *   4. Send it a 113-byte ping to make it aware of the assigned IP:
+		 *        $ ping 10.0.1.101 -s 113
+		 *   5. If the test fails at this point, install APC's powernet417.mib and try:
+		 *        $ snmpwalk -v 1 -c public 10.0.1.101 enterprises.318
+		 *   6. Confirm 'PowerNet-MIB::sPDUOutletCtl.1 = INTEGER:' is in the output.
+		 */
 		public void TestApcSwitchedRackPowerDistributionUnit()
 		{
-			RunTestAgainstActualDevice(APC_SWITCHED_RACK_POWER_DISTRIBUTION_UNIT_AP7900);
+			var agent = new SnmpAgent("10.0.1.101",	161, "public", "private", 1000);
+			var apc = new SnmpPowerDistributionUnit(agent, OIDs, OnCode, OffCode);
+
+			Assert.DoesNotThrow(() => apc.SanityCheck(timeout: 3000));
 		}
 		#endregion
 	}
 
 	[TestFixture]
-	class ApcPowerMonitorTests
+	class SnmpPowerMonitorTests
 	{
-		private ConcurrentQueue<string> _history;
+		private ConcurrentQueue<string> _history = new ConcurrentQueue<string>();
 
 		private MonitorRunner SnmpPowerRunner(Dictionary<string, string> args)
 		{
-			args.Add("Host", "10.0.1.101");
-			args.Add("Port", "161");
-			args.Add("OIDs", ".1.3.6.1.4.1.318.1.1.4.4.2.1.3.6,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.7,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.8");
-			// TODO: check for other important args
+			var defaultArgs = new Dictionary<string, string>
+			{
+				{ "Host", "10.0.1.101" },
+				{ "OIDs", ".1.3.6.1.4.1.318.1.1.4.4.2.1.3.1,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.2,.1.3.6.1.4.1.318.1.1.4.4.2.1.3.3" },
+			};
 
-			var runner = new MonitorRunner("ApcPower", args)
+			defaultArgs.ForEach(pair =>
+				{
+					if (!args.ContainsKey(pair.Key))
+						args.Add(pair.Key, pair.Value);
+				});
+
+			var runner = new MonitorRunner("SnmpPower", args)
 				{
 					SessionStarting = m =>
 						{
 							m.InternalEvent += (s, e) =>
 								{
 									var sce = (StateChangeAttemptArgs)e;
-									var monitor = (ApcPowerMonitor)s;
 
-									_history.Enqueue("Port {0}: {1}".Fmt(monitor.Port, sce.CurrentState));
+									_history.Enqueue(sce.TargetState.ToString());
 								};
 
 							m.SessionStarting();
@@ -196,7 +189,6 @@ namespace Peach.Pro.Test.Core.Monitors
 					DetectedFault = m =>
 						{
 							Assert.False(m.DetectedFault(), "Monitor should not have detected fault");
-							// Force data collection
 							return true;
 						}
 				};
@@ -204,6 +196,8 @@ namespace Peach.Pro.Test.Core.Monitors
 		}
 
 		[Test]
+		[Ignore("APC Power Distribution Unit must be set up for test")]
+		/* See directions above for TestApcSwitchedRackPowerDistributionUnit */
 		public void TestSnmpPowerBasic()
 		{
 			var runner = SnmpPowerRunner(new Dictionary<string, string>());
@@ -212,12 +206,61 @@ namespace Peach.Pro.Test.Core.Monitors
 
 			var expected = new[]
 				{
-					"Port 161: On",
-					"Port 161: Off",
-					"Port 161: On",
+					"On",
+					"Off",
+					"On",
 				};
 
 			Assert.That(_history.ToArray(), Is.EqualTo(expected), "History mismatch");
+		}
+
+		[Test]
+		public void ExceptionForMissingOIDs()
+		{
+			var runner = SnmpPowerRunner(new Dictionary<string, string>
+				{
+					{ "OIDs", "" },
+				});
+
+			Assert.Throws(Is.TypeOf<PeachException>().And.Message.Contains("Expected at least one OID"),
+				() => runner.Run());
+		}
+
+		[Test]
+		public void ExceptionForInvalidOID()
+		{
+			var runner = SnmpPowerRunner(new Dictionary<string, string>
+				{
+					{ "OIDs", "xyz" },
+				});
+
+			Assert.Throws(Is.TypeOf<PeachException>().And.Message.Contains("Invalid SNMP OID 'xyz'"),
+				() => runner.Run());
+		}
+
+		[Test]
+		public void UnavailableOID()
+		{
+			var runner = SnmpPowerRunner(new Dictionary<string, string>
+				{
+					{ "OIDs", ".1.3.6.1.4.1.318.1.1.4.4.2.1.3.999" },
+				});
+
+			Assert.Throws(Is.TypeOf<PeachException>().And.Message.Contains("Failed to set state of SNMP OID '.1.3.6.1.4.1.318.1.1.4.4.2.1.3.999' (agent '10.0.1.101', port: '161', community: 'private'"),
+				() => runner.Run());
+		}
+
+		[Test]
+		public void ExceptionForSameOnOffCodes()
+		{
+			var runner = SnmpPowerRunner(new Dictionary<string, string>
+				{
+					{ "OnCode", "5" },
+					{ "OffCode", "5" },
+				});
+
+			Assert.Throws(Is.TypeOf<PeachException>().And.Message.Contains("OnCode and OffCode must be different, but they're both '5'"),
+				() => runner.Run());
 		}
 	}
 }
