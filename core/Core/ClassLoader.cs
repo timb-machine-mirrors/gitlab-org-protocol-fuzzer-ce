0b7bf270c6eb8d67e073eb5afdee0f82fb9a474f
Seth Hinze <seth@peachfuzzer.com>
2016-04-29 17:54:37 -0700

    Use the assembly location as the key. Fixes duplicate entries of assemblies.

--- core/Core/ClassLoader.cs
+++ core/Core/ClassLoader.cs
@@ -181,7 +181,7 @@ namespace Peach.Core
 					{
 						var asm = Load(file);
 						asm.GetTypes(); // make sure we can load exported types.
-						AssemblyCache.Add(file, asm);
+						AssemblyCache[asm.Location] = asm;
 					}
 					catch (Exception ex)
 					{
@@ -270,7 +270,7 @@ namespace Peach.Core
 				}
 
 				asm.GetExportedTypes(); // make sure we can load exported types.
-				AssemblyCache.Add(fullPath, asm);
+				AssemblyCache[asm.Location] = asm;
 			}
 
 			return true;
