16bb5ecfac2728d67040efabef8dd8be93ccd6b7
Frank Laub <frank.laub@gmail.com>
2016-05-11 21:29:37 -0700

    Protect pit resources via AES encryption

--- pro/Test/Core/PitParserTests/EmbeddedTests.cs
+++ pro/Test/Core/PitParserTests/EmbeddedTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -19,18 +20,9 @@ namespace Peach.Pro.Test.Core.PitParserTests
 		static readonly Assembly _asm = Assembly.GetExecutingAssembly();
 		const string PitsResourcePrefix = "Peach.Pro.Test.Core.Resources.Pits";
 
-		public class PitManifest : Dictionary<string, PitManifestEntry> { }
-
-		public class PitManifestEntry
-		{
-			public string Pit { get; set; }
-			public string[] Assets { get; set; }
-		}
-
 		[Test]
-		public void BasicTest()
+		public void TestLoadFromAssembly()
 		{
-
 			using (var tmpDir = new TempDirectory())
 			{
 				ExtractFile(tmpDir.Path, "DNP3_Slave.xml", "Net");
@@ -59,14 +51,69 @@ namespace Peach.Pro.Test.Core.PitParserTests
 		}
 
 		[Test]
+		public void TestLoadFromDisk()
+		{
+			using (var tmpDir = new TempDirectory())
+			{
+				ExtractFile(tmpDir.Path, "DNP3_Slave.xml", "Net");
+				ExtractFile(tmpDir.Path, "DNP3_Slave.xml.config", "Net");
+				ExtractFile(tmpDir.Path, "DNP3.py", "_Common", "Models", "Net");
+				ExtractDirectory(tmpDir.Path, "_Common", "Models", "Net");
+				ExtractDirectory(tmpDir.Path, "_Common", "Samples", "Net", "DNP3");
+
+				var pitFile = Path.Combine(tmpDir.Path, "Net", "DNP3_Slave.xml");
+				var pitConfigFile = pitFile + ".config";
+
+				var defs = PitDefines.ParseFile(pitConfigFile, tmpDir.Path, new Dictionary<string, string> {
+					{"Source", "0"},
+					{"Destination", "0"},
+				});
+
+				var args = new Dictionary<string, object> {
+					{ PitParser.DEFINED_VALUES, defs.Evaluate() }
+				};
+
+				var parser = new ProPitParser(tmpDir.Path);
+				var dom = parser.asParser(args, pitFile);
+				var config = new RunConfiguration() { singleIteration = true, };
+				var e = new Engine(null);
+				e.startFuzzing(dom, config);
+			}
+		}
+
+		[Test]
 		public void ParseManifest()
 		{
-			using (var stream = _asm.GetManifestResourceStream(PitsResourcePrefix + ".manifest.json"))
-			using (var reader = new StreamReader(stream))
-			using (var json = new JsonTextReader(reader))
+			var manifest = PitResourceLoader.LoadManifest(_asm, PitsResourcePrefix);
+			CollectionAssert.Contains(manifest.Features.Keys, "PeachPit-Net-DNP3_Slave");
+		}
+
+		[Test]
+		public void TestProtectResources()
+		{
+			using (var tmpDir = new TempDirectory())
 			{
-				var manifest = JsonUtilities.CreateSerializer().Deserialize<PitManifest>(json);
-				CollectionAssert.Contains(manifest.Keys, "PeachPit-Net-DNP3_Slave");
+				var password = "password";
+				var resourceName = "Net.DNP3_Slave.xml";
+				var expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+				var encrypted = Path.Combine(tmpDir.Path, "TestProtectResources.dll");
+				PitResourceLoader.EncryptResources(Assembly.GetExecutingAssembly(), encrypted, password);
+
+				var asm = Assembly.LoadFile(encrypted);
+				var name = PitResourceLoader.MakeFullName(PitsResourcePrefix, resourceName);
+				using (var stream = asm.GetManifestResourceStream(name))
+				using (var reader = new StreamReader(stream))
+				{
+					var actual = reader.ReadLine();
+					Assert.AreNotEqual(expected, actual);
+				}
+
+				using (var stream = PitResourceLoader.DecryptResource(asm, PitsResourcePrefix, resourceName, password))
+				using (var reader = new StreamReader(stream))
+				{
+					var actual = reader.ReadLine();
+					Assert.AreEqual(expected, actual);
+				}
 			}
 		}
 
@@ -77,7 +124,10 @@ namespace Peach.Pro.Test.Core.PitParserTests
 			Directory.CreateDirectory(dir);
 
 			var asm = Assembly.GetExecutingAssembly();
-			var prefix = string.Join(".", new[] { PitsResourcePrefix }.Concat(parts));
+			var prefix = string.Join(".",
+				new[] { PitsResourcePrefix }
+				.Concat(parts)
+			);
 			foreach (var name in asm.GetManifestResourceNames())
 			{
 				if (name.StartsWith(prefix))
@@ -96,7 +146,7 @@ namespace Peach.Pro.Test.Core.PitParserTests
 			Directory.CreateDirectory(dir);
 
 			var asm = Assembly.GetExecutingAssembly();
-			var name = string.Join(".", 
+			var name = string.Join(".",
 				new[] { PitsResourcePrefix }
 				.Concat(dirs)
 				.Concat(new[] { filename })
---
+++ pro/Test/Core/PitResource.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using Peach.Pro.Core;
+
+namespace Peach.Pro.Test.Core
+{
+	public class PitManifest
+	{
+		public byte[] Salt { get; set; }
+		public byte[] IV { get; set; }
+		public Dictionary<string, PitManifestFeature> Features { get; set; }
+	}
+
+	public class PitManifestFeature
+	{
+		public string Pit { get; set; }
+		public string[] Assets { get; set; }
+	}
+
+	public static class PitResourceLoader
+	{
+		public static PitManifest LoadManifest(Assembly asm, string prefix)
+		{
+			var name = MakeFullName(prefix, "manifest.json");
+			using (var stream = asm.GetManifestResourceStream(name))
+				return LoadManifest(stream);
+		}
+
+		internal static string MakeFullName(string prefix, string name)
+		{
+			var parts = new List<string>();
+			if (!string.IsNullOrEmpty(prefix))
+				parts.Add(prefix);
+			parts.Add(name);
+			return string.Join(".", parts);
+		}
+
+		private static PitManifest LoadManifest(Stream stream)
+		{
+			using (var reader = new StreamReader(stream))
+			using (var json = new JsonTextReader(reader))
+			{
+				return JsonUtilities.CreateSerializer().Deserialize<PitManifest>(json);
+			}
+		}
+
+		private static void SaveManifest(Stream stream, PitManifest manifest)
+		{
+			using (var writer = new StreamWriter(stream, Encoding.UTF8, 4096, true))
+			{
+				JsonUtilities.CreateSerializer().Serialize(writer, manifest);
+			}
+		}
+
+		public static void EncryptResources(Assembly asmInput, string output, string password)
+		{
+			var dir = Path.GetDirectoryName(output);
+			var asmName = Path.GetFileNameWithoutExtension(output);
+			var fileName = Path.GetFileName(output);
+
+			var builder = AppDomain.CurrentDomain.DefineDynamicAssembly(
+				new AssemblyName(asmName),
+				System.Reflection.Emit.AssemblyBuilderAccess.Save,
+				dir
+			);
+
+			using (var cipher = new AesManaged())
+			{
+				cipher.Mode = CipherMode.CBC;
+				cipher.Padding = PaddingMode.PKCS7;
+				cipher.GenerateIV();
+
+				byte[] salt;
+				using (var pbkdf = new Rfc2898DeriveBytes(password, cipher.KeySize / 8))
+				{
+					salt = pbkdf.Salt;
+					cipher.Key = pbkdf.GetBytes(cipher.KeySize / 8);
+				}
+
+				using (var encrypter = cipher.CreateEncryptor())
+				{
+					var module = builder.DefineDynamicModule(asmName, fileName);
+					foreach (var resourceName in asmInput.GetManifestResourceNames())
+					{
+						if (resourceName.Contains("manifest.json"))
+						{
+							PitManifest manifest;
+							using (var stream = asmInput.GetManifestResourceStream(resourceName))
+							{
+								manifest = LoadManifest(stream);
+								manifest.IV = cipher.IV;
+								manifest.Salt = salt;
+							}
+
+							var ms = new MemoryStream();
+							SaveManifest(ms, manifest);
+							module.DefineManifestResource(resourceName, ms, ResourceAttributes.Public);
+						}
+						else
+						{
+							var ms = new MemoryStream();
+							using (var stream = asmInput.GetManifestResourceStream(resourceName))
+							using (var crypto = new CryptoStream(stream, encrypter, CryptoStreamMode.Read))
+							{
+								crypto.CopyTo(ms);
+								crypto.Clear();
+							}
+
+							ms.Seek(0, SeekOrigin.Begin);
+							module.DefineManifestResource(resourceName, ms, ResourceAttributes.Public);
+						}
+					}
+					cipher.Clear();
+				}
+
+				builder.Save(fileName);
+			}
+		}
+
+		public static Stream DecryptResource(Assembly asm, string prefix, string resourceName, string password)
+		{
+			var manifest = LoadManifest(asm, prefix);
+
+			var cipher = new AesManaged();
+			cipher.Mode = CipherMode.CBC;
+			cipher.Padding = PaddingMode.PKCS7;
+			cipher.IV = manifest.IV;
+
+			using (var pbkdf = new Rfc2898DeriveBytes(password, manifest.Salt))
+			{
+				cipher.Key = pbkdf.GetBytes(cipher.KeySize / 8);
+			}
+
+			var decrypter = cipher.CreateDecryptor();
+			var name = MakeFullName(prefix, resourceName);
+
+			var ms = new MemoryStream();
+			using (var stream = asm.GetManifestResourceStream(name))
+			using (var crypto = new CryptoStream(stream, decrypter, CryptoStreamMode.Read))
+			{
+				crypto.CopyTo(ms);
+				crypto.Clear();
+			}
+
+			ms.Seek(0, SeekOrigin.Begin);
+			return ms;
+		}
+	}
+}
+
