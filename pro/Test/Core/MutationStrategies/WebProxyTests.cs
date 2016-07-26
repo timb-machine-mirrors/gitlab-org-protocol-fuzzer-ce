128b33bc37649de2c9c2f2f7d6140dc1b6da5a7a
Seth Hinze <seth@peachfuzzer.com>
2016-07-26 13:43:01 -0700

    Getting web proxy route DTOs implemented.

--- pro/Test/Core/MutationStrategies/WebProxyTests.cs
+++ pro/Test/Core/MutationStrategies/WebProxyTests.cs
@@ -1,8 +1,11 @@
+using System.IO;
 using NUnit.Framework;
 using Peach.Core;
 using Peach.Core.Dom;
 using Peach.Core.Test;
+using Peach.Pro.Core;
 using Peach.Pro.Core.MutationStrategies;
+using Peach.Pro.Core.WebApi;
 
 namespace Peach.Pro.Test.Core.MutationStrategies
 {
@@ -30,6 +33,31 @@ namespace Peach.Pro.Test.Core.MutationStrategies
 			strategy.Initialize(ctx, null);
 
 			strategy.Finalize(ctx, null);
+		}
+
+		[Test]
+		public void TestDeserialize()
+		{
+			const string xml = @"
+<WebProxy>
+	<Route url='*' />
+	<Route url='/foo/bar' mutate='false' swagger='/tmp/swagger/json' baseUrl='google.com' />
+</WebProxy>
+";
+
+			var obj = XmlTools.Deserialize<WebProxyModel>(new StringReader(xml));
+
+			Assert.NotNull(obj);
+			Assert.NotNull(obj.Routes);
+			Assert.AreEqual(2, obj.Routes.Count);
+			Assert.NotNull(obj.Routes[0]);
+			Assert.AreEqual("*", obj.Routes[0].Url);
+			Assert.AreEqual(true, obj.Routes[0].Mutate);
+			Assert.NotNull(obj.Routes[1]);
+			Assert.AreEqual("/foo/bar", obj.Routes[1].Url);
+			Assert.AreEqual(false, obj.Routes[1].Mutate);
+			Assert.AreEqual("/tmp/swagger/json", obj.Routes[1].Swagger);
+			Assert.AreEqual("google.com", obj.Routes[1].BaseUrl);
 		}
 	}
 }
