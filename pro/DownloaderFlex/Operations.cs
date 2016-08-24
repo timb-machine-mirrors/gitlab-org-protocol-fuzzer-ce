26e29daa01c1391363a9f8f9efa0a3fa9d3ba2b0
Frank Laub <frank.laub@gmail.com>
2016-08-24 13:45:16 -0700

    Use FlexNet.Library and fix SelectActivations when license server is not auto-provisioned

--- pro/DownloaderFlex/Operations.cs
+++ pro/DownloaderFlex/Operations.cs
@@ -3,138 +3,52 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net;
-using System.Web;
+using FlexNet.Library;
 using FlexNet.Operations;
 using NLog;
 
 namespace PeachDownloader
 {
-	// TODO - Make the copy in servicefactory.cs public and use it instead.
-	class Environment
-	{
-		public Dictionary<Type, string> ServiceUrls { get; set; }
-	}
-
 	public class Operations
 	{
-		static Logger logger = LogManager.GetCurrentClassLogger();
+		static Logger _logger = LogManager.GetCurrentClassLogger();
 
-		private string Username { get; set; }
-		private string Password { get; set; }
-		private string AuthToken { get; set; }
-		private EnvironmentType EnvironmentType { get; set; }
-		private NetworkCredential Credentials { get; set; }
-		private NetworkCredential ServiceCredentials { get; set; }
+		private string _username;
+		private string _password;
+		private EnvironmentType _env;
+		private readonly NetworkCredential _creds;
 
 		private static readonly string BaseFeature = ConfigurationManager.AppSettings["OperationsBaseFeature"];
 
-		// TODO - Make the copy in servicefactory.cs public and use it instead.
-		private static readonly Dictionary<EnvironmentType, Environment> _envs = new Dictionary<EnvironmentType, Environment>
-		{
-			{
-				EnvironmentType.Production,
-				new Environment
-				{
-					ServiceUrls = new Dictionary<Type, string>()
-					{
-						{
-							typeof(ProductPackagingService),
-							"https://flex1253-fno.flexnetoperations.com/flexnet/services/ProductPackagingService"
-						},
-						{
-							typeof(EntitlementOrderService),
-							"http://flex1253-fno.flexnetoperations.com/flexnet/services/EntitlementOrderService"
-						},
-						{
-							typeof(UserOrgHierarchyService),
-							"https://flex1253-fno.flexnetoperations.com/flexnet/services/UserOrgHierarchyService"
-						},
-						{
-							typeof(FlexnetAuthenticationService),
-							"https://flex1253-fno.flexnetoperations.com/flexnet/services/FlexnetAuthentication"
-						},
-						{
-							typeof(ManageDeviceService),
-							"https://flex1253-fno.flexnetoperations.com/flexnet/services/ManageDeviceService"
-						},
-						{
-							typeof(UsageService),
-							"https://flex1253-fno.flexnetoperations.com/flexnet/services/UsageService"
-						},
-					}
-				}
-			},
-			{
-				EnvironmentType.UAT,
-				new Environment
-				{
-					ServiceUrls = new Dictionary<Type, string>()
-					{
-						{
-							typeof(ProductPackagingService),
-							"https://flex1253-fno-uat.flexnetoperations.com/flexnet/services/ProductPackagingService"
-						},
-						{
-							typeof(EntitlementOrderService),
-							"http://flex1253-fno-uat.flexnetoperations.com/flexnet/services/EntitlementOrderService"
-						},
-						{
-							typeof(UserOrgHierarchyService),
-							"https://flex1253-fno-uat.flexnetoperations.com/flexnet/services/UserOrgHierarchyService"
-						},
-						{
-							typeof(FlexnetAuthenticationService),
-							"https://flex1253-fno-uat.flexnetoperations.com/flexnet/services/FlexnetAuthentication"
-						},
-						{
-							typeof(ManageDeviceService),
-							"https://flex1253-fno-uat.flexnetoperations.com/flexnet/services/ManageDeviceService"
-						},
-						{
-							typeof(UsageService),
-							"https://flex1253-fno-uat.flexnetoperations.com/flexnet/services/UsageService"
-						},
-					}
-				}
-			}
-		};
-
 		public Operations(string user, string pass)
 		{
-			Username = user;
-			Password = pass;
-			EnvironmentType = ConfigurationManager.AppSettings["OperationsEnvironment"] == "PROD" ? EnvironmentType.Production : EnvironmentType.UAT;
-			Credentials = new NetworkCredential(Username, Password);
-			ServiceCredentials = new NetworkCredential(
-				ConfigurationManager.AppSettings["OperationsServiceUser"], 
-				ConfigurationManager.AppSettings["OperationsServicePassword"]);
+			_username = user;
+			_password = pass;
+			_env = ConfigurationManager.AppSettings["OperationsEnvironment"] == "PROD" ? EnvironmentType.Production : EnvironmentType.UAT;
+			_creds = new NetworkCredential(
+				ConfigurationManager.AppSettings["OperationsServiceUser"],
+				ConfigurationManager.AppSettings["OperationsServicePassword"]
+			);
 		}
 
 		public bool ValidateCredentials()
 		{
 			try
 			{
-				using (var authService = new FlexnetAuthenticationService()
+				var creds = new NetworkCredential(_username, _password);
+				using (var service = Factory.Create<FlexnetAuthenticationService>(_env, creds))
 				{
-					Url = _envs[EnvironmentType].ServiceUrls[typeof(FlexnetAuthenticationService)],
-					Credentials = Credentials
-				})
-				{
-
-					var identity = new IdentityType() {userId = Username};
-					var token = authService.getSecureToken(identity);
-
+					var token = service.getSecureToken(new IdentityType { userId = _username });
 					if (token.token == null)
 						return false;
 
-					AuthToken = token.token;
-
+					//_authToken = token.token;
 					return true;
 				}
 			}
 			catch (Exception ex)
 			{
-				logger.Error(ex, "ValidateCredentials failed for user: {0}", Username);
+				_logger.Error(ex, "ValidateCredentials failed for user: {0}", _username);
 				return false;
 			}
 		}
@@ -151,196 +65,44 @@ namespace PeachDownloader
 		{
 			try
 			{
-				var orgName = string.Empty;
-				var deviceUrl = string.Empty;
+				var fno = new FlexNetOperations(_env, _creds);
+				var user = fno.GetUser(_username);
+				var org = user.orgRolesList[0].organization;
+
 				var activations = new List<Activation>();
+				var entitlements = fno.GetEntitlements(org);
+				foreach (var entitlement in entitlements)
+				{
+					var device = fno.GetDevice(entitlement);
+					var deviceUrl = string.Format(
+						ConfigurationManager.AppSettings["OperationsLicenseServerUrl"],
+						device.deviceIdentifier.deviceId
+					);
 
-				using (var userOrgService = new UserOrgHierarchyService()
-				{
-					Url = _envs[EnvironmentType].ServiceUrls[typeof(UserOrgHierarchyService)],
-					Credentials = ServiceCredentials
-				})
-				{
-					var query = new getUsersQueryRequestType
+					foreach (var item in entitlement.simpleEntitlement.lineItems)
 					{
-						batchSize = "5",
-						pageNumber = "1",
-						queryParams = new userQueryParametersType
+						var product = fno.GetProduct(item.product.primaryKeys.name, item.product.primaryKeys.version);
+						if (!product.features.Any(x => x.featureIdentifier.primaryKeys.name == BaseFeature))
+							continue;
+
+						var act = new Activation
 						{
-							canLogIn = true,
-							isActive = true,
-							userName = new SimpleQueryType {value = Username}
-						}
-					};
+							ActivationId = item.activationId.id,
+							OrgName = entitlement.simpleEntitlement.soldTo,
+							Product = item.product.primaryKeys.name,
+							LicenseServerUrl = deviceUrl
+						};
 
-					// we should only get one
-
-					var result = userOrgService.getUsersQuery(query);
-					if (result.statusInfo.status != StatusType.SUCCESS)
-					{
-						logger.Error("getUsersQuery failed for user '{1}' with error: {0}", 
-							result.statusInfo.reason,
-							Username);
-
-						return null;
-					}
-
-					if (result.responseData == null)
-					{
-						logger.Warn("getUsersQuery reault.responseData was null for user: {0}", Username);
-						return null;
-					}
-
-					orgName = result.responseData[0].orgRolesList[0].organization.primaryKeys.name;
-				}
-
-				using (var deviceService = new ManageDeviceService()
-				{
-					Url = _envs[EnvironmentType].ServiceUrls[typeof(ManageDeviceService)],
-					Credentials = ServiceCredentials
-				})
-				{
-					var query = new getAutoProvisionedServerRequest { orgName = orgName };
-					var result = deviceService.getAutoProvisionedServer(query);
-
-					if (result.statusInfo.status != OpsEmbeddedStatusType.SUCCESS)
-					{
-						logger.Error("getAutoProvisionedServer failed for orgName '{1}' with error: {0}",
-							result.statusInfo.reason,
-							orgName);
-
-						return null;
-					}
-
-					if (result.cloudLicenseServer == null)
-					{
-						logger.Warn("getAutoProvisionedServer reault.cloudLicenseServer was null for org: {0}", orgName);
-					}
-					else
-					{
-						deviceUrl = string.Format(ConfigurationManager.AppSettings["OperationsLicenseServerUrl"],
-							result.cloudLicenseServer.deviceId);
+						activations.Add(act);
 					}
 				}
 
-				using (var entitleService = new EntitlementOrderService()
-				{
-					Url = _envs[EnvironmentType].ServiceUrls[typeof(EntitlementOrderService)],
-					Credentials = ServiceCredentials
-				})
-				{
-					var search = new searchEntitlementRequestType
-					{
-						batchSize = "5",
-						pageNumber = "1",
-						entitlementSearchCriteria = new searchEntitlementDataType
-						{
-							soldTo = new SimpleQueryType
-							{
-								searchType = simpleSearchType.EQUALS,
-								value = orgName
-							}
-						}
-					};
-
-					var result = entitleService.getEntitlementsQuery(search);
-
-					if (result.statusInfo.status != StatusType.SUCCESS)
-					{
-						logger.Error("getEntitlementsQuery failed for orgName '{1}' with error: {0}",
-							result.statusInfo.reason,
-							orgName);
-
-						return null;
-					}
-
-					if (result.entitlement == null)
-					{
-						logger.Warn("getEntitlementsQuery reault.entitlement was null for org: {0}", orgName);
-						return null;
-					}
-
-					foreach (var ent in result.entitlement)
-					{
-						foreach (var item in ent.simpleEntitlement.lineItems)
-						{
-							if (!HasBaseFeature(item.product.primaryKeys.name))
-								continue;
-
-							var act = new Activation
-							{
-								ActivationId = item.activationId.id,
-								OrgName = ent.simpleEntitlement.soldTo,
-								Product = item.product.primaryKeys.name,
-								LicenseServerUrl = deviceUrl
-							};
-
-							activations.Add(act);
-						}
-					}
-
-					return activations;
-				}
+				return activations;
 			}
 			catch (Exception ex)
 			{
-				logger.Error(ex, "Exception in ActivationIds for username: {0}", Username);
+				_logger.Error(ex, "Exception in ActivationIds for username: {0}", _username);
 				return null;
-			}
-		}
-
-		private bool HasBaseFeature(string productName)
-		{
-			try
-			{
-				using (var productService = new ProductPackagingService()
-				{
-					Url = _envs[EnvironmentType].ServiceUrls[typeof(ProductPackagingService)],
-					Credentials = ServiceCredentials
-				})
-				{
-					var query = new getProductsQueryRequestType
-					{
-						batchSize = "5",
-						pageNumber = "1",
-						returnContainedObjects = true,
-						queryParams = new productQueryParametersType
-						{
-							productName = new SimpleQueryType
-							{
-								searchType = simpleSearchType.EQUALS,
-								value = productName
-							}
-						}
-					};
-
-					var result = productService.getProductsQuery(query);
-					
-					if (result.statusInfo.status != StatusType.SUCCESS)
-					{
-						logger.Error("getProductsQuery failed for productName '{1}' with error: {0}",
-							result.statusInfo.reason,
-							productName);
-
-						throw new ArgumentException("HasBaseFeature, request failed.");
-					}
-
-					if (result.responseData == null)
-					{
-						logger.Warn("getProductsQuery reault.responseData was null for productName: {0}", productName);
-						throw new ArgumentException(string.Format("HasBaseFeature, unable to find productName: {0}", productName));
-					}
-
-					var feature =
-						result.responseData[0].features.FirstOrDefault(f => f.featureIdentifier.primaryKeys.name == BaseFeature);
-
-					return feature != null;
-				}
-			}
-			catch (Exception ex)
-			{
-				logger.Error(ex, "Exception in HasBaseFeature for productName: {0}", productName);
-				throw;
 			}
 		}
 	}
