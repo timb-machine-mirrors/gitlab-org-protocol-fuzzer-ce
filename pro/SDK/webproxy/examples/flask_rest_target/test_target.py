4a7632d96df4adb6c0b5834630680b6bd8fc2637
Michael Eddington <mike@dejavusecurity.com>
2016-08-30 15:58:36 -0700

    Updated sdk webproxy contents

--- pro/SDK/webproxy/examples/flask_rest_target/test_target.py
+++ pro/SDK/webproxy/examples/flask_rest_target/test_target.py
@@ -2,7 +2,12 @@
 import pytest, os, json
 from requests import put, get, delete, post
 
-@classmethod
+@pytest.fixture
+def mytest():
+	setup_class()
+	yield ""
+	teardown_class()
+	
 def setup_class():
 	try:
 		delete('http://127.0.0.1:5000/api/users/2')
@@ -13,7 +18,6 @@ def setup_class():
 	except:
 		pass
 	
-@classmethod
 def teardown_class():
 	try:
 		delete('http://127.0.0.1:5000/api/users/2')
@@ -24,18 +28,20 @@ def teardown_class():
 	except:
 		pass
 	
-def test_users_getall():
+def test_users_getall(mytest):
 	get('http://127.0.0.1:5000/api/users')
 
-def test_user_create():
-	user = post('http://127.0.0.1:5000/api/users', data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"})).json()
-	get('http://127.0.0.1:5000/api/users/%s' % user['user_id'])
-	delete('http://127.0.0.1:5000/api/users/%' % user['user_id'])
+def test_user_create(mytest):
+	r = post('http://127.0.0.1:5000/api/users', data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"}))
+	user = r.json()
+	get('http://127.0.0.1:5000/api/users/%d' % user['user_id'])
+	delete('http://127.0.0.1:5000/api/users/%d' % user['user_id'])
 
-def test_user_update():
-	post('http://127.0.0.1:5000/api/users', data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"}))
-	get('http://127.0.0.1:5000/api/users/%s' % user['user_id'])
-	put('http://127.0.0.1:5000/api/users/%s' % user['user_id'], data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"}))
-	delete('http://127.0.0.1:5000/api/users/%s' % user['user_id'])
+def test_user_update(mytest):
+	r = post('http://127.0.0.1:5000/api/users', data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"}))
+	user = r.json()
+	get('http://127.0.0.1:5000/api/users/%d' % user['user_id'])
+	put('http://127.0.0.1:5000/api/users/%d' % user['user_id'], data=json.dumps({"user":"dd", "first":"mike", "last":"smith", "password":"hello"}))
+	delete('http://127.0.0.1:5000/api/users/%d' % user['user_id'])
 
 # end
