3ba94dde113acac7a37adcbddda87246dbf8d27a
Seth Hinze <seth@peachfuzzer.com>
2016-09-14 11:09:55 -0700

    Ensure default WebPRoxy route gets added when state model runs.

--- pro/Test/WebProxy/WebProxyRouteTests.cs
+++ pro/Test/WebProxy/WebProxyRouteTests.cs
@@ -8,6 +8,7 @@ using Peach.Core.Test;
 using Peach.Pro.Core.MutationStrategies;
 using Peach.Pro.Core.Runtime;
 using Peach.Pro.Core.WebApi;
+using Peach.Pro.Core.WebApi.Proxy;
 
 namespace Peach.Pro.Test.WebProxy
 {
@@ -157,6 +158,52 @@ namespace Peach.Pro.Test.WebProxy
 
 			Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
 			Assert.AreEqual("FOOBAR", method);
+		}
+
+		[Test]
+		public void TestNoRoutes()
+		{
+			// Default route gets added when the proxy runs
+			const string xml = @"<?xml version='1.0' encoding='utf-8'?>
+<Peach>
+	<Test name='Default' maxOutputSize='65000'>
+		<WebProxy />
+		<Strategy class='WebProxy' />
+		<Publisher class='WebApiProxy'>
+			<Param name='Port' value='0' />
+		</Publisher>
+	</Test>
+</Peach>";
+
+			WebProxyOptions options = null;
+
+			RunEngine(xml,
+				hookRequestEventPost: (e, context, op) =>
+				{
+					// Since no routes are specified, we have to manually rewrite the url
+					e.WebSession.Request.RequestUri = e.WebSession.Request.RequestUri.Rewrite(Server.Uri.ToString());
+				},
+				hook: e =>
+				{
+					e.IterationFinished += (ctx, it) =>
+					{
+						options = ((WebProxyStateModel)ctx.test.stateModel).Options;
+					};
+				});
+
+			var client = GetHttpClient();
+			var response = client.GetAsync(BaseUrl + "/unknown/api/values/5").Result;
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+			Assert.NotNull(options);
+			Assert.NotNull(options.Routes);
+			Assert.AreEqual(1, options.Routes.Count);
+
+			var r = options.Routes[0];
+
+			Assert.AreEqual("*", r.Url);
+			Assert.False(r.Mutate);
+			Assert.AreEqual("500,501", r.FaultOnStatusCodesAttr);
 		}
 	}
 }
