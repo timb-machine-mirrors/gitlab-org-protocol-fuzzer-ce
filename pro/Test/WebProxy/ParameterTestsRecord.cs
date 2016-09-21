c511aaf8f7348b562f37b0e2560fb12a41644fa1
Seth Hinze <seth@peachfuzzer.com>
2016-09-21 15:58:01 -0700

    Properly handle duplicate path/query/header/formData paremeters.

--- pro/Test/WebProxy/ParameterTestsRecord.cs
+++ pro/Test/WebProxy/ParameterTestsRecord.cs
@@ -349,6 +349,204 @@ namespace Peach.Pro.Test.WebProxy
 		}
 
 		[Test]
+		public void TestDuplicateQuery()
+		{
+			var client = GetHttpClient();
+			var response = client.GetAsync(BaseUrl + "/unknown/api/values?filter=foo&filter=bar").Result;
+			var op = GetOp();
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+			Assert.NotNull(op);
+			Assert.AreEqual("GET", op.Method);
+			Assert.NotNull(op.Path);
+			Assert.Null(op.ShadowOperation);
+			Assert.LessOrEqual(1, op.Parameters.Count);
+
+			var queryParams = op.Parameters.Where(i => i.In == WebApiParameterIn.Query).ToArray();
+
+			Assert.AreEqual(2, queryParams.Length);
+
+			var param = queryParams[0];
+
+			Assert.AreEqual(WebApiParameterIn.Query, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("filter", param.Key);
+			Assert.AreEqual("filter", param.Name);
+			Assert.AreEqual("foo", (string)param.DataElement.DefaultValue);
+
+			param = queryParams[1];
+
+			Assert.AreEqual(WebApiParameterIn.Query, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("filter", param.Key);
+			Assert.AreEqual("filter_1", param.Name);
+			Assert.AreEqual("bar", (string)param.DataElement.DefaultValue);
+
+			var dom = OperationsToDom();
+			Assert.NotNull(dom);
+			var pit = SerializeOperations(dom);
+			Assert.NotNull(pit);
+			var rtt = TestBase.ParsePit(pit);
+			Assert.NotNull(rtt);
+		}
+
+		[Test]
+		public void TestDuplicatePath()
+		{
+			var client = GetHttpClient();
+			var response = client.GetAsync(BaseUrl + "/unknown/api/values/values").Result;
+			var op = GetOp();
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+			Assert.NotNull(op);
+			Assert.AreEqual("GET", op.Method);
+			Assert.NotNull(op.Path);
+			Assert.Null(op.ShadowOperation);
+			Assert.LessOrEqual(1, op.Parameters.Count);
+
+			var pathParams = op.Parameters.Where(i => i.In == WebApiParameterIn.Path).ToArray();
+
+			Assert.AreEqual(4, pathParams.Length);
+
+			var param = pathParams[0];
+
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("unknown", param.Key);
+			Assert.AreEqual("unknown", param.Name);
+			Assert.AreEqual("unknown", (string)param.DataElement.DefaultValue);
+
+			param = pathParams[1];
+
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("api", param.Key);
+			Assert.AreEqual("api", param.Name);
+			Assert.AreEqual("api", (string)param.DataElement.DefaultValue);
+
+			param = pathParams[2];
+
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("values", param.Key);
+			Assert.AreEqual("values", param.Name);
+			Assert.AreEqual("values", (string)param.DataElement.DefaultValue);
+
+
+			param = pathParams[3];
+
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.Null(param.ShadowParameter);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual("values", param.Key);
+			Assert.AreEqual("values_1", param.Name);
+			Assert.AreEqual("values", (string)param.DataElement.DefaultValue);
+
+			var dom = OperationsToDom();
+			Assert.NotNull(dom);
+			var pit = SerializeOperations(dom);
+			Assert.NotNull(pit);
+			var rtt = TestBase.ParsePit(pit);
+			Assert.NotNull(rtt);
+		}
+
+		[Test]
+		public void TestDuplicateHeaders()
+		{
+			var c = new TcpClient();
+
+			c.Connect(IPAddress.Loopback, Port);
+
+			var msg = new[]
+			{
+				"GET /unknown/api/values/values",
+				"Host: sometesthost",
+				"X-Peach: Testing 1..2..3..",
+				"X-Peach: Testing 4..5..6..",
+				"",
+				"",
+			};
+
+			var buf = Encoding.ASCII.GetBytes(string.Join("\r\n", msg));
+
+			c.Client.Send(buf);
+
+			string val;
+
+			using (var rdr = new StreamReader(c.GetStream()))
+			{
+				val = rdr.ReadLine();
+			}
+
+			Assert.NotNull(val);
+
+			var op = GetOp();
+
+			Assert.AreEqual("HTTP/1.1 200 OK", val);
+			Assert.NotNull(op);
+			Assert.AreEqual("GET", op.Method);
+			Assert.NotNull(op.Path);
+			Assert.Null(op.ShadowOperation);
+			Assert.AreEqual(7, op.Parameters.Count);
+
+			var param = op.Parameters[0];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.AreEqual("unknown", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[1];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.AreEqual("api", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[2];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.AreEqual("values", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[3];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Path, param.In);
+			Assert.AreEqual("values", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[4];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Header, param.In);
+			Assert.AreEqual("Host", param.Key);
+			Assert.AreEqual("sometesthost", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[5];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Header, param.In);
+			Assert.AreEqual("X-Peach", param.Key);
+			Assert.AreEqual("x-peach", param.Name);
+			Assert.AreEqual("Testing 1..2..3..", (string)param.DataElement.DefaultValue);
+
+			param = op.Parameters[6];
+			Assert.NotNull(param);
+			Assert.Null(param.ShadowParameter);
+			Assert.AreEqual(WebApiParameterIn.Header, param.In);
+			Assert.AreEqual("X-Peach", param.Key);
+			Assert.AreEqual("x-peach_1", param.Name);
+			Assert.AreEqual("Testing 4..5..6..", (string)param.DataElement.DefaultValue);
+
+			var dom = OperationsToDom();
+			Assert.NotNull(dom);
+			var pit = SerializeOperations(dom);
+			Assert.NotNull(pit);
+			var rtt = TestBase.ParsePit(pit);
+			Assert.NotNull(rtt);
+		}
+
+		[Test]
 		public void TestQueryArray()
 		{
 			var client = GetHttpClient();
