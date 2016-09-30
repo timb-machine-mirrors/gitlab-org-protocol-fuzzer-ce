7c1f8b87892d3253dd8dad0a9fab11380800b402
Seth Hinze <seth@peachfuzzer.com>
2016-09-29 17:51:24 -0700

    Getting python webapi library implemented.

--- pro/Test/WebProxy/WebProxyRouteTests.cs
+++ pro/Test/WebProxy/WebProxyRouteTests.cs
@@ -6,10 +6,13 @@ using System.Net.Http;
 using NUnit.Framework;
 using Peach.Core;
 using Peach.Core.Test;
+using Peach.Pro.Core;
 using Peach.Pro.Core.MutationStrategies;
 using Peach.Pro.Core.Runtime;
 using Peach.Pro.Core.WebApi;
 using Peach.Pro.Core.WebApi.Proxy;
+using Titanium.Web.Proxy.Http;
+using Titanium.Web.Proxy.Models;
 
 namespace Peach.Pro.Test.WebProxy
 {
@@ -189,7 +192,7 @@ namespace Peach.Pro.Test.WebProxy
 from peach import webproxy
 
 def on_request(context, request, body):
-	request.Method = 'FOOBAR'
+	request.__peach_req.Method = 'FOOBAR'
 
 webproxy.register_event(webproxy.EVENT_ACTION, on_request)
 ";
@@ -279,5 +282,92 @@ webproxy.register_event(webproxy.EVENT_ACTION, on_request)
 			Assert.AreEqual("500,501", r.FaultOnStatusCodesAttr);
 		}
 
+
+		[Test]
+		public void TestRequest()
+		{
+			const string script = @"
+from peach import webproxy
+
+def map_req(req):
+	return '%s' % dir(webproxy.Request(req))
+
+def to_h(k, v):
+	return webproxy.Header(k, v)
+
+def to_p(req):
+	return webproxy.Request(req)
+";
+
+			using (var d = new TempDirectory())
+			{
+				var p = new PythonScripting();
+				p.AddSearchPath(Configuration.ScriptsPath);
+
+				File.WriteAllText(Path.Combine(d.Path, "mymodule.py"), script);
+				p.AddSearchPath(d.Path);
+				p.ImportModule("mymodule");
+
+				var req = new Request
+				{
+					Method = "PUT",
+					RequestUri = new Titanium.Web.Proxy.Uri("https://the.host:9999/path/to/item?query&string&goes=here"),
+					ContentType = "application/json"
+				};
+
+				req.NonUniqueRequestHeaders.Add("x-peachy", new List<HttpHeader>
+				{
+					new HttpHeader("X-Peachy", "Foo"),
+					new HttpHeader("X-Peachy", "Bar"),
+				});
+
+				var scope = new Dictionary<string, object> { { "req", req } };
+
+				p.Exec("mymodule.to_p(req).method = 'BBB'", scope);
+
+				Assert.AreEqual("BBB", req.Method);
+
+				Assert.AreEqual("https", p.Eval("mymodule.to_p(req).uri.scheme", scope));
+				Assert.AreEqual("the.host", p.Eval("mymodule.to_p(req).uri.host", scope));
+				Assert.AreEqual(9999, p.Eval("mymodule.to_p(req).uri.port", scope));
+				Assert.AreEqual("/path/to/item", p.Eval("mymodule.to_p(req).uri.path", scope));
+				Assert.AreEqual("?query&string&goes=here", p.Eval("mymodule.to_p(req).uri.query", scope));
+
+				Console.WriteLine(p.Eval("str(mymodule.to_p(req).headers)", scope));
+				Console.WriteLine(p.Eval("str([x for x in mymodule.to_p(req).headers])", scope));
+				Console.WriteLine(p.Eval("str([str(item) for _,sublist in mymodule.to_p(req).headers.iteritems() for item in sublist])", scope));
+
+				p.Exec("mymodule.to_p(req).headers['X-pEaChY'][1].value = 'aaabbb'", scope);
+
+				Assert.AreEqual("aaabbb", req.NonUniqueRequestHeaders["x-peachy"][1].Value);
+
+				p.Exec("mymodule.to_p(req).headers['foo1'] = 'bar'", scope);
+
+				Assert.That(req.RequestHeaders, Contains.Key("foo1"));
+				Assert.That(req.NonUniqueRequestHeaders, !Contains.Key("foo1"));
+				Assert.That(req.RequestHeaders["foo1"].Name, Is.EqualTo("foo1"));
+				Assert.That(req.RequestHeaders["foo1"].Value, Is.EqualTo("bar"));
+
+				p.Exec("mymodule.to_p(req).headers['foo2'] = [ mymodule.to_h('foo2', 'baz') ]", scope);
+
+				Assert.That(req.RequestHeaders, Contains.Key("foo2"));
+				Assert.That(req.NonUniqueRequestHeaders, !Contains.Key("foo2"));
+				Assert.That(req.RequestHeaders["foo2"].Name, Is.EqualTo("foo2"));
+				Assert.That(req.RequestHeaders["foo2"].Value, Is.EqualTo("baz"));
+
+				p.Exec("mymodule.to_p(req).headers['foo1'] = [ mymodule.to_h('foo1', 'qux'), mymodule.to_h('foo1', 'quux') ]", scope);
+
+				Assert.That(req.RequestHeaders, !Contains.Key("foo1"));
+				Assert.That(req.NonUniqueRequestHeaders, Contains.Key("foo1"));
+
+				var items = req.NonUniqueRequestHeaders["foo1"];
+				Assert.AreEqual(2, items.Count);
+
+				Assert.That(items[0].Name, Is.EqualTo("foo1"));
+				Assert.That(items[1].Name, Is.EqualTo("foo1"));
+				Assert.That(items[0].Value, Is.EqualTo("qux"));
+				Assert.That(items[1].Value, Is.EqualTo("quux"));
+			}
+		}
 	}
 }
