d07fdd7b1494a2bc33ed6e48f50c2a2131322233
Rebecca Dadlani <r.dadlani@dejavusecurity.com>
2016-10-06 23:01:02 -0700

    Added tests to verify that mutations work.

--- pro/Test/WebProxy/WebProxyFuzzTests.cs
+++ pro/Test/WebProxy/WebProxyFuzzTests.cs
@@ -6,11 +6,14 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using NUnit.Framework;
+using Org.BouncyCastle.Asn1.Cmp;
 using Peach.Core;
 using Peach.Core.Test;
 using Peach.Pro.Core.Agent.Monitors;
 using Peach.Pro.Core.Storage;
+using Peach.Pro.Core.WebApi;
 using Peach.Pro.Core.WebServices.Models;
+using Peach.Pro.Test.WebProxy.TestTarget.Controllers;
 using Titanium.Web.Proxy;
 using Encoding = Peach.Core.Encoding;
 using FaultSummary = Peach.Pro.Core.WebServices.Models.FaultSummary;
@@ -155,6 +158,92 @@ QN2CJgB1sNtKNTOAbKHcGxgk6hQPaM5SYzEh8R888ei/vxj12O6Qow==
 
 				var response = client.PutAsync(BaseUrl + "/unknown/api/values/5?filter=foo", content).Result;
 				Assert.NotNull(response);
+			}
+		}
+
+		[Test]
+		public void TestMutations()
+		{
+			SetUpFixture.EnableDebug();
+
+			File.WriteAllText(Path.Combine(TempDir.Path, "CaCert.pem"), CaCert);
+			File.WriteAllText(Path.Combine(TempDir.Path, "Cakey.pem"), CaKey);
+			File.WriteAllText(Path.Combine(TempDir.Path, "ServerKey.pem"), ServerKey);
+
+			var xml = @"<?xml version='1.0' encoding='utf-8'?>
+<Peach>
+	<Test name='Default' maxOutputSize='65000'>
+		<WebProxy>
+			<Route
+				url='*/unknown?api/*' mutate='true'
+				baseUrl='{0}'
+			/> 
+			<Route
+				url='*' mutate='false'
+				baseUrl='{0}'
+			/> 
+		</WebProxy>
+		<Strategy class='WebProxy' />
+		<Publisher class='WebApiProxy'>
+			<Param name='Port' value='0' />
+			<Param name='CaCert' value='{1}/CaCert.pem' />
+			<Param name='CaKey' value='{1}/CaKey.pem' />
+			<Param name='ServerKey' value='{1}/ServerKey.pem' />
+		</Publisher>
+	</Test>
+</Peach>".Fmt(Server.Uri, TempDir.Path);
+			RunEngine(xml);
+
+			for (var i = 0; i < 20; i++)
+			{
+
+				var content = new FormUrlEncodedContent(new[]
+				{
+					new KeyValuePair<string, string>("value", "Foo Bar") 
+				});
+
+				var client = GetHttpClient();
+				var headers = client.DefaultRequestHeaders;
+				headers.Add("X-Peachy", "Testing 1..2..3..");
+
+				var response = client.PutAsync(BaseUrl + "/unknown/api/values/5?filter=foo", content).Result;
+				Assert.NotNull(response);
+				
+				if (i == 0)
+				{
+					Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+					var op = GetOp();
+					Assert.NotNull(op);
+					Assert.AreEqual("PUT", op.Method);
+					Assert.NotNull(op.Path);
+					Assert.AreEqual("/{unknown}/{api}/{values}/{5}", op.Path.Path);
+					Assert.LessOrEqual(2, op.Parameters.Count);
+
+					var param = op.Parameters.First(j => j.In == WebApiParameterIn.Path);
+					Assert.AreEqual(WebApiParameterIn.Path, param.In);
+					Assert.AreEqual("unknown", (string)param.DataElement.DefaultValue);
+					Assert.AreEqual(-1, ValuesController.Id);
+				}
+				else
+				{
+					try
+					{
+						Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+					}
+					catch (AssertionException ex)
+					{
+						//Testing that an exception does not actually get thrown.
+						StringAssert.DoesNotContain(ex.ToString(), ex.Message);
+					}
+					var op = GetOp();
+					Assert.NotNull(op);
+					Assert.AreNotEqual("/{unknown}/{api}/{values}/{5}/", op.Path.Path);
+
+					var param = op.Parameters.First(j => j.In == WebApiParameterIn.Path);
+					Assert.AreNotEqual("api", (string)param.DataElement.DefaultValue);
+					Assert.AreNotEqual(5, ValuesController.Id);
+				}
 			}
 		}
 
