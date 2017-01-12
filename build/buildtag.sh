1cff492b4ed0f7f660a652dd7b0c0cfc9800d74b
Frank Laub <frank.laub@gmail.com>
2017-01-11 23:59:59 -0800

    Allow for multiple independent build tags

--- build/buildtag.sh
+++ build/buildtag.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-. jenkins.properties
-
-last=$(git describe --match v${RELEASE_VERSION}.* --abbrev=0 | cut -f3 -d.)
+last=$(git describe --match ${TAG_PREFIX}${RELEASE_VERSION}.* --abbrev=0 | cut -f3 -d.)
 next=$((last+1))
 
 echo BUILDTAG=${RELEASE_VERSION}.${next}
-echo RELEASE_VERSION=${RELEASE_VERSION}
