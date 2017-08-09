5ca8b8a07af179d3d4ab8122a7764ca29c9d538d
Michael Eddington <mike@peachfuzzer.com>
2017-08-09 14:18:31 -0700

    new: Add JsonRaw data element to allow non-standard json output (#535)
    JsonRaw allows adding non-standard text and also binary content into a json document. This feature was requested by an existing customer to support there non-standard usage of JSON documents.

--- pro/Core/JsonUtilities.cs
+++ pro/Core/JsonUtilities.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using Peach.Pro.Core.WebServices;
 
@@ -68,6 +70,73 @@ namespace Peach.Pro.Core
 		public static JsonSerializer CreateSerializer()
 		{
 			return JsonSerializer.Create(GetSettings());
+		}
+	}
+
+	/// <summary>
+	/// Extend JRaw to allow binary data
+	/// </summary>
+	public class JTypeTransform : JRaw
+	{
+		public JTypeTransform(byte[] obj)
+			: base(obj)
+		{
+		}
+
+		public JTypeTransform(Stream obj)
+			: base(obj)
+		{
+		}
+
+		public override void WriteTo(JsonWriter writer, params JsonConverter[] converters)
+		{
+			var sb = Value as Stream;
+			if (sb != null)
+			{
+				((CustomJsonWriter)writer).WriteTypeTransformValue(sb);
+				return;
+			}
+
+			((CustomJsonWriter)writer).WriteTypeTransformValue((byte[])Value);
+		}
+	}
+
+	/// <summary>
+	/// Allow writing out Binary data
+	/// </summary>
+	public class CustomJsonWriter : JsonTextWriter
+	{
+		private readonly StreamWriter _writer;
+
+		public CustomJsonWriter(StreamWriter writer)
+			: base(writer)
+		{
+			_writer = writer;
+		}
+
+		public void WriteTypeTransformValue(Stream bs)
+		{
+			WriteRaw("");
+
+			Flush();
+
+			var origPosition = bs.Position;
+			bs.Position = 0;
+			bs.CopyTo(_writer.BaseStream);
+			bs.Position = origPosition;
+
+			WriteRawValue("");
+		}
+
+		public void WriteTypeTransformValue(byte[] buf)
+		{
+			WriteRaw("");
+
+			Flush();
+
+			_writer.BaseStream.Write(buf, 0, buf.Length);
+
+			WriteRawValue("");
 		}
 	}
 }
