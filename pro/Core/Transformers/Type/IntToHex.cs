12bf4370d36206329ff6ac4febdca201fdcd1ea4
Michael Eddington <mike@peachfuzzer.com>
2017-09-16 14:54:06 -0400

    chg: Add FormatString parameter to IntToHex transformer

--- pro/Core/Transformers/Type/IntToHex.cs
+++ pro/Core/Transformers/Type/IntToHex.cs
@@ -7,28 +7,38 @@ using Peach.Core;
 using Peach.Core.Dom;
 using Peach.Core.IO;
 using System.ComponentModel;
+using NLog;
+using Logger = Peach.Core.Logger;
 
 namespace Peach.Pro.Core.Transformers.Type
 {
 	[Description("Transforms an integer into hex.")]
 	[Transformer("IntToHex", true)]
 	[Transformer("type.IntToHex")]
+	[Parameter("FormatString", typeof(string), ".NET style format string. Defaults to 'X'", "X")]
 	[Serializable]
 	public class IntToHex : Transformer
 	{
+		private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+		public string FormatString { get; set; }
+
 		public IntToHex(DataElement parent, Dictionary<string, Variant> args)
 			: base(parent, args)
 		{
+			ParameterParser.Parse(this, args);
 		}
 
 		protected override BitwiseStream internalEncode(BitwiseStream data)
 		{
 			try
 			{
-
 				string dataAsStr = new BitReader(data).ReadString();
 				int dataAsInt = Int32.Parse(dataAsStr);
-				string dataAsHexStr = dataAsInt.ToString("X");
+				string dataAsHexStr = dataAsInt.ToString(FormatString);
+
+				logger.Trace("Converting '{0}' to '{1}' with FormatString of '{2}'", dataAsInt, dataAsHexStr, FormatString);
+
 				var ret = new BitStream();
 				var writer = new BitWriter(ret);
 				writer.WriteString(dataAsHexStr);
