a38d24602b3b47b8ac072c1b351cc5ee02709e2e
Michael Eddington <mike@peach.tech>
2018-03-13 14:35:39 -0700

    chg: Add required parameter to FromFile (#574)

--- pro/Core/Fixups/FromFileFixup.cs
+++ pro/Core/Fixups/FromFileFixup.cs
@@ -18,11 +18,13 @@ namespace Peach.Pro.Core.Fixups
 	[Fixup("FromFile", true)]
 	[Parameter("Filename", typeof(string), "Filename to load data from")]
 	[Parameter("Encoding", typeof(FromFileEncoding), "File encoding [Raw, Pem]", "Raw")]
+	[Parameter("Required", typeof(bool), "Is file required?", "true")]
 	[Serializable]
 	public class FromFileFixup : Fixup
 	{
 		public string Filename { get; set; }
 		public FromFileEncoding Encoding { get; set; }
+		public bool Required { get; set; }
 
 		public FromFileFixup(DataElement parent, Dictionary<string, Variant> args)
 			: base(parent, args)
@@ -34,7 +36,7 @@ namespace Peach.Pro.Core.Fixups
 		{
 			var _pemContentRegex = new Regex(@"-----BEGIN [A-Za-z0-9 ]*-----(.*)-----END [A-Za-z0-9 ]*-----", RegexOptions.Singleline);
 
-			var pem = ASCIIEncoding.UTF8.GetString(bytes);
+			var pem = Peach.Core.Encoding.UTF8.GetString(bytes);
 			var match = _pemContentRegex.Match(pem);
 
 			if (!match.Success)
@@ -49,8 +51,13 @@ namespace Peach.Pro.Core.Fixups
 		protected override Variant fixupImpl()
 		{
 			if (!System.IO.File.Exists(Filename))
-				throw new PeachException(string.Format("Error, FromFile fixup cannot find file '{0}'.",
-					Filename));
+			{
+				if(Required)
+					throw new PeachException(string.Format("Error, FromFile fixup cannot find file '{0}'.",
+						Filename));
+
+				return new Variant(new byte[0]);
+			}
 
 			var bytes = System.IO.File.ReadAllBytes(Filename);
 
