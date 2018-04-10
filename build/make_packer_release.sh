d19882431d734e7fbfad1478c78c71102b6ae860
Jeff Tucker <j.tucker@peachfuzzer.com>
2018-04-10 13:29:32 -0700

    new: Create trial AMI and target VM (#580)
    * dev: .gitignore will now ignore vim swapfiles
    
    * dev: packer build and template initial commit
    
    * dev: split installing peach and pits into two separate tasks, now grabs sources from build output with buildtag instead of files dir
    
    * dev: tmp directory now handled properly in packer build, fixed install_peach to use buildtag
    
    * dev: gitignore now ignores packer artifacts
    
    * dev: packer now using ubuntu 16.04.4 for vm
    
    * dev: make_packer_release will now add the .ova file to release.json
    
    * dev: fixed nginx conf for trial instances to use letsencrypt properly
    
    * dev: scripts will sleep for 90 seconds before activating letsencrypt to ensure dns propagation
    
    * dev: packer should be integrated into the jenkins build for peach-pro
    
    * dev: removed self-signed certs
    
    * dev: ami uses real cert now instead of letsencrypt staging
    
    * dev: removed peach-web files from common
    
    * dev: removed commented-out stuff from base_02.sh and removed references to peachweb-docker stuff

---
+++ build/make_packer_release.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+set -ex
+
+. build/common.sh
+
+usage() {
+    echo "USAGE: make_packer_release.sh --buildtag 1.2.3 --filesdir /path/to/files [--publish true|false]"
+}
+
+# packer comes from: https://www.packer.io/downloads.html
+# ovatool comes from: https://www.vmware.com/support/developer/ovf/
+
+requires "packer"
+requires "jq"
+if [ -z $ESXI_PASSWORD ]; then
+    requires "ovftool"
+fi
+
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --buildtag)
+            export BUILDTAG="$2"
+            shift
+        ;;
+        --except)
+            export NO_BUILD="$2"
+            shift
+        ;;
+        --filesdir)
+            export FILES_DIR="$2"
+            shift
+        ;;
+        --publish)
+            export PUBLISH_AMI="$2"
+            shift
+        *)
+            usage
+            echo "Unknown Option: $key"
+            exit 1
+        ;;
+    esac
+    shift
+done
+if [ -z "$BUILDTAG" ]; then
+    echo "Missing --buildtag."
+    usage
+    exit 1
+fi
+
+if [ -z "$PUBLISH_AMI" ];
+    echo "--publish not set, defaulting to false"
+    export PUBLISH_AMI="false"
+fi
+
+if [ "$PUBLISH_AMI" == "true" ]; then
+    export BUILD_SUFFIX="prod"
+else
+    export BUILD_SUFFIX="test"
+fi
+
+if [ -z "$FILES_DIR" ]; then
+    echo "Missing --filesdir"
+    usage
+    exit 1
+fi
+
+echo ""
+echo "Running packer"
+echo ""
+
+
+ovadir="$(pwd)/output/release/${BUILDTAG}"
+pushd packer
+
+ESXI_PASSWORD=""
+
+# support local and remote packer builds
+if [ -z "$ESXI_PASSWORD" ]; then
+    # Ensure old packer directory is clean
+    rm -rvf "output-vmware-iso ${ovadir}/peach-targetvm-${BUILDTAG}.ova" 2>/dev/null || {}
+    remote_var=""
+else
+    # Ensure old packer directory is clean
+    rm -rvf "peach-targetvm" 2>/dev/null || {}
+    remote_var="-var remote_type=esx5"
+fi
+
+# Ensure tmp directory for .tar files is clean
+rm -rvf "tmp" 2>/dev/null || {}
+mkdir tmp
+
+# If packer is unable to connect over VNC to the esxi server
+# ensure that the firewall settings did not get reset
+# https://nickcharlton.net/posts/using-packer-esxi-6.html
+
+export AWS_DEFAULT_REGION="us-west-1"
+export PACKER_CACHE_DIR="/root/.packer_cache"
+
+packer build \
+    -var "buildtag=${BUILDTAG}" \
+    -var "ami_name_suffix=${BUILD_SUFFIX}" \
+    -var "files_dir=${FILES_DIR}" \
+    ${remote_var} \
+    template.json
+
+if [ -z "$ESXI_PASSWORD" ]; then
+    ovftool "output-vmware-iso/peach-targetvm.vmx" "${ovadir}/peach-targetvm-${BUILDTAG}.ova"
+else
+    mv "peach_targetvm/peach-targetvm.ova/peach-targetvm.ova" "${ovadir}/peach-targetvm-${BUILDTAG}.ova"
+fi
+
+# last step is to modify the release.json to include the .ova as a file
+jq --arg OVANAME peach-targetvm-${BUILDTAG}.ova '.files += [$OVANAME]' ${ovadir}/release.json > ${ovadir}/release.json.tmp && mv ${ovadir}/release.json.tmp ${ovadir}/release.json
+
+popd
+
+echo ""
+echo "Successfully created peach-targetvm-${BUILDTAG}"
