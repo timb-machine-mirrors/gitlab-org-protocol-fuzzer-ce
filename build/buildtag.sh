dc9855ddbc3686432f33642adb01ad3bfa130e93
Michael Eddington <mike@peach.tech>
2018-06-27 10:00:36 -0700

    dev: Jenkinsfile updates

--- build/buildtag.sh
+++ build/buildtag.sh
@@ -3,4 +3,4 @@
 last=$(git describe --match ${TAG_PREFIX}${RELEASE_VERSION}.* --abbrev=0 | cut -f3 -d.)
 next=$((last+1))
 
-echo BUILDTAG=${RELEASE_VERSION}.${next}
+echo ${RELEASE_VERSION}.${next}
