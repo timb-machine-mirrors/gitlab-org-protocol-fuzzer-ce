1c244a926e9105f3912dca5f8f702275968662ed
Michael Eddington <mike@peach.tech>
2018-09-18 14:40:16 -0700

    dev: upgrading sqlite

--- pro/Core/Runtime/BaseProgram.cs
+++ pro/Core/Runtime/BaseProgram.cs
@@ -150,7 +150,7 @@ namespace Peach.Pro.Core.Runtime
 			// Mono 5.X doesn't yet work with IronPython
 			// https://bugzilla.xamarin.com/show_bug.cgi?id=57962
 			var maxVer = new Version(6, 0, 0);
-			var minVer = new Version(4, 0, 0);
+			var minVer = new Version(4, 2, 3);
 			var badVer = new Version(4, 4);
 
 			var mi = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
