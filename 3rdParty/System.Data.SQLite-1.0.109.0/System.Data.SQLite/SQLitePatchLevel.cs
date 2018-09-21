263a235b13a74f40a7d22e5aba4547d9aedf36b9
Michael Eddington <mike@peach.tech>
2018-09-21 11:58:10 -0700

    dev: more sqlite mess

---
+++ 3rdParty/System.Data.SQLite-1.0.109.0/System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("86636b58e29f85d17a2194bfacdb3b989a81b2bb")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2018-08-11 19:58:48 UTC")]
