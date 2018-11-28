9122688e68cc87f57f015dd53d4127f50584be3c
Michael Eddington <mike@peach.tech>
2018-11-27 16:36:52 -0800

    dev: azure

--- build/azure/linux/03-test.sh
+++ build/azure/linux/03-test.sh
@@ -6,23 +6,24 @@ set -ev
 
 echo "*** Running tests ***"
 
-bin_folder = "output/linux_x86_64_debug/bin/"
-result = "output/nunit-linux_x86_64_debug"
-nunit_error = false
+bin_folder="output/linux_x86_64_debug/bin/"
+result="output/nunit-linux_x86_64_debug"
+nunit_error=false
 include=Quick
 
-TESTDLLS=[
-    "Peach.Core.Test.dll",
-    "Peach.Pro.Test.OS.Linux.dll",
-    "Peach.Pro.Test.WebApi.exe",
+declare -a TESTDLLS=(
+    "Peach.Core.Test.dll"
+    "Peach.Pro.Test.OS.Linux.dll"
+    "Peach.Pro.Test.WebApi.exe"
     "Peach.Pro.Test.dll"
-    ]
+)
 
-for it in $TESTDLLS; do
+for it in "${TESTDLLS[@]}"
+do
 
     echo "*** Running tests for '$it' ***"
 
-    mono -debug \
+    mono --debug \
         ${bin_folder}nunit3-console.exe \
         --labels=All \
         --where "cat == ${include}" \
@@ -31,14 +32,15 @@ for it in $TESTDLLS; do
     
     $ret=$_
 
-    print "Nunit exit code: $ret"
+    echo "Nunit exit code: $ret"
 
     #if $ret < 0
     #{
     #    error("nunit test runner failed") 
     #}
 
-    if ["$ret" != "0"] then
+    if ["$ret" != "0"] 
+    then
         nunit_error = true
     fi
 
